08bc91004ef5c2d246a24bcdcc058b8fa863a3fb
Matthew R Hanlon <mrhanlon@tacc.utexas.edu>
2015-09-22 13:29:58 -0500

    added cascade-cms plugin

--- meetings/cms_plugins.py
+++ meetings/cms_plugins.py
@@ -1,10 +1,22 @@
-from cms.plugin_base import CMSPluginBase
 from cms.plugin_pool import plugin_pool
+from cms.plugin_base import CMSPluginBase
+from cmsplugin_cascade.plugin_base import CascadePluginBase
 from .forms import MeetingRequestForm
 
 @plugin_pool.register_plugin
 class MeetingFormPlugin(CMSPluginBase):
     name = 'Meeting Request Form'
+    render_template = "meetings/cms/plugin.html"
+
+    def render(self, context, instance, placeholder):
+        context['instance'] = instance
+        context['form'] = MeetingRequestForm()
+
+        return context
+
+@plugin_pool.register_plugin
+class MeetingFormCascadePlugin(CascadePluginBase):
+    name = 'Meeting Request Form (Cascade)'
     render_template = "meetings/cms/plugin.html"
 
     def render(self, context, instance, placeholder):
