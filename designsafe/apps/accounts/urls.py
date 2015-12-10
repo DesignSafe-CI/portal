57052af0449c41821178247efa215af76dd05aa7
Matthew R Hanlon <mrhanlon@tacc.utexas.edu>
2015-12-10 15:03:15 -0600

    add register view stub

--- designsafe/apps/accounts/urls.py
+++ designsafe/apps/accounts/urls.py
@@ -2,4 +2,5 @@ from django.conf.urls import patterns, include, url
 
 urlpatterns = patterns('designsafe.apps.accounts.views',
     url(r'^$', 'index', name='index'),
+    url(r'^register/$', 'register', name='register'),
 )
--- designsafe/apps/accounts/views.py
+++ designsafe/apps/accounts/views.py
@@ -3,3 +3,6 @@ from django.shortcuts import render
 # Create your views here.
 def index(request):
     return render(request, 'designsafe/apps/accounts/index.html')
+
+def register(request):
+    return render(request, 'designsafe/apps/accounts/register.html')
