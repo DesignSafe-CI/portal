29b139cb37057b9cec69e7ab3f4a2722b435b7b3
Josue Balandrano Coronel <xirdneh@gmail.com>
2015-12-11 16:36:41 -0600

    Added Agave auth from master

--- designsafe/common_settings.py
+++ designsafe/common_settings.py
@@ -411,3 +411,4 @@ AGAVE_TENANT_BASEURL = os.environ.get('AGAVE_TENANT_BASEURL')
 # Agave Client Configuration
 AGAVE_CLIENT_KEY = os.environ.get('AGAVE_CLIENT_KEY')
 AGAVE_CLIENT_SECRET = os.environ.get('AGAVE_CLIENT_SECRET')
+
