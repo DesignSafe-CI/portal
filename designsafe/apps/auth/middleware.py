843a38f72d4a248f6a13e8f76778f9fd727adb3b
Matthew R Hanlon <mrhanlon@tacc.utexas.edu>
2016-01-04 09:21:01 -0600

    clean up token refresh

--- designsafe/apps/auth/middleware.py
+++ designsafe/apps/auth/middleware.py
@@ -1,20 +1,11 @@
 from django.conf import settings
 
-from agavepy.agave import Agave, Token
+from agavepy.agave import Agave
 import logging
 import time
 
 
 logger = logging.getLogger(__name__)
-
-class _AgaveStub(object):
-    """
-    A stub class to mimic the Agave object that is typically the "parent" of
-    agavepy.agave.Token
-    """
-
-    def refresh_aris(self):
-        pass
 
 class AgaveTokenRefreshMiddleware(object):
 
@@ -26,24 +17,27 @@ class AgaveTokenRefreshMiddleware(object):
                 token = request.session[token_key]
                 valid_seconds = token['created'] + token['expires_in'] - current_time
                 if valid_seconds <= 0:
-                    logger.debug('refreshing current token: %s' %
-                        token['access_token'][:8].ljust(len(token), '-'))
+                    masked_token = token['access_token'][:8].ljust(
+                        len(token['access_token']), '-')
+                    logger.debug('refreshing current token: %s' % masked_token)
 
-                    agave_token = Token(None, None,
-                        getattr(settings, 'AGAVE_TENANT_BASEURL'),
-                        getattr(settings, 'AGAVE_CLIENT_KEY'),
-                        getattr(settings, 'AGAVE_CLIENT_SECRET'), True, _AgaveStub())
-                    agave_token.token_info = {'refresh_token': token['refresh_token']}
-                    agave_token.refresh()
-                    agave_token.token_info['created'] = current_time
+                    ag = Agave(
+                        api_server=getattr(settings, 'AGAVE_TENANT_BASEURL'),
+                        api_key=getattr(settings, 'AGAVE_CLIENT_KEY'),
+                        api_secret=getattr(settings, 'AGAVE_CLIENT_SECRET'),
+                        token=token['access_token'], refresh_token=token['refresh_token'])
+                    ag.token.refresh()
 
-                    request.session[token_key] = agave_token.token_info
+                    ag.token.token_info['created'] = current_time
+                    request.session[token_key] = ag.token.token_info
                     request.session.save()
 
-                    logger.info('refreshed token: %s' %
-                        agave_token.token_info['access_token'][:8].ljust(len(token), '-'))
+                    masked_token = ag.token.token_info['access_token'][:8].ljust(
+                        len(ag.token.token_info['access_token']), '-')
+                    logger.debug('refreshed token: %s' % masked_token)
                 else:
+                    masked_token = token['access_token'][:8].ljust(
+                        len(token['access_token']), '-')
                     logger.debug('session.%s valid for %s additional seconds: %s' %
-                        (token_key, valid_seconds,
-                            token['access_token'][:8].ljust(len(token), '-')))
+                        (token_key, valid_seconds, masked_token))
 
