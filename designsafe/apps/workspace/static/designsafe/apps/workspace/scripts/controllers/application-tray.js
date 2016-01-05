9f2d958b2c936125ca226eddb76b0177bca7250e
Manuel Rojas <mrojas@tacc.utexas.edu>
2016-01-05 07:33:47 -0600

    Added apps-list, files-list, jobs-list, job-details to urls and views

--- designsafe/apps/workspace/static/designsafe/apps/workspace/scripts/controllers/application-tray.js
+++ designsafe/apps/workspace/static/designsafe/apps/workspace/scripts/controllers/application-tray.js
@@ -1,6 +1,6 @@
 (function(window, angular, $) {
   "use strict";
-  angular.module('WorkspaceApp').controller('ApplicationTrayCtrl', ['$scope', 'Apps', function($scope, Apps) {
+  angular.module('WorkspaceApp').controller('ApplicationTrayCtrl', ['$scope', '$http', 'Apps', function($scope, $http, Apps) {
 
     $scope.data = {};
     $scope.data.apps = Apps.list();
@@ -8,6 +8,64 @@
     $scope.launchApp = function(app) {
       window.alert('GET ' + app._links.self.href);
     };
+
+    // apps-list
+    $http(
+      {
+        method: 'GET',
+        url: 'apps-list',
+      }
+    ).success(function(resp) {
+        console.log('apps-list success');
+        console.log(resp);
+      }).error(function(resp) {
+        console.log('apps-list error ', resp);
+    });
+
+    // files-list
+    $http(
+      {
+        method: 'GET',
+        url: 'files-list',
+      }
+    ).success(function(resp) {
+        console.log('files-list success');
+        console.log(resp);
+      }).error(function(resp) {
+        console.log('files-list error ', resp);
+    });
+
+    // jobs list
+    $http(
+      {
+        method: 'GET',
+        url: 'jobs-list',
+      }
+    ).success(function(resp) {
+        console.log('jobs-list success');
+        console.log(resp);
+      }).error(function(resp) {
+       console.log('jobs-list error ', resp);
+    });
+
+    // jobs details
+    var data = {
+      'id': '4042824268683350501-242ac118-0001-007'
+    };
+    $http(
+      {
+        method: 'POST',
+        url: 'jobs-details/',
+        data: data
+      }
+    ).success(function(resp) {
+        console.log('jobs-details success');
+        console.log(resp);
+      }).error(function(resp) {
+        console.log('jobs-details error ', resp);
+    });
+
+
   }]);
 
 })(window, angular, jQuery);
--- designsafe/apps/workspace/urls.py
+++ designsafe/apps/workspace/urls.py
@@ -5,4 +5,8 @@ from django.views.generic.base import TemplateView
 urlpatterns = patterns('designsafe.apps.workspace.views',
     url(r'^$', 'index', name='index'),
     url(r'^templates/(?P<resource>.+?)\.html$', 'get_template', name='ws_get_template'),
+    url(r'^apps-list/$', 'apps_list', name='data-apps_list'),
+    url(r'^files-list/$', 'files_list', name='data-files_list'),
+    url(r'^jobs-list/$', 'jobs_list', name='data-jobs_list'),
+    url(r'^jobs-details/$', 'jobs_details', name='data-jobs_details'),
 )
