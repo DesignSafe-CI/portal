6f87849cd46e9a00c28b43974cc4f521af953004
Josue Balandrano Coronel <xirdneh@gmail.com>
2016-01-06 10:36:10 -0600

    Moved templates to be served static

--- designsafe/apps/data/static/designsafe/apps/data/scripts/app.js
+++ designsafe/apps/data/static/designsafe/apps/data/scripts/app.js
@@ -8,7 +8,6 @@
     "use strict";
 
     function config($interpolateProvider, $httpProvider) {
-        $interpolateProvider.startSymbol('[[').endSymbol(']]');
         $httpProvider.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
         $httpProvider.defaults.headers.post['Content-Type'] = 'application/x-www-form-urlencoded';
         $httpProvider.defaults.xsrfCookieName = 'csrftoken';
