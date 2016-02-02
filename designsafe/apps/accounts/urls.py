5fcece0df2e72231fdb045120f7c6e1510b60d07
Matthew R Hanlon <mrhanlon@tacc.utexas.edu>
2016-02-02 16:48:34 -0600

    "Complete" stubs for each account section
    - Made the account sections easily extensible.
    - Added support for extensible, auto-discovered app integrations

--- designsafe/apps/accounts/urls.py
+++ designsafe/apps/accounts/urls.py
@@ -1,8 +1,14 @@
 from django.conf.urls import patterns, include, url
 
-urlpatterns = patterns('designsafe.apps.accounts.views',
+urlpatterns = patterns(
+    'designsafe.apps.accounts.views',
     url(r'^$', 'index', name='index'),
+    url(r'^profile/$', 'manage_profile', name='manage_profile'),
+    url(r'^authentication/$', 'manage_authentication', name='manage_authentication'),
+    url(r'^identities/$', 'manage_identities', name='manage_identities'),
+    url(r'^applications/$', 'manage_applications', name='manage_applications'),
+    url(r'^notifications/$', 'notifications', name='notifications'),
+    url(r'^notifications/settings/$', 'manage_notifications', name='manage_notifications'),
     url(r'^register/$', 'register', name='register'),
     url(r'^departments\.json$', 'departments_json', name='departments_json'),
-    url(r'^notifications/$', 'notifications', name='notifications'),
 )
