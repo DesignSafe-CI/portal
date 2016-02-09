510a6d9da6c3cf3bf2e15cb599f8a5f7a64e412f
Matthew R Hanlon <mrhanlon@tacc.utexas.edu>
2016-02-08 20:56:00 -0600

    refactor signals

---
+++ designsafe/apps/auth/signals.py
@@ -0,0 +1,8 @@
+from django.contrib.auth.signals import user_logged_in
+from django.dispatch import receiver
+from .tasks import check_or_create_agave_home_dir
+
+
+@receiver(user_logged_in)
+def on_user_logged_in(sender, user, request, **kwargs):
+    check_or_create_agave_home_dir.delay(user.username)
