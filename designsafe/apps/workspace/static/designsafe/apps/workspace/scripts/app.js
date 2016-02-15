744bb42032aa68ac1d081c87ed977d76d1001502
Marjo Poindexter <mpoindexter@dhcp-146-6-176-201.tacc.utexas.edu>
2016-02-15 11:24:36 -0600

    use wss:// when page is secure on workspace app

--- designsafe/apps/workspace/static/designsafe/apps/workspace/scripts/app.js
+++ designsafe/apps/workspace/static/designsafe/apps/workspace/scripts/app.js
@@ -5,7 +5,12 @@
     $httpProvider.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
     $httpProvider.defaults.xsrfCookieName = 'csrftoken';
     $httpProvider.defaults.xsrfHeaderName = 'X-CSRFToken';
-    WSBusServiceProvider.setUrl('wss://' + window.location.hostname + (window.location.port ? ':' + window.location.port : '') + '/ws/websockets?subscribe-broadcast');
+    WSBusServiceProvider.setUrl(
+            (window.location.protocol === 'https:' ? 'wss://' : 'ws://') +
+            window.location.hostname +
+            (window.location.port ? ':' + window.location.port : '') +
+            '/ws/websockets?subscribe-broadcast'
+    );
   }
 
   angular.module('WorkspaceApp', ['ngCookies', 'ng.django.urls', 'ui.bootstrap', 'schemaForm', 'ds.wsBus', 'ds.notifications']).config(['WSBusServiceProvider', 'NotificationServiceProvider', '$interpolateProvider', '$httpProvider', config]);
