21327495cc6f593219e0855819914ea83806e8b8
Matthew R Hanlon <mrhanlon@tacc.utexas.edu>
2016-02-16 11:05:36 -0600

    fix bug in webhook response

--- designsafe/apps/notifications/views.py
+++ designsafe/apps/notifications/views.py
@@ -56,8 +56,7 @@ def job_notification_handler(request):
 
     Event.send_event(JOB_EVENT, body)
 
-    return HttpResponse(json.dumps(data, cls=DjangoJSONEncoder),
-        content_type='application/json')
+    return HttpResponse('OK')
 
 def get_number_unread_notifications(request):
     # nondeleted = JobNotification.objects.filter(deleted=False, user=str(request.user)).count()
