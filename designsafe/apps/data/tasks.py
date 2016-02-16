94784ce724a6f038cccb15a1a281d67a28254e32
Matthew R Hanlon <mrhanlon@tacc.utexas.edu>
2016-02-16 13:18:11 -0600

    fix indexing job outputs

--- designsafe/apps/data/tasks.py
+++ designsafe/apps/data/tasks.py
@@ -16,19 +16,23 @@ def index_job_outputs(data):
     job_owner = data['job_owner']
     job_id = data['job_id']
 
-    try:
-        user = get_user_model().objects.get(username=job_owner)
-        if user.agave_oauth.expired:
-            user.agave_oauth.refresh()
-        ag = Agave(api_server=settings.AGAVE_TENANT_BASEURL,
-                   token=user.agave_oauth.token.access_token)
+    if data['status'] in ['FINISHED', 'FAILED']:
+        try:
+            user = get_user_model().objects.get(username=job_owner)
+            if user.agave_oauth.expired:
+                user.agave_oauth.refresh()
+            ag = Agave(api_server=settings.AGAVE_TENANT_BASEURL,
+                       token=user.agave_oauth.token['access_token'])
+            job = ag.jobs.get(jobId=job_id)
+            system_id = job['archiveSystem']
+            archive_path = job['archivePath']
+            base_file = ag.files.list(systemId=system_id, filePath=archive_path)
+            # the listing returns "name"="." for the first folder in the list
+            # we need the actual folder name
+            base_file[0]['name'] = base_file[0]['path'].split('/')[-1]
+            get_or_create_from_file(ag, base_file[0])
+            for f in fs_walk(ag, system_id, archive_path):
+                get_or_create_from_file(ag, f)
 
-        job = ag.jobs.get(jobId=job_id)
-        system_id = job['archiveSystem']
-        archive_path = job['archivePath']
-
-        for f in fs_walk(ag, system_id, archive_path):
-            get_or_create_from_file(ag, f)
-
-    except ObjectDoesNotExist:
-        logger.exception('Unable to locate local user=%s' % job_owner)
+        except ObjectDoesNotExist:
+            logger.exception('Unable to locate local user=%s' % job_owner)
