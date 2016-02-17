9df05ab43c360f3c333ff374b271e60c2981d376
Matthew R Hanlon <mrhanlon@tacc.utexas.edu>
2016-02-17 08:34:04 -0700

    notify events from local job watch

--- designsafe/apps/notifications/models.py
+++ designsafe/apps/notifications/models.py
@@ -1,4 +1,7 @@
 from django.db import models
+from django.dispatch import receiver
+from designsafe.apps.signals.signals import notify_event
+from .apps import Event
 import datetime
 import json
 
@@ -18,3 +21,17 @@ class Notification(models.Model):
     def mark_read(self):
         self.read = True
         self.save()
+
+
+@receiver(notify_event)
+def receive_notification(sender, **kwargs):
+    event_type = kwargs.get('event_type')
+    event_data = kwargs.get('event_data')
+    event_users = kwargs.get('event_users')
+    
+    for user in event_users:
+        notification = Notification(event_type=event_type, user=user,
+                                    body=json.dumps(event_data))
+        notification.save()
+
+    Event.send_event(event_type, event_users, event_data)
--- designsafe/apps/workspace/tasks.py
+++ designsafe/apps/workspace/tasks.py
@@ -2,7 +2,7 @@ from __future__ import absolute_import
 
 from celery import shared_task, task
 from designsafe.celery import app
-
+from designsafe.apps.signals.signals import notify_event
 from agavepy.agave import Agave, AgaveException
 
 import logging
@@ -33,30 +33,45 @@ def watch_job_status(data):
         job = ag.jobs.get(jobId=job_id)
         job_status = job['status']
 
+        event_data = {
+            'job_name': job['name'],
+            'job_id': job['id'],
+            'event': job_status,
+            'status': job_status,
+            'archive_path': job['archivePath'],
+            'job_owner': job['owner'],
+        }
+
         if 'retry' in data:
             # clear out any past retries
             del data['retry']
 
         if job_status in ['FINISHED', 'FAILED']:
-            # index job output
-            logger.debug('JOB FINALIZED: id=% status=%s' % (job_id, job_status))
+            # job finished, no additional tasks; notify
+            logger.debug('JOB FINALIZED: id=%s status=%s' % (job_id, job_status))
+            notify_event.send_robust(None, event_type='job', event_data=event_data,
+                                     event_users=[username])
         elif current_status and current_status == job_status:
-            # DON'T notify, but still queue another watch task
-            watch_job_status.apply_async(args=[data], countdown=10)
+            # DO NOT notify, but still queue another watch task
+            checks = data.get('status_checks', 0) + 1
+            data['status_checks'] = checks
+            watch_job_status.apply_async(args=[data], countdown=10*checks)
         else:
-            # notify and queue another watch task
-            logger.debug('JOB STATUS CHANGE: id=% status=%s' % (job_id, job_status))
+            # queue another watch task
             data['current_status'] = job_status
+            data['status_checks'] = 0
             watch_job_status.apply_async(args=[data], countdown=10)
+            # notify
+            logger.debug('JOB STATUS CHANGE: id=%s status=%s' % (job_id, job_status))
+            notify_event.send_robust(None, event_type='job', event_data=event_data,
+                                     event_users=[username])
     except ObjectDoesNotExist:
         logger.exception('Unable to locate local user account: %s' % username)
     except (AgaveException, RequestException):
-        if 'retry' in data:
-            logger.exception('Agave API error. Retry failed. Aborting.')
-        else:
-            logger.warning('Agave API error. Retrying...')
-            data['retry'] = True
-            watch_job_status.apply_async(args=[data], countdown=10)
+        retries = data.get('retry', 0) + 1
+        data['retry'] = retries
+        logger.warning('Agave API error. Retry number %s...' % retries)
+        watch_job_status.apply_async(args=[data], countdown=10**retries)
 
 
 @app.task
