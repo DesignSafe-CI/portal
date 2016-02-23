4dd02470dc97c741d2da57d4b23b8873ba9487db
Matthew R Hanlon <mrhanlon@tacc.utexas.edu>
2016-02-23 10:03:08 -0600

    improve log messages

--- designsafe/apps/cms_plugins/menu.py
+++ designsafe/apps/cms_plugins/menu.py
@@ -16,24 +16,27 @@ def _menu_nodes_for_apps(menu_type):
         try:
             mod = import_module('%s.urls' % app)
             try:
-                for item in mod.menu_items(type=menu_type):
-                    node = NavigationNode(item['label'], item['url'], node_id,
-                                          visible=item.get('visible', True))
-                    nodes.append(node)
-                    if 'children' in item:
-                        parent_id = node_id
-                        for c in item['children']:
-                            node_id += 1
-                            node = NavigationNode(c['label'], c['url'],
-                                                  node_id, parent_id,
-                                                  visible=c.get('visible', True))
-                            nodes.append(node)
-                    node_id += 1
+                items = mod.menu_items(type=menu_type)
+                if items is not None:
+                    for item in items:
+                        node = NavigationNode(item['label'], item['url'], node_id,
+                                              visible=item.get('visible', True))
+                        nodes.append(node)
+                        if 'children' in item:
+                            parent_id = node_id
+                            for c in item['children']:
+                                node_id += 1
+                                node = NavigationNode(c['label'], c['url'],
+                                                      node_id, parent_id,
+                                                      visible=c.get('visible', True))
+                                nodes.append(node)
+                        node_id += 1
             except AttributeError:
                 continue
+            except TypeError:
+                logger.warning('Call to %s.cms_menu_nodes should return an iterable!')
             except:
-                logger.exception('Call to module.cms_menu_nodes fail for module: '
-                                 '%s' % mod.__name__)
+                logger.exception('Call to %s.cms_menu_nodes failed!' % mod.__name__)
         except:
             continue
     return nodes
