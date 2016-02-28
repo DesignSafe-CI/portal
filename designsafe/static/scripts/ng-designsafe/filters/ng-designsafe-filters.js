3a7fa37b22b07d55b9d49006327b82931934f178
Matthew R Hanlon <mrhanlon@tacc.utexas.edu>
2016-02-28 17:05:56 -0600

    fix NaN with bytes=0

--- designsafe/static/scripts/ng-designsafe/filters/ng-designsafe-filters.js
+++ designsafe/static/scripts/ng-designsafe/filters/ng-designsafe-filters.js
@@ -3,8 +3,8 @@
     return function(bytes, precision) {
       if (isNaN(parseFloat(bytes)) || !isFinite(bytes)) return '-';
       if (typeof precision === 'undefined') precision = 1;
-      var units = ['bytes', 'kB', 'MB', 'GB', 'TB', 'PB'],
-        number = Math.floor(Math.log(bytes) / Math.log(1024));
+      var units = ['bytes', 'kB', 'MB', 'GB', 'TB', 'PB'];
+      var number = bytes === 0 ? 0 : Math.floor(Math.log(bytes) / Math.log(1024));
       return (bytes / Math.pow(1024, Math.floor(number))).toFixed(precision) +  ' ' + units[number];
     }
   });
