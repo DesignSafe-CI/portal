7e9337192153d506f42da4482827c689eaf21fa1
Josue Balandrano Coronel <xirdneh@gmail.com>
2016-02-27 21:03:35 -0600

    Making data view public

--- designsafe/apps/data/views/mixins.py
+++ designsafe/apps/data/views/mixins.py
@@ -40,11 +40,10 @@ class AgaveMixin(object):
             me = get_user_model().objects.get(username=request.user.username)
             me.agave_oauth.refresh()
             self.token = me.agave_oauth
+            self.access_token = self.token.access_token
         else:
-            me = get_user_model().objects.get(username='envision')
-            me.agave_oauth.refresh()
-            self.token = me.agave_oauth
-        self.access_token = self.token.access_token
+            self.token = None
+            self.access_token = ''
         self.agave_url = getattr(settings, 'AGAVE_TENANT_BASEURL')
         self.set_agave_client(api_server = self.agave_url, token = self.access_token)
 
