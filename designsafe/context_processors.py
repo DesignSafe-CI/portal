a4608231133b090e0115a9b58e1724895e30b7b5
Matthew R Hanlon <mrhanlon@tacc.utexas.edu>
2016-03-01 10:38:18 -0600

    custom messages processor to prevent duplicate message display

--- designsafe/context_processors.py
+++ designsafe/context_processors.py
@@ -1,4 +1,7 @@
 from django.conf import settings
+from django.contrib.messages.api import get_messages
+from django.contrib.messages.constants import DEFAULT_LEVELS
+
 
 def analytics( request ):
     """
@@ -10,3 +13,27 @@ def analytics( request ):
     if not settings.DEBUG and ga_prop_id:
         context['GOOGLE_ANALYTICS_PROPERTY_ID'] = ga_prop_id
     return context
+
+
+def messages(request):
+    """
+    Same as the default messages middleware, but it is duplicate-message aware,
+    preventing duplicate messages with the same content from being displayed.
+
+    Args:
+        request:
+
+    Returns:
+        a dict of messages to be added to the context
+    """
+    msgs = []
+    unique_msgs = []
+    for m in get_messages(request):
+        if m.message not in msgs:
+            msgs.append(m.message)
+            unique_msgs.append(m)
+
+    return {
+        'messages': unique_msgs,
+        'DEFAULT_MESSAGE_LEVELS': DEFAULT_LEVELS,
+    }
