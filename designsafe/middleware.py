6d4bedbaff10e5ba6d3438152b880c2efc4a521e
Matthew R Hanlon <mrhanlon@tacc.utexas.edu>
2016-03-02 15:04:32 -0600

    typo

--- designsafe/middleware.py
+++ designsafe/middleware.py
@@ -28,7 +28,7 @@ class DesignSafeTermsMiddleware(TermsAndConditionsRedirectMiddleware):
                                          '/terms/accept/') + term
                     messages.warning(
                         request, '<h4>Please Accept the Terms of Use</h4>'
-                                 'You have not yet agreed to the current Terms of Use.'
+                                 'You have not yet agreed to the current Terms of Use. '
                                  'Please <a href="%s">CLICK HERE</a> to review and '
                                  'accept the Terms of Use.<br>Acceptance of the Terms of '
                                  'Use is required for continued use of DesignSafe-CI '
