87d1ce597e2691dffa9d5a32d67864d96081a89d
Matthew R Hanlon <mrhanlon@tacc.utexas.edu>
2016-03-02 13:57:49 -0600

    update dump script

--- bin/dumpdata.sh
+++ bin/dumpdata.sh
@@ -4,5 +4,4 @@ python /portal/manage.py dumpdata \
     --natural-foreign --natural-primary \
     --exclude=cmsplugin_cascade.Segmentation \
     --exclude=admin.logentry \
-    --exclude=cms.pageusergroup \
-    --exclude=reversion > /datadump/datadump-`date +%Y%m%d`.json
+    --exclude=cms.pageusergroup > /datadump/datadump-`date +%Y%m%d`.json
