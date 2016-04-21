2b6ae53935d0d1f11e19de92161e77ad8e768b62
Matthew R Hanlon <mrhanlon@tacc.utexas.edu>
2016-04-21 16:16:04 -0500

    cleanup box_integration, remove sync

--- designsafe/apps/box_integration/migrations/0003_auto_20160328_2215.py
+++ designsafe/apps/box_integration/migrations/0003_auto_20160328_2215.py
@@ -13,15 +13,6 @@ class Migration(migrations.Migration):
     ]
 
     operations = [
-        migrations.CreateModel(
-            name='BoxUserStreamPosition',
-            fields=[
-                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
-                ('box_user_id', models.CharField(max_length=48)),
-                ('stream_position', models.CharField(default=b'now', max_length=48)),
-                ('user', models.OneToOneField(related_name='box_stream_pos', to=settings.AUTH_USER_MODEL)),
-            ],
-        ),
         migrations.AlterField(
             model_name='boxusertoken',
             name='user',
