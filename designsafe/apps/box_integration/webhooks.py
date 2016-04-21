ae4da8adaafcf27cb10f58fc05d1929f5f33692d
Matthew R Hanlon <mrhanlon@tacc.utexas.edu>
2016-04-21 16:01:59 -0500

    disable box webhook task trigger

--- designsafe/apps/box_integration/webhooks.py
+++ designsafe/apps/box_integration/webhooks.py
@@ -40,23 +40,23 @@ def box_webhook(request):
         None
 
     """
-    try:
-        if request.method == 'POST':
-            if request.META['CONTENT_TYPE'] == 'application/json':
-                event_data = json.loads(request.body)
-            else:
-                event_data = request.POST.copy()
-                # to_user_ids is a list as string
-                event_data['to_user_ids'] = ast.literal_eval(event_data['to_user_ids'])
-        else:
-            event_data = request.GET.copy()
-            # to_user_ids is a list as string
-            event_data['to_user_ids'] = ast.literal_eval(event_data['to_user_ids'])
-
-        logger.debug('Received Box Webhook; event_data=%s' % event_data)
-        handle_box_webhook_event.apply_async(args=(event_data,))
-    except AttributeError:
-        pass
-    except KeyError:
-        pass
+    # try:
+    #     if request.method == 'POST':
+    #         if request.META['CONTENT_TYPE'] == 'application/json':
+    #             event_data = json.loads(request.body)
+    #         else:
+    #             event_data = request.POST.copy()
+    #             # to_user_ids is a list as string
+    #             event_data['to_user_ids'] = ast.literal_eval(event_data['to_user_ids'])
+    #     else:
+    #         event_data = request.GET.copy()
+    #         # to_user_ids is a list as string
+    #         event_data['to_user_ids'] = ast.literal_eval(event_data['to_user_ids'])
+    #
+    #     logger.debug('Received Box Webhook; event_data=%s' % event_data)
+    #     handle_box_webhook_event.apply_async(args=(event_data,))
+    # except AttributeError:
+    #     pass
+    # except KeyError:
+    #     pass
     return HttpResponse('OK')
