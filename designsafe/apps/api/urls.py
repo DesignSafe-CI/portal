4b21bfd290482d5a9dbefd111c31ec39917ce808
Josue Balandrano Coronel <xirdneh@gmail.com>
2016-04-29 16:55:31 -0500

    Moved views.py outside of a directory. We don't need that here. Moved mixins.py to base so every api can use it

--- designsafe/apps/api/urls.py
+++ designsafe/apps/api/urls.py
@@ -1,8 +1,8 @@
-from django.conf.urls import patterns, url
+from django.conf.urls import patterns, url, include
 from designsafe.apps.api.views import LoggerApi
 
 urlpatterns = patterns(
     'designsafe.apps.api.views',
     url(r'^logger/$', LoggerApi.as_view(), name='logger'),
-
+    url(r'^data/', include('data.urls')),
 )
