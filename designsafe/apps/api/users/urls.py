8e080afc010440dca7f5bb7257b8435966d5e326
Matthew R Hanlon <mrhanlon@tacc.utexas.edu>
2016-06-05 15:48:26 -0500

    fix route name conflict

--- designsafe/apps/api/users/urls.py
+++ designsafe/apps/api/users/urls.py
@@ -3,5 +3,5 @@ from designsafe.apps.api.users.views import SearchView
 
 urlpatterns = patterns(
     'designsafe.apps.api.users.views',
-    url(r'^$', SearchView.as_view(), name='search'),
+    url(r'^$', SearchView.as_view(), name='user_search'),
 )
