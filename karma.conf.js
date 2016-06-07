93b52a36db20f7065929be1f1abc2ad34afa3a60
Matthew R Hanlon <mrhanlon@tacc.utexas.edu>
2016-06-07 16:47:06 -0500

    fix front-end test config

--- karma.conf.js
+++ karma.conf.js
@@ -5,9 +5,43 @@ module.exports = function(config){
 
     files : [
       '/var/www/designsafe-ci.org/static/js/vendor.js',
+
+      '/var/www/designsafe-ci.org/static/vendor/angular/angular.min.js',
+      '/var/www/designsafe-ci.org/static/vendor/angular-sanitize/angular-sanitize.min.js',
+      '/var/www/designsafe-ci.org/static/vendor/angular-toastr/dist/angular-toastr.tpls.min.js',
+      '/var/www/designsafe-ci.org/static/vendor/ngstorage/ngStorage.js',
+
+      '/var/www/designsafe-ci.org/static/vendor/angular-sanitize/angular-sanitize.js',
+      '/var/www/designsafe-ci.org/static/vendor/tv4/tv4.js',
+      '/var/www/designsafe-ci.org/static/vendor/objectpath/lib/ObjectPath.js',
+      '/var/www/designsafe-ci.org/static/vendor/angular-schema-form/dist/schema-form.js',
+      '/var/www/designsafe-ci.org/static/vendor/angular-schema-form/dist/bootstrap-decorator.js',
+
+      '/var/www/designsafe-ci.org/static/vendor/angular-translate/angular-translate.js',
+      '/var/www/designsafe-ci.org/static/vendor/angular-cookies/angular-cookies.js',
+      '/var/www/designsafe-ci.org/static/vendor/angular-bootstrap/ui-bootstrap-tpls.js',
+      '/var/www/designsafe-ci.org/static/vendor/underscore/underscore.js',
+      '/var/www/designsafe-ci.org/static/vendor/filesaver/FileSaver.js',
+
       '/var/www/designsafe-ci.org/static/djng/js/django-angular.js',
-      '/var/www/designsafe-ci.org/static/scripts/**/*.js',
+
+      '/var/www/designsafe-ci.org/static/scripts/utils.js',
+      '/var/www/designsafe-ci.org/static/scripts/navbar.js',
+      '/var/www/designsafe-ci.org/static/scripts/dateinput.js',
+      '/var/www/designsafe-ci.org/static/scripts/ng-logging/**/*.js',
+      '/var/www/designsafe-ci.org/static/scripts/ng-modernizr/**/*.js',
+      '/var/www/designsafe-ci.org/static/scripts/ng-designsafe/**/*.js',
+
+      '/var/www/designsafe-ci.org/static/scripts/logging/logger.js',
+      '/var/www/designsafe-ci.org/static/designsafe/apps/signals/scripts/module.js',
+      '/var/www/designsafe-ci.org/static/designsafe/apps/signals/scripts/provider.js',
+
+      '/var/www/designsafe-ci.org/static/designsafe/apps/notifications/scripts/module.js',
+      '/var/www/designsafe-ci.org/static/designsafe/apps/notifications/scripts/provider.js',
+
       '/var/www/designsafe-ci.org/static/designsafe/apps/workspace/scripts/**/*.js',
+
+      './designsafe/static/vendor/angular-mocks/angular-mocks.js',
       './designsafe/apps/workspace/tests/**/*.js'
     ],
 
