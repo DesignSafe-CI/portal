94056b561c0a1a5f72d8560c539b42b45808c540
Josue Balandrano Coronel <xirdneh@gmail.com>
2016-06-07 20:43:21 -0500

    Adding multiple users to heal_meta command

--- designsafe/apps/data/management/commands/heal_meta.py
+++ designsafe/apps/data/management/commands/heal_meta.py
@@ -29,12 +29,30 @@ class Command(BaseCommand):
                 help="If set it will index the full file object with permissions.", 
                 action='store_true', 
                 default=False)
+        parser.add_argument('-A', '--all-users',
+                help="If set it will loop through all the users in the DB.",
+                action='store_true',
+                default=False)
 
         #parser.add_argument('-t', '--trash', 
         #        help="""If set checks for metadata objects marked as deleted and 
         #                move them to the trash folder.""", 
         #        action='store_true', 
         #        default=False)
+
+    def _get_users(self, *args, **options): 
+        all_users = options['all-users']
+        base_path = options['base_path'] or username
+        if not all_users:
+            if base_path:
+                base_path = base_path.strip('/')
+                #If the agaveclient is created with a super user the 
+                #"optimist permissions" should use the path's username.
+                username = base_path.split('/')[0]
+            return [username]
+        else:
+            um = get_user_model()
+            return [o.username for o in um.objects.all()]
 
     def handle(self, *args, **options):
         system_id = options['system'] or settings.AGAVE_STORAGE_SYSTEM
@@ -46,21 +64,20 @@ class Command(BaseCommand):
         user = get_user_model().objects.get(username = username)
         mgr = FileManager(user)
 
-        if base_path:
-            base_path = base_path.strip('/')
-            #If the agaveclient is created with a super user the 
-            #"optimist permissions" should use the path's username.
-            username = base_path.split('/')[0]
+        usernames = self._get_users(*args, **options)
 
-        elif options['pemsindex']:
-            self.stdout.write('Indexing permissions only.')
-            mgr.index_permissions(system_id, base_path, username, levels = levels, bottom_up = False)
-        elif options['fullindex']:
-            self.stdout.write('Full Indexing.')
-            mgr.index(system_id, base_path, username, full_indexing = True, levels = levels)
-        else:
-            self.stdout.write('Normal Indexing.')
-            mgr.index(system_id, base_path, username, levels = levels)
+        for username in usernames:
+            if options['pemsindex']:
+                self.stdout.write('Indexing permissions only with username: %s.' % username)
+                mgr.indexer.index_permissions(system_id, 
+                                base_path, username, levels = levels, bottom_up = False)
+            elif options['fullindex']:
+                self.stdout.write('Full Indexing with username: %s' % username)
+                mgr.indexer.index(system_id, 
+                                base_path, username, full_indexing = True, levels = levels)
+            else:
+                self.stdout.write('Normal Indexing with username: %s' % username)
+                mgr.indexer.index(system_id, base_path, username, levels = levels)
 
         #if options['trash']:
         #    r, s = Object().search_marked_deleted(system_id, username, base_path)
