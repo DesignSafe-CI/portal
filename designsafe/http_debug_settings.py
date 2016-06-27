eab0904295d271a3cc0ad429b2194718d40e28b2
Matthew R Hanlon <mrhanlon@tacc.utexas.edu>
2016-06-27 11:23:00 -0500

    turn on deprecation warnings when debugging

--- designsafe/http_debug_settings.py
+++ designsafe/http_debug_settings.py
@@ -1,3 +1,9 @@
+import warnings
+
+# Turn on warnings for debugging!
+warnings.simplefilter('module')
+
 from common_settings import *
+
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 SITE_ID = 1
