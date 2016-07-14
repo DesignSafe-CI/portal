79b964476b6a8e98cb07ca60b486e2b018cb0dbc
Josue Balandrano Coronel <xirdneh@gmail.com>
2016-07-14 07:00:02 -0500

    Removing test settings

--- designsafe/elasticsearch_settings.py
+++ designsafe/elasticsearch_settings.py
@@ -1,17 +1,8 @@
-#ELASTIC_SEARCH = {
-#    'cluster': {
-#        'hosts': [
-#            'designsafe-es01.tacc.utexas.edu',
-#            'designsafe-es02.tacc.utexas.edu',
-#        ]
-#    },
-#    'default_index': 'designsafe'
-#}
-
 ELASTIC_SEARCH = {
     'cluster': {
         'hosts': [
-            'localhost',
+            'designsafe-es01.tacc.utexas.edu',
+            'designsafe-es02.tacc.utexas.edu',
         ]
     },
     'default_index': 'designsafe'
