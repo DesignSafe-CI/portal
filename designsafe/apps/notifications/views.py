ea9ec08c2309aa97c1ada803ec91226c73baaeda
Matthew R Hanlon <mrhanlon@tacc.utexas.edu>
2016-08-05 10:12:43 -0500

    fix interactive job notification

--- designsafe/apps/notifications/views.py
+++ designsafe/apps/notifications/views.py
@@ -1,19 +1,14 @@
-# from designsafe.apps.notifications.tasks import send_job_notification
-from designsafe.apps.notifications.apps import Event
-from designsafe.apps.signals.signals import generic_event
 from designsafe.apps.api.notifications.models import Notification
 from designsafe.apps.notifications.models import Notification as LegacyNotification
 from designsafe.apps.signals.signals import generic_event
 
 from django.core import serializers
-# from django.core.serializers.json import DjangoJSONEncoder
-from django.core.urlresolvers import reverse
 from django.http import HttpResponse
-from django.shortcuts import render, redirect
+from django.shortcuts import render
 from django.views.decorators.csrf import csrf_exempt
 from django.views.decorators.http import require_POST
-from agavepy.agave import Agave, AgaveException
-from django.conf import settings
+from agavepy.agave import AgaveException
+from requests import HTTPError
 from django.contrib.auth import get_user_model
 
 from itertools import chain
@@ -24,6 +19,7 @@ import logging
 
 logger = logging.getLogger(__name__)
 
+
 def index(request):
     return render(request, 'designsafe/apps/notifications/index.html')
 
@@ -31,15 +27,8 @@ def index(request):
 @require_POST
 @csrf_exempt
 def generic_webhook_handler(request):
-    if request.method == 'POST':
-        # do stuff
-        event_type = request.POST.get('event_type', None)
-    else:
-        # do other stuff, to be determined in future webhooks I suppose
-        return HttpResponse('Unexpected', status=400)
-
+    event_type = request.POST.get('event_type', None)
     if event_type == 'VNC':
-        event_type = request.POST.get('event_type', '')
         job_owner = request.POST.get('owner', '')
         host = request.POST.get('host', '')
         port = request.POST.get('port','')
@@ -49,25 +38,12 @@ def generic_webhook_handler(request):
             'https://vis.tacc.utexas.edu/no-vnc/vnc.html?' \
             'hostname=%s&port=%s&autoconnect=true&password=%s' % (host, port, password)
 
-        #body = {
-        #    'event_type': event_type,
-        #    'job_owner': job_owner,
-        #    'host': host,
-        #    'port': port,
-        #    'password': password,
-        #    'associationIds': job_uuid,
-        #    'action_link': {
-        #        'value': vnc_connect_link,
-        #        'label': 'Connect',
-        #    }
-        #}
-
         event_data = {
-            Notification.EVENT_TYPE: 'job',
+            Notification.EVENT_TYPE: event_type,
             Notification.STATUS: '',
             Notification.USER: job_owner,
             Notification.MESSAGE: '',
-            Notification.EXTRA:{
+            Notification.EXTRA: {
                 'host': host,
                 'port': port,
                 'password': password,
@@ -75,7 +51,6 @@ def generic_webhook_handler(request):
                 'target_uri': vnc_connect_link
             }
         }
-        #generic_event.send_robust('generic_webhook_handler', event_type=event_type, event_data=body)
         n = Notification.objects.create(**event_data)
         n.save()
 
@@ -83,27 +58,27 @@ def generic_webhook_handler(request):
         try:
             agave_job_meta = {
                 'name': 'interactiveJobDetails',
-                'value': body,
+                'value': event_data,
                 'associationIds': [job_uuid],
             }
             user = get_user_model().objects.get(username=job_owner)
             agave = user.agave_oauth.client
             agave.meta.addMetadata(body=json.dumps(agave_job_meta))
 
-        except AgaveException as e:
+        except (HTTPError, AgaveException) as e:
+            logger.exception('Could not add interactive connection data to metadata')
             return HttpResponse(json.dumps(e.message), content_type='application/json',
-                status=400)
-        except Exception as e:
-            logger.exception('Could not add interactive connection data to metadata {}'.format(e))
+                                status=400)
 
         return HttpResponse('OK')
     else:
         return HttpResponse('Unexpected', status=400)
 
+
 @require_POST
 @csrf_exempt
 def job_notification_handler(request):
-    JOB_EVENT='job'
+    JOB_EVENT = 'job'
     logger.debug('request body: {}'.format(request.body))
 
     try:
@@ -116,7 +91,7 @@ def job_notification_handler(request):
         job_id = request.GET.get('job_id')
         job_owner = notification['owner']
         archive_path = notification['archivePath']
-    except ValueError as e: #for testing ->used when mocking agave notification
+    except ValueError as e:  # for testing ->used when mocking agave notification
         job_name = request.POST.get('job_name')
         status = request.POST.get('status')
         event = request.POST.get('event')
