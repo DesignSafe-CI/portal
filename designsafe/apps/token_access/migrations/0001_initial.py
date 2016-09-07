29418216b9d273447917debb7dd8f13de1f653cb
Matthew R Hanlon <mrhanlon@tacc.utexas.edu>
2016-09-07 10:31:15 -0500

    token_access migrations

---
+++ designsafe/apps/token_access/migrations/0001_initial.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import migrations, models
+from django.conf import settings
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        migrations.swappable_dependency(settings.AUTH_USER_MODEL),
+    ]
+
+    operations = [
+        migrations.CreateModel(
+            name='Token',
+            fields=[
+                ('token', models.CharField(max_length=40, serialize=False, primary_key=True)),
+                ('nickname', models.CharField(max_length=255)),
+                ('created', models.DateTimeField(auto_now_add=True)),
+                ('user', models.ForeignKey(to=settings.AUTH_USER_MODEL)),
+            ],
+        ),
+    ]
