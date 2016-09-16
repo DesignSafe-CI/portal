745700bd8f9d28ce80a00f7113d3de79a9a6831b
Matthew R Hanlon <mrhanlon@tacc.utexas.edu>
2016-09-16 12:57:44 -0500

    flesh out the projects apis

---
+++ designsafe/apps/api/agave/util.py
@@ -0,0 +1,16 @@
+from django.core.serializers.json import DjangoJSONEncoder
+from .agave import BaseAgaveResource
+import logging
+
+logger = logging.getLogger(__name__)
+
+
+class AgaveJSONEncoder(DjangoJSONEncoder):
+
+    def default(self, o):
+        if isinstance(o, BaseAgaveResource):
+            agave_dict = o.__dict__
+            agave_dict.pop('_agave')
+            return agave_dict
+
+        return super(AgaveJSONEncoder, self).default(o)
--- designsafe/apps/api/urls.py
+++ designsafe/apps/api/urls.py
@@ -1,12 +1,11 @@
-from django.conf.urls import patterns, url, include
+from django.conf.urls import url, include
 from designsafe.apps.api.views import LoggerApi
 
-urlpatterns = patterns(
-    'designsafe.apps.api.views',
+urlpatterns = [
     url(r'^data/', include('designsafe.apps.api.data.urls')),
     url(r'^logger/$', LoggerApi.as_view(), name='logger'),
     url(r'^notifications/%', include('designsafe.apps.api.notifications.urls')),
     url(r'^users/', include('designsafe.apps.api.users.urls')),
     url(r'^projects/', include('designsafe.apps.api.projects.urls',
                                namespace='ds_projects_api'))
-)
+]
