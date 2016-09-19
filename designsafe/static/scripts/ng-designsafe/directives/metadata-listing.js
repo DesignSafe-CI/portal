a758ef387a862a62a7d80a6501bd86011920fe0e
Matthew R Hanlon <mrhanlon@tacc.utexas.edu>
2016-09-19 13:43:02 -0500

    update base_angular template with additional libs

---
+++ designsafe/static/scripts/ng-designsafe/directives/metadata-listing.js
@@ -0,0 +1,24 @@
+/**
+ *
+ */
+(function(window, angular) {
+
+  var module = angular.module('ng.designsafe');
+
+  module.directive('metadataListing', function() {
+    return {
+      restrict: 'E',
+      transclude: true,
+      replace: true,
+      templateUrl: '/static/scripts/ng-designsafe/html/directives/metadata-listing.html',
+      scope: {
+        columns: '=',
+        listing: '=',
+        onBrowse: '&',
+        onSelect: '&',
+        onDetail: '&'
+      }
+    };
+  });
+
+})(window, angular);
