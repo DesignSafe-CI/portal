d8c587160adbf1a8ef557984e790980a4c3cc658
Matthew R Hanlon <mrhanlon@tacc.utexas.edu>
2016-09-20 14:49:17 -0500

    data depot states are working, but active states are not

--- designsafe/static/scripts/data-depot/controllers/data-depot-nav.js
+++ designsafe/static/scripts/data-depot/controllers/data-depot-nav.js
@@ -11,24 +11,20 @@
       name: 'Public',
       collapsible: true,
       collapse: true,
-      active: false,
       children: [
         {
           name: 'Publications',
           collapsible: false,
-          active: false,
           state: 'publications'
         },
         {
           name: 'Community Data',
           collapsible: false,
-          active: false,
           state: 'communityData'
         },
         {
           name: 'Training Materials',
           collapsible: false,
-          active: false,
           state: 'trainingMaterials'
         }
       ]
@@ -39,36 +35,30 @@
         name: 'Private',
         collapsible: true,
         collapse: false,
-        active: true,
         children: [
           {
             name: 'My Data',
             collapsible: false,
-            active: true,
             state: 'myData'
           },
           {
             name: 'My Projects',
             collapsible: false,
-            active: false,
-            state: 'myProjects'
+            state: 'projects.list'
           },
           {
             name: 'My Publications',
             collapsible: false,
-            active: false,
             state: 'myPublications'
           },
           {
             name: 'Shared with Me',
             collapsible: false,
-            active: false,
             state: 'sharedData'
           },
           {
             name: 'Box.com',
             collapsible: false,
-            active: false,
             state: 'boxData'
           }
         ]
@@ -77,24 +67,20 @@
           name: 'Workspace',
           collapsible: true,
           collapse: true,
-          active: false,
           children: [
             {
               name: 'Application Catalog',
               collapsible: false,
-              active: false,
               state: 'applicationCatalog'
             },
             {
               name: 'Run Application',
               collapsible: false,
-              active: false,
               state: 'runApplication'
             },
             {
               name: 'Job History',
               collapsible: false,
-              active: false,
               state: 'jobHistory'
             }
           ]
