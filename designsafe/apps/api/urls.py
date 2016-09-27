26c530f8aa3bc3472661637076c030c066756112
Matthew R Hanlon <mrhanlon@tacc.utexas.edu>
2016-09-27 12:55:52 -0500

    route new data views

--- designsafe/apps/api/urls.py
+++ designsafe/apps/api/urls.py
@@ -3,6 +3,7 @@ from designsafe.apps.api.views import LoggerApi
 
 urlpatterns = [
     url(r'^data/', include('designsafe.apps.api.data.urls')),
+    url(r'^data2/', include('designsafe.apps.api.data.urls2')),
     url(r'^logger/$', LoggerApi.as_view(), name='logger'),
     url(r'^notifications/%', include('designsafe.apps.api.notifications.urls')),
     url(r'^users/', include('designsafe.apps.api.users.urls')),
