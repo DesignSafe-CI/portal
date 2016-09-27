60ccaa9b08b236944897680d903a4531364c9b46
Matthew R Hanlon <mrhanlon@tacc.utexas.edu>
2016-09-27 12:53:53 -0500

    new urlpatterns format

--- designsafe/apps/api/data/urls.py
+++ designsafe/apps/api/data/urls.py
@@ -4,20 +4,16 @@ from designsafe.apps.api.data.views import (SourcesView,
                                             DataSearchView,
                                             DataFileManageView)
 
-"""
-The basic url architecture when calling a Data Api should be:
-api/data/<[listing | file | search]>/<resource>/<filepath>
-
-operation: Can be any of these:
-            - listing [GET]. Should return an array of file-like objects.
-            - search [GET]. Should return an array of file-like objects.
-            - download [GET]. Should return a direct url to download the folder/file 
-            - file [POST | PUT | DELETE]. To update or create file/folder. 
-                                 More specific action should live in the body.
-
-"""
-urlpatterns = patterns(
-    'designsafe.apps.api.data.views',
+# The basic url architecture when calling a Data Api should be:
+# api/data/<[listing | file | search]>/<resource>/<filepath>
+#
+# operation: Can be any of these:
+#             - listing [GET]. Should return an array of file-like objects.
+#             - search [GET]. Should return an array of file-like objects.
+#             - download [GET]. Should return a direct url to download the folder/file
+#             - file [POST | PUT | DELETE]. To update or create file/folder.
+#                                  More specific action should live in the body.
+urlpatterns = [
     url(r'^sources/$', SourcesView.as_view(), name='sources'),
 
     url(r'^sources/(?P<source_id>[\w.-]+)/$', SourcesView.as_view(), name='sources'),
@@ -34,4 +30,4 @@ urlpatterns = patterns(
     #     DataDownloadView.as_view(), name='download'),
 
     url(r'^search/(?P<resource>[\w.-]+)/?$', DataSearchView.as_view(), name='search'),
-)
+]
