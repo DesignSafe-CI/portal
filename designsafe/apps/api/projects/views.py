1efc9485cae70c027007acee3a6b6ceeb746c427
Matthew R Hanlon <mrhanlon@tacc.utexas.edu>
2016-10-12 13:36:40 -0500

    refactoring for system-per-project

--- designsafe/apps/api/projects/views.py
+++ designsafe/apps/api/projects/views.py
@@ -1,14 +1,25 @@
+from django.conf import settings
 from django.http import JsonResponse
 from designsafe.apps.api.views import BaseApiView
 from designsafe.apps.api.mixins import SecureMixin
 from designsafe.apps.api.projects.models import Project
 from designsafe.apps.api.agave import get_service_account_client
-from designsafe.apps.api.agave.models.files import BaseFileResource, BaseFilePermissionResource
+from designsafe.apps.api.agave.models.files import BaseFileResource
+from designsafe.apps.api.agave.models.systems import BaseSystemResource
 from designsafe.apps.api.agave.models.util import AgaveJSONEncoder
 import logging
 import json
 
 logger = logging.getLogger(__name__)
+
+
+def template_project_storage_system(project_uuid):
+    system_template = settings.PROJECT_STORAGE_SYSTEM_TEMPLATE.copy()
+    system_template['id'] += project_uuid
+    system_template['name'] += project_uuid
+    system_template['description'] += project_uuid
+    system_template['storage']['rootDir'] += project_uuid
+    return system_template
 
 
 class ProjectCollectionView(BaseApiView, SecureMixin):
@@ -42,30 +53,29 @@ class ProjectCollectionView(BaseApiView, SecureMixin):
         # portal service account needs to create the objects on behalf of the user
         ag = get_service_account_client()
 
-        # create Project (metadata)
         if request.is_ajax():
             post_data = json.loads(request.body)
         else:
             post_data = request.POST.copy()
 
+        # create Project (metadata)
         p = Project(ag)
         p.title = post_data.get('title')
         p.pi = post_data.get('pi', None)
         p.save()
 
-        # grant creating user permissions
-        p.add_collaborator(request.user.username)
-        if p.pi:
-            p.add_collaborator(p.pi)
-
-        # create the data directory File and grant user permissions
+        # create Project Directory on Managed system
         project_storage_root = BaseFileResource(ag, Project.STORAGE_SYSTEM_ID, '/')
-        project_dir = project_storage_root.mkdir(p.uuid)
-        project_dir.share(request.user.username, BaseFilePermissionResource.ALL)
+        project_storage_root.mkdir(p.uuid)
 
-        # relate the Project as metadata for the File
-        p.project_directory = project_dir
-        p.save()
+        # Wrap Project Directory as private system for project
+        project_system_tmpl = template_project_storage_system(p.uuid)
+        ag.systems.add(body=project_system_tmpl)
+
+        # grant initial permissions for creating user and PI, if exists
+        p.add_collaborator(request.user.username)
+        if p.pi and p.pi != request.user.username:
+            p.add_collaborator(p.pi)
 
         return JsonResponse(p, encoder=AgaveJSONEncoder, safe=False)
 
@@ -101,8 +111,8 @@ class ProjectDataView(BaseApiView, SecureMixin):
         :rtype: JsonResponse
         """
         ag = request.user.agave_oauth.client
-
+        p = Project(ag, uuid=project_id)
         list_path = '/'.join([project_id, file_path])
-        listing = BaseFileResource.listing(ag, Project.STORAGE_SYSTEM_ID, list_path)
+        listing = BaseFileResource.listing(ag, p.project_system_id, list_path)
 
         return JsonResponse(listing, encoder=AgaveJSONEncoder, safe=False)
