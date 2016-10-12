f7164e972f0fd3bc75a3ab3f3c8a781ca2084f63
Matthew R Hanlon <mrhanlon@tacc.utexas.edu>
2016-10-12 17:03:10 -0500

    normalize how models are serialized

--- designsafe/apps/api/users/views.py
+++ designsafe/apps/api/users/views.py
@@ -12,12 +12,13 @@ logger = logging.getLogger(__name__)
 class SearchView(SecureMixin, View):
 
     def get(self, request):
+        resp_fields = ['first_name', 'last_name', 'email', 'username']
 
         model = get_user_model()
         q = request.GET.get('username')
         if q:
             user = model.objects.get(username=q)
-            return JsonResponse(model_to_dict(user))
+            return JsonResponse(model_to_dict(user, fields=resp_fields))
 
         q = request.GET.get('q')
 
@@ -27,12 +28,7 @@ class SearchView(SecureMixin, View):
                 return JsonResponse({})
 
             user_rs = model.objects.filter(query)
-            resp = [{
-                        'first_name': u.first_name,
-                        'last_name': u.last_name,
-                        'email': u.email,
-                        'username': u.username
-                    } for u in user_rs]
+            resp = [model_to_dict(u, fields=resp_fields) for u in user_rs]
             return JsonResponse(resp, safe=False)
 
         return HttpResponseNotFound()
