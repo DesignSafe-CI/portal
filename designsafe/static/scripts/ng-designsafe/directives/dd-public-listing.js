dd844898aff640fa403c6e410e11336ae845d7b5
Josue Balandrano Coronel <xirdneh@gmail.com>
2016-10-20 13:18:19 -0500

    Adding public data listing directive

---
+++ designsafe/static/scripts/ng-designsafe/directives/dd-public-listing.js
@@ -0,0 +1,24 @@
+/**
+ *
+ */
+(function(window, angular) {
+
+  var module = angular.module('ng.designsafe');
+
+  module.directive('ddPublicListing', function() {
+    return {
+      restrict: 'E',
+      transclude: true,
+      replace: true,
+      templateUrl: '/static/scripts/ng-designsafe/html/directives/dd-public-listing.html',
+      scope: {
+        browser: '=',
+        onBrowse: '&',
+        onSelect: '&',
+        onDetail: '&',
+        renderPath: '&'
+      }
+    };
+  });
+
+})(window, angular);
