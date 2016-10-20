22619f7e5eb7648c5ef134492d01f43d32cd4a78
Josue Balandrano Coronel <xirdneh@gmail.com>
2016-10-20 13:22:11 -0500

    ADding public data endpoints

--- designsafe/apps/api/urls.py
+++ designsafe/apps/api/urls.py
@@ -3,6 +3,7 @@ from designsafe.apps.api.views import LoggerApi
 
 urlpatterns = [
     url(r'^agave/', include('designsafe.apps.api.agave.urls')),
+    url(r'^public/', include('designsafe.apps.api.agave.urls')),
     url(r'^data/', include('designsafe.apps.api.data.urls')),
     url(r'^logger/$', LoggerApi.as_view(), name='logger'),
     url(r'^notifications/%', include('designsafe.apps.api.notifications.urls')),
