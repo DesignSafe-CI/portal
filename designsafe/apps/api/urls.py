f4ac5e0830519af18a5a7f70c02a0aea2856108b
Josue Balandrano Coronel <xirdneh@gmail.com>
2016-11-04 18:53:48 -0500

    Adding external resources url

--- designsafe/apps/api/urls.py
+++ designsafe/apps/api/urls.py
@@ -1,13 +1,16 @@
+# pylint: disable=missing-docstring
 from django.conf.urls import url, include
 from designsafe.apps.api.views import LoggerApi
 
 urlpatterns = [
     url(r'^agave/', include('designsafe.apps.api.agave.urls')),
     url(r'^public/', include('designsafe.apps.api.public_data.urls')),
+    url(r'^projects/', include('designsafe.apps.api.projects.urls',
+                               namespace='ds_projects_api')),
+    url(r'^external-resources/', include('designsafe.apps.api.external_resources.urls')),
+
     url(r'^data/', include('designsafe.apps.api.data.urls')),
     url(r'^logger/$', LoggerApi.as_view(), name='logger'),
     url(r'^notifications/%', include('designsafe.apps.api.notifications.urls')),
     url(r'^users/', include('designsafe.apps.api.users.urls')),
-    url(r'^projects/', include('designsafe.apps.api.projects.urls',
-                               namespace='ds_projects_api'))
 ]
