d87949fd694b84bce94e472ace6439267e7848af
Josue Balandrano Coronel <xirdneh@gmail.com>
2016-11-05 11:42:09 -0500

    Adding correct api params overriding in children listing and mapping public urls correctly

--- designsafe/apps/api/public_data/urls.py
+++ designsafe/apps/api/public_data/urls.py
@@ -1,6 +1,10 @@
 from django.conf.urls import url
 from designsafe.apps.api.public_data.views import (PublicDataListView,
                                                    PublicMediaView)
+from designsafe.apps.api.agave.views import (FileSearchView,
+                                             FileMediaView,
+                                             FilePermissionsView,
+                                             SystemsView)
 
 urlpatterns = [
     # Browsing:
@@ -15,4 +19,34 @@ urlpatterns = [
     url(r'^files/listing/(?P<file_mgr_name>[\w.-]+)/(?P<system_id>[\w.-]+)/$',
         PublicDataListView.as_view(),
         name='public_data_listing'),
+
+    # Search operations:
+    #
+    #     GET     /search/<file_mgr_name>/
+    #     POST    /search/<file_mgr_name>/
+    url(r'^files/search/(?P<file_mgr_name>[\w.-]+)/$',
+        FileSearchView.as_view(), name='files_search'),
+
+    # File operations:
+    #
+    #     GET     /media/<file_mgr_name>/<system_id>/<file_path>/
+    #     POST    /media/<file_mgr_name>/<system_id>/<file_path>/
+    #     PUT     /media/<file_mgr_name>/<system_id>/<file_path>/
+    #     DELETE  /media/<file_mgr_name>/<system_id>/<file_path>/
+    url(r'^files/media/(?P<file_mgr_name>[\w.-]+)/(?P<system_id>[\w.-]+)/(?P<file_path>[ \S]+)$',
+        FileMediaView.as_view(), name='files_media'),
+
+
+    # Permission operations:
+    #
+    #     GET     /pems/<file_mgr_name>/<system_id>/<file_path>/
+    #     POST    /pems/<file_mgr_name>/<system_id>/<file_path>/
+    #     DELETE  /pems/<file_mgr_name>/<system_id>/<file_path>/
+    url(r'^files/pems/(?P<file_mgr_name>[\w.-]+)/(?P<system_id>[\w.-]+)/(?P<file_path>[ \S]+)$',
+        FilePermissionsView.as_view(), name='files_pems'),
+
+
+    # Systems
+    url(r'^systems/$', SystemsView.as_view(), name='systems'),
+    url(r'^systems/(?P<system_id>[\w.-]+)/$', SystemsView.as_view(), name='systems'),
 ]
