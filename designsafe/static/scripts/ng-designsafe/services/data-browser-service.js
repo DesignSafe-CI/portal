aed310504417bc42145b10fb8baf71037867f792
Josue Balandrano Coronel <xirdneh@gmail.com>
2017-01-20 09:10:36 -0600

    Fixing metadata rendering bug in public data

--- designsafe/static/scripts/ng-designsafe/services/data-browser-service.js
+++ designsafe/static/scripts/ng-designsafe/services/data-browser-service.js
@@ -1227,7 +1227,7 @@
               typeof listing.metadata !== 'undefined' &&
               !_.isEmpty(listing.metadata.project)){
             var _listing = angular.copy(listing);
-            $scope.file.metadata = _listing.metadata;
+            $scope.data.file.metadata = _listing.metadata;
           }else{
             file.getMeta().then(function(file){
               $scope.ui.busy = false;
