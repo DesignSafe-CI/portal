2d2c8fc182af03210cee7ed1b521ab883d187147
Josue Balandrano Coronel <xirdneh@gmail.com>
2017-02-09 00:39:54 -0600

    Loading agave metadata models on init

--- designsafe/apps/api/models.py
+++ designsafe/apps/api/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+from designsafe.apps.api.agave.models.fields import (CharField, UuidField, DateTimeField,
+                                                     IntField, DecimalField, ListField,
+                                                     NestedObjectField, RelatedObjectField)
+from designsafe.apps.api.projects.models import (ExperimentalProject, FileModel,
+                                                 Experiment, ModelConfiguration)
+
+from designsafe.apps.api.agave.models.base import set_lazy_rels
+set_lazy_rels()
