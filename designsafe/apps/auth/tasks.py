040c62d7d3c4d46c09eb9acb34598749972677e1
Josue Balandrano Coronel <xirdneh@gmail.com>
2017-02-14 14:14:59 -0600

    Creating home only if user was created today

--- designsafe/apps/auth/tasks.py
+++ designsafe/apps/auth/tasks.py
@@ -3,6 +3,8 @@ from agavepy.agave import Agave, AgaveException
 from dsapi.agave.daos import FileManager
 from celery import shared_task
 from requests import HTTPError
+from django.contrib.auth import get_user_model
+import datetime
 import logging
 import json
 
@@ -16,15 +18,18 @@ def check_or_create_agave_home_dir(username):
         username, settings.AGAVE_STORAGE_SYSTEM))
     try:
         # TODO should use DS Files API for this
-        ag = Agave(api_server=settings.AGAVE_TENANT_BASEURL,
-                   token=settings.AGAVE_SUPER_TOKEN)
-        body = {'action': 'mkdir', 'path': username}
-        ag.files.manage(systemId=settings.AGAVE_STORAGE_SYSTEM,
-                        filePath='', body=body)
+        user = get_user_model().objects.get(username=username)
+        if datetime.datetime.now().date() == user.date_joined.date():
+            ag = Agave(api_server=settings.AGAVE_TENANT_BASEURL,
+                       token=settings.AGAVE_SUPER_TOKEN)
+            body = {'action': 'mkdir', 'path': username}
+            ag.files.manage(systemId=settings.AGAVE_STORAGE_SYSTEM,
+                            filePath='', body=body)
 
-        # add dir to index
-        fm = FileManager(agave_client=ag)
-        fm.index(settings.AGAVE_STORAGE_SYSTEM, username, username, levels=1)
+            # add dir to index
+            fm = FileManager(agave_client=ag)
+            fm.index(settings.AGAVE_STORAGE_SYSTEM, username, username, levels=1)
+
     except (HTTPError, AgaveException):
         logger.exception('Failed to create home directory.',
                          extra={'user': username,
