137abf42c01f915f64a180398610eccc1d768f6a
Josue Balandrano Coronel <xirdneh@gmail.com>
2017-03-17 16:20:53 -0500

    Importing * when initializing django app to do ORM stuff

--- designsafe/apps/api/models.py
+++ designsafe/apps/api/models.py
@@ -1,12 +1,8 @@
 from django.db import models
 
 # Create your models here.
-from designsafe.apps.api.agave.models.fields import (CharField, UuidField, DateTimeField,
-                                                     IntField, DecimalField, ListField,
-                                                     NestedObjectField, RelatedObjectField)
-from designsafe.apps.api.projects.models import (ExperimentalProject, FileModel,
-                                                 Experiment, ModelConfiguration,
-                                                 Event, Analysis, SensorList)
+from designsafe.apps.api.agave.models.fields import *
+from designsafe.apps.api.projects.models import *
 
 from designsafe.apps.api.agave.models.base import set_lazy_rels
 set_lazy_rels()
