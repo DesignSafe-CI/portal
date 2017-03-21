53e05f2e5fad6a3416b7cc89c382e78051890ae8
Josue Balandrano Coronel <xirdneh@gmail.com>
2017-03-21 12:52:55 -0500

    Adding rabbitmq in celery config

--- designsafe/celery_settings.py
+++ designsafe/celery_settings.py
@@ -1,11 +1,33 @@
-###
+"""
 # Celery settings
-#
-BROKER_URL = 'redis://redis:6379/0'
-#BROKER_URL = 'amqp://designsafe:8WEESynT82y73qhG@rabbitmq:5672//'
+"""
+import os
+#BROKER_URL = 'redis://redis:6379/0'
+BROKER_URL_PROTOCOL = os.environ.get('BROKER_URL_PROTOCOL')
+BROKER_URL_USERNAME = os.environ.get('BROKER_URL_USERNAME')
+BROKER_URL_PWD = os.environ.get('BROKER_URL_PWD')
+BROKER_URL_HOST = os.environ.get('BROKER_URL_HOST')
+BROKER_URL_PORT = os.environ.get('BROKER_URL_PORT')
+BROKER_URL_VHOST = os.environ.get('BROKER_URL_VHOST')
+BROKER_URL = ''.join([BROKER_URL_PROTOCOL, BROKER_URL_USERNAME, ':',
+                      BROKER_URL_PWD, '@', BROKER_URL_HOST, ':',
+                      BROKER_URL_PORT, '/', BROKER_URL_VHOST])
+#BROKER_URL = 'amqp://designsafe:pwd@rabbitmq:5672//'
+CELERY_RESULT_BACKEND_PROTOCOL = os.environ.get('CELERY_RESULT_BACKEND_PROTOCOL')
+CELERY_RESULT_BACKEND_USERNAME = os.environ.get('CELERY_RESULT_BACKEND_USERNAME')
+CELERY_RESULT_BACKEND_PWD = os.environ.get('CELERY_RESULT_BACKEND_PWD')
+CELERY_RESULT_BACKEND_HOST = os.environ.get('CELERY_RESULT_BACKEND_HOST')
+CELERY_RESULT_BACKEND_PORT = os.environ.get('CELERY_RESULT_BACKEND_PORT')
+CELERY_RESULT_BACKEND_DB = os.environ.get('CELERY_RESULT_BACKEND_DB')
+CELERY_RESULT_BACKEND = ''.join([CELERY_RESULT_BACKEND_PROTOCOL, CELERY_RESULT_BACKEND_USERNAME,
+                                 ':', CELERY_RESULT_BACKEND_PWD, '@',
+                                 CELERY_RESULT_BACKEND_HOST, ':', CELERY_RESULT_BACKEND_PORT,
+                                 '/', CELERY_RESULT_BACKEND_DB])
+
 CELERY_ACCEPT_CONTENT = ['json']
 CELERY_TASK_SERIALIZER = 'json'
 CELERY_RESULT_SERIALIZER = 'json'
 CELERYD_HIJACK_ROOT_LOGGER = False
-# CELERYD_LOG_FORMAT ='[DJANGO] $(processName)s %(levelname)s %(asctime)s %(module)s %(name)s.%(funcName)s:%(lineno)s: [%(task_name)s(%(task_id)s)]%(message)s'
+CELERYD_LOG_FORMAT = '[DJANGO] $(processName)s %(levelname)s %(asctime)s %(module)s '\
+                     '%(name)s.%(funcName)s:%(lineno)s: [%(task_name)s(%(task_id)s)]%(message)s'
 #CELERY_ANOTATIONS = {'designsafe.apps.api.tasks.reindex_agave': {'time_limit': 60 * 15}}
