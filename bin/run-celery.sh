7c26091a4d7f5414fe87e89ea3b5e841e0f9290b
Josue Balandrano Coronel <xirdneh@gmail.com>
2017-03-23 15:52:29 -0500

    Updating how we run celery workers

--- bin/run-celery.sh
+++ bin/run-celery.sh
@@ -2,4 +2,5 @@
 
 # Run Celery as the DesignSafe Community Account
 su tg458981 -c "celery -A designsafe beat -l info --pidfile= --schedule=/tmp/celerybeat-schedule" &
-su tg458981 -c "celery -A designsafe worker -l info --autoscale=10,2"
+su tg458981 -c "celery -A designsafe worker -l info --autoscale=6,3 --concurrency=4"
+su tg458981 -c "celery -A designsafe worker -l info --autoscale=6,3 --concurrenty=4"
