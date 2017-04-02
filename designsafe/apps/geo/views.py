030e20685b1e82483ec90465623880d5f4d8e55c
Joe Meiring <jmeiring@tacc.utexas.edu>
2017-04-02 10:12:56 -0500

    login_required added to routes

--- designsafe/apps/geo/views.py
+++ designsafe/apps/geo/views.py
@@ -3,6 +3,8 @@ from django.http import HttpResponse
 from django.shortcuts import render
 from requests import HTTPError
 from django.contrib.auth import get_user_model
+from django.contrib.auth.decorators import login_required
+
 
 
 import json
@@ -11,9 +13,10 @@ import logging
 
 logger = logging.getLogger(__name__)
 
-
+@login_required
 def index(request):
     return render(request, 'designsafe/apps/geo/index.html')
 
+@login_required
 def test(request):
     return render(request, 'designsafe/apps/geo/test.html')
