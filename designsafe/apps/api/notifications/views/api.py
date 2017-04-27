40abe5714dafabf605dc0cb4085db382dc671407
Marjo Poindexter <mlm55@users.noreply.github.com>
2017-04-27 17:03:33 -0500

    add paging to notifications

--- designsafe/apps/api/notifications/views/api.py
+++ designsafe/apps/api/notifications/views/api.py
@@ -18,22 +18,34 @@ logger = logging.getLogger(__name__)
 class ManageNotificationsView(SecureMixin, JSONResponseMixin, BaseApiView):
 
     def get(self, request, event_type = None, *args, **kwargs):
-        limit = request.GET.get('limit', None)
+        limit = request.GET.get('limit', 0)
+        page = request.GET.get('page', 0)
+
         if event_type is not None:
             notifs = Notification.objects.filter(event_type = event_type,
                           deleted = False,
                           user = request.user.username).order_by('-datetime')
+            total = Notification.objects.filter(event_type = event_type,
+                          deleted = False,
+                          user = request.user.username).count()
         else:
             notifs = Notification.objects.filter(deleted = False,
                           user = request.user.username).order_by('-datetime')
+            total = Notification.objects.filter(deleted = False,
+                          user = request.user.username).count()
         if limit:
-            notifs = notifs[0:limit]
+            limit = int(limit)
+            page = int(page)
+            offset = page * limit
+            notifs = notifs[offset:offset+limit]
+
         for n in notifs:
             if not n.read:
                 n.mark_read()
 
         notifs = [n.to_dict() for n in notifs]
-        return self.render_to_json_response(notifs)
+        return self.render_to_json_response({'notifs':notifs, 'page':page, 'total': total})
+        # return self.render_to_json_response(notifs)
 
     def post(self, request, *args, **kwargs):
         body_json = json.loads(request.body)
