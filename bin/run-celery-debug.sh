575691cb0e524759b947a0bf2d34f1b5f5fb2cf3
Josue Balandrano Coronel <xirdneh@gmail.com>
2017-09-06 15:24:05 -0500

    Removing --autoreload since it's invalid on newer Celery versions

--- bin/run-celery-debug.sh
+++ bin/run-celery-debug.sh
@@ -4,4 +4,4 @@
 # Run Celery task queue for development
 #
 su tg458981 -c "celery -A designsafe beat -l info --pidfile= --schedule=/tmp/celerybeat-schedule" &
-su tg458981 -c "celery -A designsafe worker -l debug --autoreload"
+su tg458981 -c "celery -A designsafe worker -l debug"
