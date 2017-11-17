87d119c4a92aadc85a5d86d3616e1072987a47e8
Josue Balandrano Coronel <xirdneh@gmail.com>
2017-11-17 16:13:44 -0600

    Adding some extra fields just in case

--- designsafe/apps/rapid/models.py
+++ designsafe/apps/rapid/models.py
@@ -1,35 +1,60 @@
+from django.conf import settings
 from datetime import datetime
-from elasticsearch_dsl import DocType, String, Date, Nested, Boolean, GeoPoint, MetaField
+from elasticsearch_dsl import (DocType, String, Date, Nested,
+                               Boolean, GeoPoint, MetaField, Text,
+                               Keyword)
 from designsafe.connections import connections
 
 class RapidNHEventType(DocType):
-
     class Meta:
-        index = 'designsafe'
+        index = settings.ES_INDICES['rapid']['name']
         dynamic = MetaField('false')
 
-    name = String()
-    display_name = String()
+    display_name = String(fields={
+        '_exact': Keyword()
+    })
+    name = String(fields={
+        '_exact': Keyword()
+    })
 
 
 class RapidNHEvent(DocType):
     class Meta:
-        index = 'designsafe'
+        index = settings.ES_INDICES['rapid']['name']
         dynamic = MetaField('false')
 
     event_date = Date()
     created_date = Date()
-    title = String()
-    event_type = String()
-    location_description = String()
+    title = Text(analyzer='english')
+    event_type = String(fields={
+                '_exact': Keyword()
+            })
+    location_description = Text(
+        analyzer='english',
+        fields={
+            '_exact': Keyword()
+        })
     location = GeoPoint()
-    main_image_uuid = String()
+    main_image_url = String(fields={
+        '_exact': Keyword()
+    })
+    main_image_uuid = String(fields={
+        '_exact': Keyword()
+    })
     datasets = Nested(
         properties={
-            "id": String(),
-            "title": String(),
-            "doi": String(),
-            "url": String(),
+            "id": String(fields={
+                '_exact': Keyword()
+            }),
+            "title": String(fields={
+                '_exact': Keyword()
+            }),
+            "doi": String(fields={
+                '_exact': Keyword()
+            }),
+            "url": String(fields={
+                '_exact': Keyword()
+            }),
         })
 
     def save(self, **kwargs):
