0cc1aaac78cc3e5fa7da68feb7e7e69e5658cd54
Josue Balandrano Coronel <xirdneh@gmail.com>
2017-11-17 16:14:30 -0600

    Trying out new es config format

--- designsafe/settings/elasticsearch_settings.py
+++ designsafe/settings/elasticsearch_settings.py
@@ -1,6 +1,66 @@
 import os
 
 """Elastic search connection configuration"""
+ES_CONNECTIONS = {
+    'default': {
+        'hosts': [
+            'designsafe-es01.tacc.utexas.edu',
+            'designsafe-es02.tacc.utexas.edu',
+        ],
+    },
+    'stagig': { #dev/qa
+        'hosts':  [
+            'designsafe-es01-dev.tacc.utexas.edu',
+        ]
+    },
+    'dev': {
+        'hosts': [
+            'es_designsafe'
+        ]
+    },
+    'localhost': {
+        'hosts': [
+            'localhost'
+        ]
+    }
+}
+
+ES_INDICES = {
+    'files': {
+        'name': 'designsafe_a',
+        'alias': ['designsafe'],
+        'documents': [{'name': 'objects',
+                       'class': 'designsafe.apps.data.models.IndexedFile'}]
+    },
+    'rapid': {
+        'name': 'rapid_nh_a',
+        'alias': ['rapid_nh'],
+        'documents': [{'name': 'event',
+                       'class': 'designsafe.apps.rapid.models.RapidNHEvent'},
+                      {'name': 'eventType',
+                       'class': 'designsafe.apps.rapid.models.RapidNHEventType'}]
+    },
+    'publications': {
+        'name': 'publications_a',
+        'alias': ['publications'],
+        'documents': [{'name': 'publication',
+                       'class': 'designsafe.apps.data.models.IndexedPublication'}]
+    },
+    'web_content': {
+        'name': 'web_content_a',
+        'alias': ['web_content'],
+        'documents': [{'name': 'page',
+                       'class': 'designsafe.apps.data.models.IndexedCMSPage'}]
+    },
+    'publications_legacy': {
+        'name': 'publications_legacy_a',
+        'alias': ['publications_legacy'],
+        'documents': [{'name': 'publication',
+                       'class': 'designsafe.apps.data.models.IndexPublicationLegacy'}]
+    }
+}
+
+"""
 if (os.environ.get('DESIGNSAFE_ENVIRONMENT', 'dev').lower() == 'prod'):
     ELASTIC_SEARCH = {
         'cluster': {
@@ -55,3 +115,4 @@ else:
             'INDEX_NAME': 'cms',
         }
     }
+"""
