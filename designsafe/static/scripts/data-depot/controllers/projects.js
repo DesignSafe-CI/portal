82fcd218302865f76ea58a97a940e73f29b3dd09
Matthew Shirley <mshirley@wireless-10-146-199-131.public.utexas.edu>
2017-11-17 16:55:03 -0600

    add functionality to highlight projects on myprojects page in accordance with DES-24

--- designsafe/static/scripts/data-depot/controllers/projects.js
+++ designsafe/static/scripts/data-depot/controllers/projects.js
@@ -50,7 +50,7 @@
     });
   }]);
 
-  app.controller('ProjectListingCtrl', ['$scope', '$state', 'Django', 'ProjectService', function ($scope, $state, Django, ProjectService) {
+  app.controller('ProjectListingCtrl', ['$scope', '$state', 'DataBrowserService', 'Django', 'ProjectService', function ($scope, $state, DataBrowserService, Django, ProjectService) {
     $scope.ui = {};
     $scope.ui.busy = true;
     $scope.data.projects = [];
@@ -64,6 +64,29 @@
       $state.go('projects.view.data', {projectId: project.uuid,
                                        filePath: '/',
                                        projectTitle: project.value.title});
+    };
+
+    // function for when the row is selected, but the link to the project detail page is not
+    $scope.onSelect = function($event, project) {
+      var selectedProjects = DataBrowserService.state().selectedProjects;
+      // holding ctrl key should toggle selected project but leave other projects unchanged
+      if ($event.ctrlKey || $event.metaKey) {
+        DataBrowserService.toggleProjects(project);
+      }
+      // shift key should select all projects between the last clicked project and the current clicked project
+      else if ($event.shiftKey && selectedProjects.length > 0) {
+        // get
+        var lastProject = selectedProjects[selectedProjects.length - 1];
+        var lastIndex = $scope.data.projects.indexOf(lastProject);
+        var projectIndex = $scope.data.projects.indexOf(project);
+        var min = Math.min(lastIndex, projectIndex);
+        var max = Math.max(lastIndex, projectIndex);
+        DataBrowserService.selectProjects($scope.data.projects.slice(min, max + 1));
+      }
+      // else no special scenario. we toggle the clicked project and unselect all others.
+      else {
+        DataBrowserService.toggleProjects(project, true);
+      }
     };
 
   }]);
