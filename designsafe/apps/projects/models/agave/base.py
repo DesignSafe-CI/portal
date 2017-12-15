ae8d684b92eb9bb15403ef6fd4b17cecc0ceb7f5
Josue Balandrano Coronel <xirdneh@gmail.com>
2017-12-14 18:39:35 -0600

    Updating base project class

--- designsafe/apps/projects/models/agave/base.py
+++ designsafe/apps/projects/models/agave/base.py
@@ -28,6 +28,18 @@ class Project(MetadataModel):
     ef = fields.CharField('Experimental Facility', max_length=512)
     keywords = fields.CharField('Keywords')
 
+    @property
+    def system(self):
+        return 'project-{uuid}'.format(uuid=self.uuid)
+
+    @property
+    def collaborators(self):
+        return {
+            'pi': self.pi,
+            'coPis': self.co_pis,
+            'teamMembers': self.team_members
+        }
+
     def to_body_dict(self):
         body_dict = super(Project, self).to_body_dict()
         body_dict['_related'] = {}
@@ -39,3 +51,11 @@ class Project(MetadataModel):
             body_dict['_related'][attrname] = field.related_obj_name
 
         return body_dict
+
+    def add_team_members(self, usernames):
+        for username in usernames:
+            self.set_pem(username, 'ALL')
+            self.manager().agave_client.systems.updateRole(
+                systemId=self.system,
+                body={'username': username, 'role': 'USER'})
+        return self
