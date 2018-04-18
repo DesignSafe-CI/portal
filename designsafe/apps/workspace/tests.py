2f8d8be7b02ed073549ef81e1911fe093e2e6244
Jake Rosenberg <jrosenberg@tacc.utexas.edu>
2018-04-18 14:33:11 -0500

    Removed placeholder test from workspace.

--- designsafe/apps/workspace/tests.py
+++ designsafe/apps/workspace/tests.py
@@ -1,7 +1,3 @@
 from django.test import TestCase
-from django.test import SimpleTestCase
 
 # Create your tests here.
-class WorkspaceTestCase(SimpleTestCase):
-    def test_tests_are_working(self):
-        self.assertEqual(True, False)
