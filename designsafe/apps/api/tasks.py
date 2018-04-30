8bcdd09401def7b3587f7dcd2cc69d9ebfbfbb6f
Jake Rosenberg <jrosenberg@tacc.utexas.edu>
2018-04-30 10:07:19 -0500

    Elasticsearch indexer works in local dev.

--- designsafe/apps/api/tasks.py
+++ designsafe/apps/api/tasks.py
@@ -22,9 +22,16 @@ def reindex_agave(self, username, file_id, full_indexing=True,
                   levels=1, pems_indexing=True, index_full_path=True):
     user = get_user_model().objects.get(username=username)
     #levels=1
-
+    
     from designsafe.apps.api.data import AgaveFileManager
+    from designsafe.apps.data.managers.indexer import AgaveIndexer as AgaveFileIndexer
     agave_fm = AgaveFileManager(user)
+    
+    if settings.DEBUG and username == 'ds_admin':
+        service_client = get_service_account_client()
+        agave_fm.agave_client = service_client
+        agave_fm.indexer = AgaveFileIndexer(agave_client=service_client)
+
     system_id, file_user, file_path = agave_fm.parse_file_id(file_id)
     if system_id != settings.AGAVE_STORAGE_SYSTEM:
         file_id_comps = file_id.strip('/').split('/')
