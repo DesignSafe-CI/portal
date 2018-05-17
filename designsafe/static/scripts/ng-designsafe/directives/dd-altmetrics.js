9e79aede61003be48d137efe0897c0c9501ace5f
Josue Balandrano Coronel <xirdneh@gmail.com>
2018-05-17 14:58:05 -0500

    The json+ld now shows data for the current project.  The data is passed to the directive through the dd-altmetrics tag, and the $watch function now tracks the state of the page.

--- designsafe/static/scripts/ng-designsafe/directives/dd-altmetrics.js
+++ designsafe/static/scripts/ng-designsafe/directives/dd-altmetrics.js
@@ -2,13 +2,46 @@
 
   var module = angular.module('designsafe');
 
-  module.directive('ddAltmetrics', ['$sce', '$filter', function ($sce, $filter, $scope) {
+  module.directive('ddAltmetrics', ['$sce','$filter',function ($sce,$filter,$scope) {
     return {
       restrict: 'EA',
+      scope: { project: '='},
       link: function (scope, element) {
-        scope.$watch('ld', function (schema) {
-          var schema = $sce.trustAsHtml($filter('json')(schema));
-          element[0].outerHTML = '<script type="application/ld+json">' + schema + '</script>';
+        scope.$watch(function stateWatch(){return scope.project;}, function(newState) {
+          if (typeof newState === 'undefined'){
+            return;
+          }
+          var publication = newState;
+          var ld = {
+            "@context": "http://schema.org",
+            "@type": "Dataset",
+            "@id": "https://doi.org/" + publication.doi, //dataset doi url 
+            "additionalType": "Project/Experimental", //dataset type
+            "name": publication.value.title, //dataset name
+            "alternateName": publication.value.projectId, //alternative name of the dataset
+            "author": [
+              {
+                "@type": "Person",
+                "name": '',//publication.user.first_name + ' ' + publication.user.last_name,
+                "givenName": '',//publication.user.first_name,
+                "familyName": '',//publication.user.last_name
+              }],
+            "description": publication.value.description,
+            "license": "http://opendatacommons.org/licenses/by/1-0",
+            "keywords": publication.value.keywords,
+            "inLanguage": "English",
+            "datePublished": $filter('date')(publication.created, 'MMM/d/yyyy'),
+            "schemaVersion": "http://datacite.org/schema/kernel-4",
+            "publisher": {
+              "@type": "Organization",
+              "name": "Designsafe-CI"
+            },
+            "provider": {
+              "@type": "Organization",
+              "name": "TACC"
+            }
+          };
+          element[0].outerHTML = '<script type="application/ld+json">' + JSON.stringify(ld) + '</script>';
         });
       }
     };
