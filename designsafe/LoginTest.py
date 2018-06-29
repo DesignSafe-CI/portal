e0e60a08833fc129878744aad5c4094a772db3f4
Frank Netscher <fnetscher@wireless-10-146-254-87.public.utexas.edu>
2018-06-29 13:34:30 -0500

    Started building request mocks.

--- designsafe/LoginTest.py
+++ designsafe/LoginTest.py
@@ -1,5 +1,7 @@
+#python manage.py test designsafe.LoginTest --settings=designsafe.settings.test_settings
 from django.test import TestCase, RequestFactory
 from django.contrib.auth import get_user_model
+from django.core.urlresolvers import reverse
 import mock
 from designsafe.apps.auth.models import AgaveOAuthToken
 from designsafe.apps.auth.tasks import check_or_create_agave_home_dir
@@ -18,20 +20,21 @@ class LoginTestClass(TestCase):
         refresh_token="123123123",
         expires_in=14400,
         created=1523633447)
-    token.user = user
+    token.user = user_with_agave
     token.save()
+    self.rf = RequestFactory()
 
     user_without_agave = User.objects.create_user(
-        'test_without_agave', 'test@test.com', 'test')
-    token = AgaveOAuthToken(
+        'test_without_agave', 'test2@test.com', 'test')
+    """ token = AgaveOAuthToken(
         token_type="bearer",
         scope="default",
-        access_token="1234abcd",
-        refresh_token="123123123",
+        access_token="5555abcd",
+        refresh_token="5555555",
         expires_in=14400,
         created=1523633447)
-    token.user = user
-    token.save()
+    token.user = user_without_agave
+    token.save() """
     
   
   def tearDown(self):
@@ -39,7 +42,8 @@ class LoginTestClass(TestCase):
     
   """ @mock.patch('designsafe.apps.auth.models.AgaveOAuthToken.client')
   @mock.patch('agavepy.agave.Agave')
-  def test_has_agave_file_listing(self, agave_client, agave):
+  @mock.patch('designsafe.apps.auth.views.agave_oauth_callback')
+  def test_has_agave_file_listing(self, agave_client, agave, agave_oauth_callback):
     #agaveclient.return_value.files.list.return_value = [] // whatever the listing should look like
     #request.post.return_value = {} // some object that looks like a requests response
     
@@ -71,6 +75,16 @@ class LoginTestClass(TestCase):
         }
     } 
     
+    request_with_agave = self.rf.get(
+        reverse('designsafe_auth:agave_oauth_callback'))
+    request_with_agave.POST.return_value = {
+        'state': 'test',
+        'session': {'auth_state': 'test'},
+        'code': 'test'
+        }
+
+    agave_oauth_callback(request_with_agave.POST)
+
     resp = self.client.get('https://agave.designsafe-ci.org/files/v2/listings/designsafe.storage.default/test')
     print resp 
     self.assertEqual(resp.status_code, 200) """
@@ -85,9 +99,16 @@ class LoginTestClass(TestCase):
           "message": "File/folder does not exist", 
           "version": "test"
           }
+      
+      request_without_agave = self.rf.get(
+        reverse('designsafe_auth:agave_oauth_callback'))
+      request_without_agave.get.return_value = {
+        'state': 'test',
+        'session': {'auth_state': 'test'},
+        'code': 'test'
+        }
 
-      resp = self.client.get(
-          'https://agave.designsafe-ci.org/files/v2/listings/designsafe.storage.default//test')
+      resp = agave_oauth_callback(request_without_agave.POST)
       print resp
       self.assertEqual(resp.status_code, 200)
 
