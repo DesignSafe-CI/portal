53b42b61873a866d3794929fb3844484235b33b5
Frank Netscher <fnetscher@wireless-10-146-254-87.public.utexas.edu>
2018-07-02 14:40:48 -0500

    Fixed mocked Agave client request.  Added assertion that JSON of mocked listing and actual listing are equal.

--- designsafe/LoginTest.py
+++ designsafe/LoginTest.py
@@ -3,6 +3,7 @@ from django.test import TestCase, RequestFactory
 from django.contrib.auth import get_user_model
 from django.core.urlresolvers import reverse
 import mock
+import json
 from designsafe.apps.auth.models import AgaveOAuthToken
 from designsafe.apps.auth.tasks import check_or_create_agave_home_dir
 from designsafe.apps.auth.views import agave_oauth_callback
@@ -40,56 +41,32 @@ class LoginTestClass(TestCase):
   def tearDown(self):
     return
     
-  """ @mock.patch('designsafe.apps.auth.models.AgaveOAuthToken.client')
+  @mock.patch('designsafe.apps.auth.models.AgaveOAuthToken.client')
   @mock.patch('agavepy.agave.Agave')
-  @mock.patch('designsafe.apps.auth.views.agave_oauth_callback')
-  def test_has_agave_file_listing(self, agave_client, agave, agave_oauth_callback):
+  def test_has_agave_file_listing(self, agave_client, agave):
     #agaveclient.return_value.files.list.return_value = [] // whatever the listing should look like
     #request.post.return_value = {} // some object that looks like a requests response
     
     self.client.login(username='test_with_agave', password='test')
 
     agave_client.files.list.return_value = {
-        "name": ".",
-        "path": "test",
-        "lastModified": "2018-06-14T10:44:03.000-05:00",
-        "length": 4096,
-        "permissions": "ALL",
-        "format": "folder",
-        "mimeType": "text/directory",
-        "type": "dir",
+        "name": "test",
         "system": "designsafe.storage.default",
-        "_links": {
-            "self": {
-                "href": "https://agave.designsafe-ci.org/files/v2/media/system/designsafe.storage.default/test"
-            },
-            "system": {
-                "href": "https://agave.designsafe-ci.org/systems/v2/designsafe.storage.default"
-            },
-            "metadata": {
-                "href": "https://agave.designsafe-ci.org/meta/v2/data?q=%7B%22associationIds%22%3A%11111111111111111111111-11111111-0001-001%22%7D"
-            },
-            "history": {
-                "href": "https://agave.designsafe-ci.org/files/v2/history/system/designsafe.storage.default//test"
-            }
-        }
-    } 
-    
-    request_with_agave = self.rf.get(
-        reverse('designsafe_auth:agave_oauth_callback'))
-    request_with_agave.POST.return_value = {
-        'state': 'test',
-        'session': {'auth_state': 'test'},
-        'code': 'test'
-        }
+        "trail": [{"path": "/", "name": "/", "system": "designsafe.storage.default"}, 
+                  {"path": "/test", "name": "test", "system": "designsafe.storage.default"}],
+        "path": "test",
+        "type": "dir",
+        "children": [],
+        "permissions": "READ"
+      } 
 
-    agave_oauth_callback(request_with_agave.POST)
+    resp = self.client.get('/api/agave/files/listing/agave/designsafe.storage.default/test', follow=True)
 
-    resp = self.client.get('https://agave.designsafe-ci.org/files/v2/listings/designsafe.storage.default/test')
-    print resp 
-    self.assertEqual(resp.status_code, 200) """
+    self.assertEqual(resp.status_code, 200)
+    self.assertJSONEqual(resp.content, agave_client.files.list.return_value, msg='Agave homedir listing has unexpected values')
 
-  @mock.patch('designsafe.apps.auth.models.AgaveOAuthToken.client')
+
+  '''@mock.patch('designsafe.apps.auth.models.AgaveOAuthToken.client')
   @mock.patch('agavepy.agave.Agave')
   def test_no_agave_file_listing(self, agave_client, agave):
       self.client.login(username='test_without_agave', password='test')
@@ -110,10 +87,12 @@ class LoginTestClass(TestCase):
 
       resp = agave_oauth_callback(request_without_agave.POST)
       print resp
-      self.assertEqual(resp.status_code, 200)
+      self.assertEqual(resp.status_code, 200)'''
 
   """ def test_user_without_agave_homedir_gets_redirected(self, mock_client, mock_Agave):
 
     pass """
     
-  
+  """ def test_agave_callbak(self):
+
+      resp = self.client.post("/auth/agave/callback?code=code&state=test", data=data) """
