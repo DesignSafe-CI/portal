6e25b9ae58f50c8b3883d7528b7638cc52fa655b
Letania Ferreira <lferreira@tacc.utexas.edu>
2018-08-01 12:21:54 -0500

    removed print statments for testing purpose

--- designsafe/settings/__init__.py
+++ designsafe/settings/__init__.py
@@ -3,7 +3,6 @@ import os
 if os.environ.get("PORTAL_TESTING", 'false').lower() == 'true':
     from .test_settings import *
 else:
-    print "NOT_IN_PORTAL_TESTING_"*100
     from .common import *
     from .celery_settings import *
     from .elasticsearch_settings import *
@@ -12,4 +11,3 @@ else:
     from .rt_settings import *
 
 from django.conf import settings
-print settings.ES_CONNECTIONS
