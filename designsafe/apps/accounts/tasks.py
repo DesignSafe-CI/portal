f773051ead853fd4ba55f653bd7667f567e48f82
Letania Ferreira <lferreira@tacc.utexas.edu>
2018-08-10 14:05:13 -0500

    completed report in tasks.py, need to double check values for research_activities and NH_interests

--- designsafe/apps/accounts/tasks.py
+++ designsafe/apps/accounts/tasks.py
@@ -17,40 +17,47 @@ logger = logging.getLogger(__name__)
 @shared_task(default_retry_delay=1*30, max_retries=3)
 def create_report(username, list_name):
    
-
-    try:
-        csv_file = StringIO.StringIO()
-        writer = csv.writer(csv_file)
-        writer.writerow(["UserEmail","FirstName","LastName","PhoneNumber","Institution",\
-            # "UserName","Country","Bio","Website","Orcid_id","Professional Level",\
-            # "Research Activities","NH_interests","Ethnicity","Gender","Citizenship",\
-            ])
-
-        notification_list = get_user_model().objects.filter(
+    notification_list = get_user_model().objects.filter(
             Q(notification_preferences__isnull=True) |
             Q(**{"notification_preferences__{}".format(list_name): True}))
+
+    try:
+
+        logger.info(
+            "Creating user report for user=%s on "
+            "default storage systemId=%s",
+            username,
+            settings.AGAVE_STORAGE_SYSTEM
+        )
+        csv_file = StringIO.StringIO()
+        writer = csv.writer(csv_file)
+        writer.writerow(["User Email","First Name","Last Name","Phone Number","Institution ID",\
+            "Username","Country of residence","Bio","Website","Orcid_id","Professional Level",\
+            "Research Activities","NH_interests","Ethnicity","Gender","Citizenship",\
+            ])
+
         for user in notification_list:
             user_profile = TASUser(username=user)
             designsafe_user = get_user_model().objects.get(username=user)
-          
-            designsafe_username = get_user_model().objects.get(username=username)
-        
-            logger.debug("here is extended info for designsafe username.profile:{}".format(designsafe_username.profile.bio.encode('utf-8') if designsafe_username.profile.bio else designsafe_username.profile.bio))
-            logger.debug("here is extended info for designsafe user.profile:{}".format(designsafe_user.profile.bio.encode('utf-8') if designsafe_user.profile.bio else designsafe_user.profile.bio))
-            
+            if hasattr(designsafe_user, "profile"):
 
-
-
-            writer.writerow([user_profile.email,\
-                user_profile.firstName.encode('utf-8') if user_profile.firstName else user_profile.firstName,\
-                user_profile.lastName.encode('utf-8') if user_profile.lastName else user_profile.lastName,\
-                user_profile.phone,\
-                user_profile.institutionId,\
-                # designsafe_profile_user.bio.encode('utf-8') if designsafe_profile_user.bio else designsafe_profile_user.bio,\
-            ])
-
-  
-
+                writer.writerow([user_profile.email,\
+                    user_profile.firstName.encode('utf-8') if user_profile.firstName else user_profile.firstName,\
+                    user_profile.lastName.encode('utf-8') if user_profile.lastName else user_profile.lastName,\
+                    user_profile.phone,\
+                    user_profile.institutionId,\
+                    user_profile,\
+                    user_profile.country,\
+                    designsafe_user.profile.bio.encode('utf-8') if designsafe_user.profile.bio else designsafe_user.profile.bio,\
+                    designsafe_user.profile.website.encode('utf-8') if designsafe_user.profile.website else designsafe_user.profile.website,\
+                    designsafe_user.profile.orcid_id.encode('utf-8') if designsafe_user.profile.orcid_id else designsafe_user.profile.orcid_id,\
+                    designsafe_user.profile.professional_level,\
+                    designsafe_user.profile.research_activities,\
+                    designsafe_user.profile.nh_interests,\
+                    designsafe_user.profile.ethnicity,\
+                    designsafe_user.profile.gender,\
+                    user_profile.citizenship,\
+                ])
 
         ag = Agave(api_server=settings.AGAVE_TENANT_BASEURL,
                    token=settings.AGAVE_SUPER_TOKEN)
