68eb5f429c6403852242c6514bbcfd0e0b651c72
Letania Ferreira <lferreira@tacc.utexas.edu>
2018-08-15 16:29:18 -0500

    added date account created and fixed order of items

--- designsafe/apps/accounts/tasks.py
+++ designsafe/apps/accounts/tasks.py
@@ -31,9 +31,9 @@ def create_report(username, list_name):
         )
         csv_file = StringIO.StringIO()
         writer = csv.writer(csv_file)
-        writer.writerow(["User Email","First Name","Last Name","Phone Number","Institution ID",\
-            "Username","Country of residence","Bio","Website","Orcid_id","Professional Level",\
-            "Research Activities","NH_interests","Ethnicity","Gender","Citizenship",\
+        writer.writerow(["Last Name","First Name","Email","Phone Number","Institution ID",\
+            "Professional Level","Bio","NH_interests","Research Activities","Username",\
+            "Ethnicity","Gender","Country of residence","Citizenship","Date Account Created",\
             ])
 
         for user in notification_list:
@@ -41,35 +41,35 @@ def create_report(username, list_name):
             designsafe_user = get_user_model().objects.get(username=user)
             if hasattr(designsafe_user, "profile"):
 
-                writer.writerow([user_profile.email,\
+                # order of items required by user
+                writer.writerow([user_profile.lastName.encode('utf-8') if user_profile.lastName else user_profile.lastName,\
                     user_profile.firstName.encode('utf-8') if user_profile.firstName else user_profile.firstName,\
-                    user_profile.lastName.encode('utf-8') if user_profile.lastName else user_profile.lastName,\
+                    user_profile.email,\
                     user_profile.phone,\
                     user_profile.institutionId,\
-                    user_profile,\
-                    user_profile.country,\
-                    designsafe_user.profile.bio.encode('utf-8') if designsafe_user.profile.bio else designsafe_user.profile.bio,\
-                    designsafe_user.profile.website.encode('utf-8') if designsafe_user.profile.website else designsafe_user.profile.website,\
-                    designsafe_user.profile.orcid_id.encode('utf-8') if designsafe_user.profile.orcid_id else designsafe_user.profile.orcid_id,\
                     designsafe_user.profile.professional_level,\
+                    designsafe_user.profile.bio.encode('utf-8') if designsafe_user.profile.bio else designsafe_user.profile.bio,\
                     # making queryset into list
-                    list(designsafe_user.profile.research_activities.all()) if designsafe_user.profile.research_activities.all() else None,\
                     list(designsafe_user.profile.nh_interests.all()) if designsafe_user.profile.nh_interests.all() else None,\
+                    list(designsafe_user.profile.research_activities.all()) if designsafe_user.profile.research_activities.all() else None,\
+                    user_profile,\
                     designsafe_user.profile.ethnicity,\
                     designsafe_user.profile.gender,\
+                    user_profile.country,\
                     user_profile.citizenship,\
+                    designsafe_user.date_joined,\
                 ])
 
-        ag = Agave(api_server=settings.AGAVE_TENANT_BASEURL,
-                   token=settings.AGAVE_SUPER_TOKEN)
-        setattr(csv_file, 'name', 'user_report.csv')
-        ag.files.importData(
-           filePath=username,
-           fileName='user_report.csv',
-           systemId=settings.AGAVE_STORAGE_SYSTEM,
-           fileToUpload=csv_file
-           )
-        # logger.debug('report contents: %s', csv_file.getvalue())
+        # ag = Agave(api_server=settings.AGAVE_TENANT_BASEURL,
+        #            token=settings.AGAVE_SUPER_TOKEN)
+        # setattr(csv_file, 'name', 'user_report.csv')
+        # ag.files.importData(
+        #    filePath=username,
+        #    fileName='user_report.csv',
+        #    systemId=settings.AGAVE_STORAGE_SYSTEM,
+        #    fileToUpload=csv_file
+        #    )
+        logger.debug('report contents: %s', csv_file.getvalue())
         csv_file.close()
 
     except (HTTPError, AgaveException):
