dcab77a1b2c960e5112f2cfcc5a60357304805be
Letania Ferreira <lferreira@tacc.utexas.edu>
2018-08-16 23:24:30 -0500

    simplified send_mail arguments and elaborated on e-mail message

--- designsafe/apps/api/projects/views.py
+++ designsafe/apps/api/projects/views.py
@@ -372,16 +372,15 @@ class ProjectCollaboratorsView(SecureMixin, BaseApiView):
         #TODO: This should also run on a task
         for username in team_members_to_add+co_pis_to_add:
             collab_users = get_user_model().objects.filter(username=username)
-            collab_users = list(collab_users)
+            # collab_users = list(collab_users)
             if collab_users:
                 collab_user = collab_users[0]
                 try:
-                    collab_user.profile.send_mail(subject="You have been added to a DesignSafe project!",\
-                        message= "Hi {}, you have been added to the project {}".format(collab_user, title=prj.title),\
-                        from_email="announce@designsafe-ci.org <announce@designsafe-ci.org>",\
-                        recipient_list=collab_users,\
-                        fail_silently=False,\
-                        )
+                    collab_user.profile.send_mail("You have been added to a DesignSafe project!",
+                        ("Hi {}, you have been added to the project {}.\n",
+                         "You can now start working on the project. Please use your TACC account to access the DesignSafe-CI website or to ask for help.\n",
+                         "<This is a programmatically generated message. Do NOT reply to this message.>\n",
+                         "Thanks, The DesignSafe-CI team").format(collab_user, project.title))
                 except DesignSafeProfile.DoesNotExist as err:
                     logger.info("Could not send email to user %s", collab_user)
 
@@ -399,7 +398,7 @@ class ProjectCollaboratorsView(SecureMixin, BaseApiView):
                 #         settings.DEFAULT_FROM_EMAIL,
                 #         [collab_user.email],
                 #         html_message=body)
-                    #logger.exception(err)
+                #     logger.exception(err)
 
         return JsonResponse(project.collaborators)
 
