ec4d5f7a03c2efbf67cc6996de550afe9ed4781d
Strmiska <keith.strmiska@gmail.com>
2018-09-18 16:39:50 -0500

    commenting out two tests that are causing errors and fixing a typo

--- designsafe/static/scripts/ng-designsafe/services/data-browser-service.js
+++ designsafe/static/scripts/ng-designsafe/services/data-browser-service.js
@@ -177,8 +177,9 @@
       tests.canViewCitation = files.length >= 1 && hasPermission('READ', files);
       tests.canShare = files.length === 1 && $state.current.name === 'myData';
       tests.canCopy = files.length >= 1 && hasPermission('READ', files);
-      tests.canMove = files.length >= 1 && hasPermission('WRITE', [currentState.listing].concat(files)) && !['dropboxData', 'boxData', 'googledriveData'].includes($state.current.name);
-      tests.canRename = files.length === 1 && hasPermission('WRITE', [currentState.listing].concat(files)) && !['dropboxData', 'boxData', 'googledriveData'].includes($state.current.name);
+      // following two tests do not work...
+      // tests.canMove = files.length >= 1 && hasPermission('WRITE', [currentState.listing].concat(files)) && !['dropboxData', 'boxData', 'googledriveData'].includes($state.current.name);
+      // tests.canRename = files.length === 1 && hasPermission('WRITE', [currentState.listing].concat(files)) && !['dropboxData', 'boxData', 'googledriveData'].includes($state.current.name);
       tests.canViewCategories = true;
 
       var trashPath = _trashPath();
@@ -2638,7 +2639,7 @@
 
             // display everything...
             $ctrl.ui.ieeeCitation = $sce.trustAsHtml(ieeeAuthors + ', (' + citationDate + '), "' + pub.value.title + '" , DesignSafe-CI [publisher], Dataset, ' + pub.doi);
-            $ctrl.doiurl = "https://ezid.cdlib.org/id/" + prj.doi;
+            $ctrl.doiurl = "https://ezid.cdlib.org/id/" + pub.doi;
             // $ctrl.getCitation();
           }
 
