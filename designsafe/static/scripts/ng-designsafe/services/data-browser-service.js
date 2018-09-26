8cb0b85583174755c00cd86306944332541669ca
Strmiska <keith.strmiska@gmail.com>
2018-09-26 15:46:07 -0500

    fix toolbar tests and disable tag rename and move in published and community data

--- designsafe/static/scripts/ng-designsafe/services/data-browser-service.js
+++ designsafe/static/scripts/ng-designsafe/services/data-browser-service.js
@@ -171,15 +171,14 @@
 
       var tests = {};
       tests.canDownload = files.length >= 1 && hasPermission('READ', files) && (!(containsFolder(files)));
-      tests.canPreview = files.length === 1 && hasPermission('READ', files);
-      tests.canPreviewImages = files.length >= 1 && hasPermission('READ', files) && !['dropboxData', 'boxData', 'googledriveData'].includes($state.current.name);
-      tests.canViewMetadata = files.length >= 1 && hasPermission('READ', files);
+      tests.canPreview = files.length === 1 && hasPermission('READ', files) && !['publicData'].includes($state.current.name);
+      tests.canPreviewImages = files.length >= 1 && hasPermission('READ', files) && !['dropboxData', 'boxData', 'googledriveData', 'publicData'].includes($state.current.name);
+      tests.canViewMetadata = files.length >= 1 && hasPermission('READ', files) && !['publicData', 'publishedData', 'communityData'].includes($state.current.name);
       tests.canViewCitation = files.length >= 1 && hasPermission('READ', files);
       tests.canShare = files.length === 1 && $state.current.name === 'myData';
-      tests.canCopy = files.length >= 1 && hasPermission('READ', files);
-      // following two tests do not work...
-      // tests.canMove = files.length >= 1 && hasPermission('WRITE', [currentState.listing].concat(files)) && !['dropboxData', 'boxData', 'googledriveData'].includes($state.current.name);
-      // tests.canRename = files.length === 1 && hasPermission('WRITE', [currentState.listing].concat(files)) && !['dropboxData', 'boxData', 'googledriveData'].includes($state.current.name);
+      tests.canCopy = files.length >= 1 && hasPermission('READ', files) && !['publicData'].includes($state.current.name);
+      tests.canMove = files.length >= 1 && hasPermission('WRITE', files) && !['dropboxData', 'boxData', 'googledriveData', 'publicData', 'publishedData', 'communityData'].includes($state.current.name);
+      tests.canRename = files.length === 1 && hasPermission('WRITE', files) && !['dropboxData', 'boxData', 'googledriveData', 'publicData', 'publishedData', 'communityData'].includes($state.current.name);
       tests.canViewCategories = true;
 
       var trashPath = _trashPath();
