8508b18b6125896b9aad2922cc3fb54c2acaee56
Sal Tijerina <r.sal.tijerina@gmail.com>
2018-10-16 15:37:59 -0700

    Remove old application-tray controller and references

--- designsafe/static/scripts/workspace/controllers/index.js
+++ designsafe/static/scripts/workspace/controllers/index.js
@@ -1,36 +1,17 @@
 import angular from 'angular';
 
 import ApplicationFormCtrl from './application-form';
-import WorkspaceAppTrayCtrl from './application-tray';
 import DataBrowserCtrl from './data-browser';
-import {JobsStatusCtrl, JobDetailsModalCtrl, VNCJobDetailsModalCtrl } from './jobs-status';
+import {JobsStatusCtrl, JobDetailsModalCtrl, VNCJobDetailsModalCtrl} from './jobs-status';
 import WorkspacePanelCtrl from './workspace-panel';
 
 
 let workspaceControllers = angular.module('workspace.controllers', []);
 workspaceControllers.controller('ApplicationFormCtrl', ApplicationFormCtrl);
-workspaceControllers.controller(
-    'WorkspaceAppTrayCtrl',
-    [
-        '$scope',
-        '$rootScope',
-        '$q',
-        '$timeout',
-        '$uibModal',
-        '$state',
-        '$stateParams',
-        '$translate',
-        'Apps',
-        'SimpleList',
-        'MultipleList',
-        '$mdToast',
-        WorkspaceAppTrayCtrl,
-    ]
-);
 workspaceControllers.controller('DataBrowserCtrl', DataBrowserCtrl);
 workspaceControllers.controller('JobsStatusCtrl', JobsStatusCtrl);
 workspaceControllers.controller('JobDetailsModalCtrl', JobDetailsModalCtrl);
 workspaceControllers.controller('VNCJobDetailsModalCtrl', VNCJobDetailsModalCtrl);
 workspaceControllers.controller('WorkspacePanelCtrl', WorkspacePanelCtrl);
 
-export default workspaceControllers
+export default workspaceControllers;
