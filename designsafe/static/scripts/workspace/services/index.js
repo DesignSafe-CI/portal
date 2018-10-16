44b442afb200cd7581e81d4f80fd80a4ef7c55a9
Sal Tijerina <r.sal.tijerina@gmail.com>
2018-10-16 16:01:47 -0700

    Remove mulitple-list-service from workspace

--- designsafe/static/scripts/workspace/services/index.js
+++ designsafe/static/scripts/workspace/services/index.js
@@ -1,17 +1,15 @@
 import angular from 'angular';
 
-import { appsService } from './apps-service';
+import {appsService} from './apps-service';
 import {jobsService} from './jobs-service';
-import {multipleListService} from './multiple-list-service';
-import { simpleListService } from './simple-list-service';
+import {simpleListService} from './simple-list-service';
 import {workspaceSystemsService} from './systems-service';
 
 
 let workspaceServices = angular.module('workspace.services', []);
-workspaceServices.factory('Apps', appsService)
-workspaceServices.factory('Jobs', jobsService)
-workspaceServices.factory('MultipleList', multipleListService)
-workspaceServices.factory('SimpleList', simpleListService)
-workspaceServices.factory('Systems', workspaceSystemsService)
+workspaceServices.factory('Apps', appsService);
+workspaceServices.factory('Jobs', jobsService);
+workspaceServices.factory('SimpleList', simpleListService);
+workspaceServices.factory('Systems', workspaceSystemsService);
 
-export default workspaceServices
+export default workspaceServices;
