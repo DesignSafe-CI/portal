93853f5a34932016ca371c8da5c37716c4e6420b
Sal Tijerina <r.sal.tijerina@gmail.com>
2018-11-09 17:17:21 -0600

    Make pretty and only call for system roles if not admin

--- designsafe/static/scripts/workspace/services/index.js
+++ designsafe/static/scripts/workspace/services/index.js
@@ -1,14 +1,13 @@
 import angular from 'angular';
 
-import {appsService} from './apps-service';
-import {jobsService} from './jobs-service';
-import {simpleListService} from './simple-list-service';
-import {workspaceSystemsService} from './systems-service';
+import { appsService } from './apps-service';
+import { jobsService } from './jobs-service';
+import { simpleListService } from './simple-list-service';
+import { workspaceSystemsService } from './systems-service';
 
 
-let workspaceServices = angular.module('workspace.services', [])
-.constant('appIcons', ['Compress', 'Extract', 'MATLAB', 'Paraview', 'Hazmapper', 'Jupyter', 'ADCIRC', 'QGIS', 'LS-DYNA', 'LS-Pre/Post', 'VisIt', 'OpenFOAM', 'OpenSees'])
-.constant('appCategories', ['Simulation', 'Visualization', 'Data Processing', 'Partner Data Apps', 'Utilities']);
+let workspaceServices = angular.module('workspace.services', []);
+
 workspaceServices.factory('Apps', appsService);
 workspaceServices.factory('Jobs', jobsService);
 workspaceServices.factory('SimpleList', ['$http', '$q', 'djangoUrl', 'appCategories', 'appIcons', simpleListService]);
