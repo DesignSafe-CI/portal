889f88dd7b9fae79972caf719cdece7af6aca12b
Sal Tijerina <r.sal.tijerina@gmail.com>
2018-11-15 12:53:02 -0600

    Turn SimpleList into class

--- designsafe/static/scripts/applications/services/simple-list-service.js
+++ designsafe/static/scripts/applications/services/simple-list-service.js
@@ -1,17 +1,34 @@
-export function simpleListFactory($http, $q, $translate, djangoUrl, appIcons, appCategories) {
-    'ngInclude';
-
-    let SimpleList = function() {
-        this.selected = null;
+export class SimpleList {
+    constructor($http, $q, djangoUrl, appIcons) {
+        'ngInject';
+        this.$http = $http;
+        this.$q = $q;
+        this.djangoUrl = djangoUrl;
+        this.appIcons = appIcons;
+        this.selected;
         this.lists = {};
         this.map = {};
-    };
+    }
 
-    SimpleList.prototype.getDefaultLists = function(query) {
+    /**
+     * Searches for a parameter in an Agave metadata.value.definition.tags field
+     * @function
+     * @param {Object} definition - Agave metadata.value.definition
+     * @param {String} param - Parameter to search for in definition.tags, i.e. 'appIcon'
+     * @return {Boolean}
+     */
+    tagIncludesParam(definition, param) {
+        return definition.tags &&
+            Array.isArray(definition.tags) &&
+            definition.tags.filter(s => s.includes(`${param}:`))[0] &&
+            definition.tags.filter(s => s.includes(`${param}:`))[0].split(':')[1];
+    }
+
+    getDefaultLists(query) {
         let self = this,
-            deferred = $q.defer();
-        $http({
-            url: djangoUrl.reverse('designsafe_applications:call_api', ['meta']),
+            deferred = this.$q.defer();
+        this.$http({
+            url: this.djangoUrl.reverse('designsafe_applications:call_api', ['meta']),
             method: 'GET',
             params: { q: query },
         }).then(
@@ -29,10 +46,10 @@ export function simpleListFactory($http, $q, $translate, djangoUrl, appIcons, ap
 
                     // Parse app icon from tags for agave apps, or from metadata field for html apps
                     appMeta.value.definition.appIcon = null;
-                    if (appMeta.value.definition.hasOwnProperty('tags') && appMeta.value.definition.tags.filter(s => s.includes('appIcon')) !== undefined && appMeta.value.definition.tags.filter(s => s.includes('appIcon')).length != 0) {
+                    if (self.tagIncludesParam(appMeta.value.definition, 'appIcon')) {
                         appMeta.value.definition.appIcon = appMeta.value.definition.tags.filter(s => s.includes('appIcon'))[0].split(':')[1];
                     } else {
-                        appIcons.some(function(icon) {
+                        self.appIcons.some(function(icon) {
                             if (appMeta.value.definition.label.toLowerCase().includes(icon)) {
                                 appMeta.value.definition.appIcon = appMeta.value.definition.orderBy = icon;
                                 return true;
@@ -62,21 +79,5 @@ export function simpleListFactory($http, $q, $translate, djangoUrl, appIcons, ap
             }
         );
         return deferred.promise;
-    };
-
-    /**
-     * Searches for a parameter in an Agave metadata.value.definition.tags field
-     * @function
-     * @param {Object} definition - Agave metadata.value.definition
-     * @param {String} param - Parameter to search for in definition.tags, i.e. 'appIcon'
-     * @return {Boolean}
-     */
-    SimpleList.prototype.tagIncludesParam = function(definition, param) {
-        return definition.tags &&
-            Array.isArray(definition.tags) &&
-            definition.tags.filter(s => s.includes(`${param}:`))[0] &&
-            definition.tags.filter(s => s.includes(`${param}:`))[0].split(':')[1];
-    };
-
-    return SimpleList;
+    }
 }
