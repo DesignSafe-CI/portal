ce089921ca2a1a0818f11cb5b7354de9d1fd3642
Sal Tijerina <r.sal.tijerina@gmail.com>
2018-11-19 16:49:07 -0600

    Remove unused task def and references to it

--- designsafe/apps/workspace/tasks.py
+++ designsafe/apps/workspace/tasks.py
@@ -50,8 +50,6 @@ def submit_job(request, username, job_post):
         response = agave.jobs.submit(body=job_post)
         logger.debug('Job Submission Response: {}'.format(response))
 
-        # watch job status
-        # watch_job_status.apply_async(args=[username, response['id']], countdown=10, queue='api')
         return response
 
     except ConnectionError as e:
@@ -77,123 +75,7 @@ def submit_job(request, username, job_post):
         logger.warning(err_resp)
         raise JobSubmitError(**err_resp)
 
-@shared_task(bind=True, max_retries=None)
 
-def watch_job_status(self, username, job_id, current_status=None):
-
-    """Polls the specified Agave job instance, and notifies the user of the job
-    status by instantiating and saving a Notification instance.
-
-    If the job is finished, we also index the job and  alert the user to the
-    URL of the job's location in the data depot.
-
-    This method is replaced by the handle_webhook_request method.
-
-    Args:
-        username (string): Username of a designsafe user.
-        job_id (string): ID for the job to be polled.
-        current_status (string): Status of the job, obtained from the previous
-        polling attempt. Used to detect changes in status.
-
-    """
-
-    try:
-
-        user = get_user_model().objects.get(username=username)
-        ag = user.agave_oauth.client
-        job = ag.jobs.get(jobId=job_id)
-
-        try:
-        #job['submitTime'] is a datetime object
-            job['submitTime']=str(job['submitTime'])
-            job['endTime']=str(job['endTime'])
-        except KeyError as e:
-            pass #unfinished jobs won't have an endTime
-
-
-        job_status = job['status']
-        #if job_status == 'FINISHED':
-        #    job_status = 'INDEXING'
-
-        job_name = job['name']
-        logger.debug(current_status)
-        logger.debug(job_status)
-        event_data = {
-            Notification.EVENT_TYPE: 'job',
-            Notification.STATUS: '',
-            Notification.USER: username,
-            Notification.MESSAGE: '',
-            Notification.EXTRA: job
-        }
-        archive_id = 'agave/%s/%s' % (job['archiveSystem'], job['archivePath'].split('/'))
-
-        if job_status == 'FAILED':
-            # end state, no additional tasks; notify
-            logger.debug('JOB FAILED: id=%s status=%s' % (job_id, job_status))
-            event_data[Notification.STATUS] = Notification.ERROR
-            event_data[Notification.MESSAGE] = "Job '%s' Failed. Please try again..." % (job_name)
-            event_data[Notification.OPERATION] = 'job_failed'
-            n = Notification.objects.create(**event_data)
-            n.save()
-
-        elif job_status == 'FINISHED':
-            # end state, start indexing outputs
-            logger.debug('JOB STATUS CHANGE: id=%s status=%s' % (job_id, job_status))
-
-
-            logger.debug('archivePath: {}'.format(job['archivePath']))
-            target_path = reverse('designsafe_data:data_depot')
-            os.path.join(target_path, 'agave', archive_id.strip('/'))
-            event_data[Notification.STATUS] = Notification.SUCCESS
-            event_data[Notification.EXTRA]['job_status'] = 'FINISHED'
-            event_data[Notification.EXTRA]['target_path'] = target_path
-            event_data[Notification.MESSAGE] = "Job '%s' finished!" % (job_name)
-            event_data[Notification.OPERATION] = 'job_finished'
-
-            n = Notification.objects.create(**event_data)
-            n.save()
-            logger.debug('Event data with action link %s' % event_data)
-
-            try:
-                logger.debug('Preparing to Index Job Output job=%s', job)
-                # reindex_agave.delay(username, job['archiveSystem']+job['archivePath'], levels=0)
-                logger.debug('Finished Indexing Job Output job=%s', job)
-
-            except Exception as e:
-                logger.exception('Error indexing job output; scheduling retry')
-                #raise self.retry(exc=e, countdown=60)
-
-        elif current_status and current_status == job_status:
-            # DO NOT notify, but still queue another watch task
-
-            self.retry(countdown=10, kwargs={'current_status': job_status})
-        else:
-            # notify
-            logger.debug('JOB STATUS CHANGE: id=%s status=%s' % (job_id, job_status))
-            event_data[Notification.STATUS] = Notification.INFO
-            event_data[Notification.MESSAGE] = "Job '%s' updated to %s." % (job_name, job_status)
-            event_data[Notification.OPERATION] = 'job_status_update'
-            n = Notification.objects.create(**event_data)
-            n.save()
-            self.retry(countdown=10, kwargs={'current_status': job_status})
-
-    except ObjectDoesNotExist:
-        logger.exception('Unable to locate local user account: %s' % username)
-
-    except HTTPError as e:
-        if e.response.status_code == 404:
-            logger.warning('Job not found. Cancelling job watch.',
-                           extra={'job_id': job_id})
-        else:
-            logger.warning('Agave API error. Retrying...')
-            raise self.retry(exc=e, countdown=60, max_retries=10)
-
-    except AgaveException as e:
-        logger.warning('Agave API error. Retrying...')
-        raise self.retry(exc=e, countdown=60, max_retries=10)
-
-
-# @shared_task(bind=True)
 def handle_webhook_request(job):
     """Notifies the user of the job status by instantiating and saving
     a Notification instance.
