26166287e4d8eade902a6b7c7c8fa790d44f09fe
Sal Tijerina <r.sal.tijerina@gmail.com>
2018-11-19 18:29:43 -0600

    Remove unused job webhook

--- designsafe/apps/notifications/views.py
+++ designsafe/apps/notifications/views.py
@@ -105,48 +105,6 @@ def generic_webhook_handler(request):
     return HttpResponse('OK')
 
 
-@require_POST
-@csrf_exempt
-def job_notification_handler(request):
-    JOB_EVENT = 'job'
-    logger.debug('request body: {}'.format(request.body))
-
-    try:
-        notification = json.loads(request.body)
-        logger.info('notification body: {}'.format(notification))
-        logger.info('notification name: {}'.format(notification['name']))
-        job_name = notification['name']
-        status = notification['status']
-        event = request.GET.get('event')
-        job_id = request.GET.get('job_id')
-        job_owner = notification['owner']
-        archive_path = notification['archivePath']
-    except ValueError as e:  # for testing ->used when mocking agave notification
-        job_name = request.POST.get('job_name')
-        status = request.POST.get('status')
-        event = request.POST.get('event')
-        job_id = request.POST.get('job_id')
-        job_owner = request.POST.get('job_owner')
-        archive_path = request.POST.get('archivePath')
-
-    logger.info('job_name: {}'.format(job_name))
-    logger.info('event: {}'.format(event))
-    logger.info('job_id: {}'.format(job_id))
-
-    body = {
-        'job_name': job_name,
-        'job_id': job_id,
-        'event': event,
-        'status': status,
-        'archive_path': archive_path,
-        'job_owner': job_owner,
-    }
-    generic_event.send_robust(None, event_type='job', event_data=body,
-                             event_users=[job_owner])
-
-    return HttpResponse('OK')
-
-
 def get_number_unread_notifications(request):
     # nondeleted = JobNotification.objects.filter(deleted=False, user=str(request.user)).count()
     unread = Notification.objects.filter(deleted=False, read=False, user=str(request.user)).count()
