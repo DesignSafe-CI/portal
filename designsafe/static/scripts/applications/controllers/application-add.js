2411c2e2fcaaac0200d0af5170e3f014015e26df
Sal Tijerina <r.sal.tijerina@gmail.com>
2018-11-28 18:32:24 -0600

    Fix typo/import error

--- designsafe/static/scripts/applications/controllers/application-add.js
+++ designsafe/static/scripts/applications/controllers/application-add.js
@@ -1,7 +1,7 @@
 export function applicationAddCtrl(window, angular, $, _) {
     'ngInject';
     angular.module('designsafe').controller('ApplicationAddCtrl',
-        ['$scope', '$rootScope', '$q', '$timeout', '$uibModal', '$translate', '$state', 'Apps', 'AppsAppsSimpleList', 'MultipleList', 'AppsWizard', 'Django', 'appCategories', 'appIcons', function($scope, $rootScope, $q, $timeout, $uibModal, $translate, $state, Apps, AppsSimpleList, MultipleList, AppsWizard, Django, appCategories, appIcons) {
+        ['$scope', '$rootScope', '$q', '$timeout', '$uibModal', '$translate', '$state', 'Apps', 'AppsSimpleList', 'MultipleList', 'AppsWizard', 'Django', 'appCategories', 'appIcons', function($scope, $rootScope, $q, $timeout, $uibModal, $translate, $state, Apps, AppsSimpleList, MultipleList, AppsWizard, Django, appCategories, appIcons) {
             /** addForm form **/
             $scope.addSchema = {
                 type: 'object',
