e9e7c50d21c3dcfda53441850205438bf3cdfe49
Keith G Strmiska <keith.strmiska@gmail.com>
2019-01-09 10:14:57 -0600

    Task/DES-671 - Manage Categories Component (#348)
    Added Category Management Component

--- designsafe/static/scripts/ng-designsafe/services/index.js
+++ designsafe/static/scripts/ng-designsafe/services/index.js
@@ -11,10 +11,12 @@ import { ProjectService } from './project-service';
 import { SystemsService } from './systems-service';
 import { TicketsService } from './tickets-service';
 import { UserService } from './user-service';
+import { ManageCategoriesComponent } from '../../projects/components/manage-categories/manage-categories.component';
 import { ManageExperimentsComponent } from '../../projects/components/manage-experiments/manage-experiments.component';
 import { ManageHybridSimComponent } from '../../projects/components/manage-hybrid-simulations/manage-hybrid-simulations.component';
 import { ManageSimulationComponent } from '../../projects/components/manage-simulations/manage-simulations.component';
 import { EditProjectComponent } from '../../projects/edit-project/edit-project.component';
+
 
 let designsafeServices = angular.module('designsafe.services', []);
 
@@ -29,6 +31,7 @@ designsafeServices.factory('ProjectService', ProjectService);
 designsafeServices.factory('SystemsService', SystemsService);
 designsafeServices.service('TicketsService', TicketsService);
 designsafeServices.service('UserService', UserService);
+designsafeServices.component('manageCategories', ManageCategoriesComponent);
 designsafeServices.component('manageExperiments', ManageExperimentsComponent);
 designsafeServices.component('manageHybridSimulations', ManageHybridSimComponent);
 designsafeServices.component('manageSimulations', ManageSimulationComponent);
