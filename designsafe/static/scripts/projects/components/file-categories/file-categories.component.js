e88acf99a9fd9be9f620ef3124a77a18fdbceb38
Josue Balandrano Coronel <jbc@rmcomplexity.com>
2019-01-24 17:30:40 -0600

    Enable actions in file categories

--- designsafe/static/scripts/projects/components/file-categories/file-categories.component.js
+++ designsafe/static/scripts/projects/components/file-categories/file-categories.component.js
@@ -1,15 +1,58 @@
+import _ from 'underscore';
+
 import FileCategoriesTemplate from './file-categories.template.html';
 
 class FileCategoriesCtrl {
-    constructor(Django, ProjectService, ProjectEntitiesService){
+    constructor($q, Django, ProjectService, ProjectEntitiesService){
         'ngInject';
         this.Django = Django;
         this.ProjectService = ProjectService;
         this.ProjectEntitiesService = ProjectEntitiesService;
+        this.$q = $q;
     }
 
     $onInit() {
         this._ui = { busy: false, error: false };
+    }
+
+    removeCategory(entity) {
+        let promise;
+        if (_.isEmpty(this.file.uuid())) {
+            promise = this.file.getMeta();
+        } else {
+            let my_promise = () => {
+                let defer = this.$q.defer();
+                defer.resolve(this.file);
+                return defer.promise;
+            };
+            promise = my_promise();
+        }
+        promise.then((file)=>{
+            if (!_.contains(entity.associationIds, file.uuid())){
+                return undefined;
+            }
+            entity.associationIds = _.difference(entity.associationIds, [this.file.uuid()]);
+            entity.value.files = _.difference(entity.value.files, [this.file.uuid()]);
+            return entity;
+        }).then( (ret) => {
+            if (!ret){
+                return undefined;
+            }
+            return this.ProjectEntitiesService.update({
+                data: {
+                    uuid: ret.uuid,
+                    entity: ret,
+                },
+            });
+        }).then( (ret) => {
+            if (!ret) {
+                return this.file;
+            }
+            let entity = this.project.getRelatedByUuid(ret.uuid);
+            entity.update(ret);
+            this.file.setEntities(this.project.uuid, this.project.getAllRelatedObjects());
+            return this.file;
+        });
     }
 }
 
