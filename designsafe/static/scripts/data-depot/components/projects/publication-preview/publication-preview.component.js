ab396849235130c77e6a0698054f77054d8c8295
Strmiska <keith.strmiska@gmail.com>
2019-02-05 15:16:55 -0600

    created curation directory and added filelisting to publication preview

--- designsafe/static/scripts/data-depot/components/projects/publication-preview/publication-preview.component.js
+++ designsafe/static/scripts/data-depot/components/projects/publication-preview/publication-preview.component.js
@@ -4,31 +4,105 @@ import _ from 'underscore';
 
 class PublicationPreviewCtrl {
 
-    constructor(ProjectEntitiesService, ProjectService, $uibModal, $state) {
+    constructor(ProjectEntitiesService, ProjectService, DataBrowserService, FileListing, $uibModal, $state, $q) {
         'ngInject';
 
         this.ProjectEntitiesService = ProjectEntitiesService;
         this.ProjectService = ProjectService;
+        this.DataBrowserService = DataBrowserService;
+        this.FileListing = FileListing;
+        this.browser = this.DataBrowserService.state();
         this.$uibModal = $uibModal;
         this.$state = $state;
+        this.$q = $q;
     }
     
     $onInit() {
         this.projectId = this.ProjectService.resolveParams.projectId;
-        this.browser = this.ProjectService.data;
+        this.filePath = this.ProjectService.resolveParams.filePath;
+        this.loading = false;
 
-        this.setEntitiesRel = (resp) => {
-            this.browser.project.appendEntitiesRel(resp);
-            return resp;
-        };
-
-        // get project data if necessary (if a user refreshes or visits path directly)
         if (!this.browser.project) {
-            this.ProjectService.get({uuid: this.projectId}).then((project) => {
-                this.browser.project = project;
-                this.ProjectEntitiesService.listEntities({uuid: this.projectId, name: 'all'}).then(this.setEntitiesRel);
-            });
+            this.$state.go('projects.view.data', {projectId: this.projectId}, {reload: true});
+        } else {
+            /*
+            update uniqe file listing
+            we might want to consider a adding this to the
+            FilesListing service if we start using it in
+            multiple places...
+            */
+           var entities = this.browser.project.getAllRelatedObjects();
+           var allFilePaths = [];
+           var apiParams = {
+               fileMgr: 'agave',
+               baseUrl: '/api/agave/files',
+               searchState: 'projects.view.data',
+           };
+           _.each(entities, (entity) => {
+               this.browser.listings[entity.uuid] = {
+                   name: this.browser.listing.name,
+                   path: this.browser.listing.path,
+                   system: this.browser.listing.system,
+                   trail: this.browser.listing.trail,
+                   children: [],
+               };
+               allFilePaths = allFilePaths.concat(entity._filePaths);
+           });
+    
+           this.setFilesDetails = (filePaths) => {
+               this.loading = true;
+               filePaths = _.uniq(filePaths);
+               var p = this.$q((resolve, reject) => {
+                   var results = [];
+                   var index = 0;
+                   var size = 5;
+                   var fileCalls = _.map(filePaths, (filePath) => {
+                       return this.FileListing.get(
+                           { system: 'project-' + this.browser.project.uuid, path: filePath }, apiParams
+                       ).then((resp) => {
+                           if (!resp) {
+                               return;
+                           }
+                           var allEntities = this.browser.project.getAllRelatedObjects();
+                           var entities = _.filter(allEntities, (entity) => {
+                               return _.contains(entity._filePaths, resp.path);
+                           });
+                           _.each(entities, (entity) => {
+                               this.browser.listings[entity.uuid].children.push(resp);
+                           });
+                           return resp;
+                       }).then(() => {
+                           this.loading = false;
+                       });
+                   });
+    
+                   var step = () => {
+                       var calls = fileCalls.slice(index, (index += size));
+                       if (calls.length) {
+                           this.$q.all(calls)
+                               .then((res) => {
+                                   results.concat(res);
+                                   step();
+                                   return res;
+                               })
+                               .catch(reject);
+                       } else {
+                           resolve(results);
+                       }
+                   };
+                   step();
+               });
+               return p.then(
+                   (results) => {
+                       return results;
+                   },
+                   (err) => {
+                       this.browser.ui.error = err;
+                   });
+           };
+           this.setFilesDetails(allFilePaths);
         }
+
     }
 
     matchingGroup(exp, model) {
@@ -44,6 +118,10 @@ class PublicationPreviewCtrl {
     
     goWork() {
         this.$state.go('projects.view.data', {projectId: this.browser.project.uuid}, {reload: true});
+    }
+
+    goCuration() {
+        this.$state.go('projects.curation', {projectId: this.browser.project.uuid}, {reload: true});
     }
 
     editProject() {
@@ -77,7 +155,7 @@ class PublicationPreviewCtrl {
     }
 }
 
-PublicationPreviewCtrl.$inject = ['ProjectEntitiesService', 'ProjectService', '$uibModal', '$state'];
+PublicationPreviewCtrl.$inject = ['ProjectEntitiesService', 'ProjectService', 'DataBrowserService', 'FileListing', '$uibModal', '$state', '$q'];
 
 export const PublicationPreviewComponent = {
     template: PublicationPreviewTemplate,
