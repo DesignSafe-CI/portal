dec9f62e0a4fe471e6bcc714fbbc96d71196af8e
Strmiska <keith.strmiska@gmail.com>
2019-02-06 10:08:59 -0600

    added listing to curation selection and category added some loading screens

--- designsafe/static/scripts/data-depot/components/projects/pipeline-selection/pipeline-selection.component.js
+++ designsafe/static/scripts/data-depot/components/projects/pipeline-selection/pipeline-selection.component.js
@@ -3,36 +3,138 @@ import _ from 'underscore';
 
 class PipelineSelectionCtrl {
 
-    constructor(ProjectEntitiesService, ProjectService, $uibModal, $state) {
+    constructor(ProjectEntitiesService, ProjectService, DataBrowserService, FileListing, $uibModal, $state, $q) {
         'ngInject';
 
         this.ProjectEntitiesService = ProjectEntitiesService;
-        this.$uibModal = $uibModal;
         this.ProjectService = ProjectService;
+        this.DataBrowserService = DataBrowserService;
+        this.browser = this.DataBrowserService.state();
+        this.FileListing = FileListing;
+        this.$uibModal = $uibModal;
         this.$state = $state;
+        this.$q = $q;
     }
 
     $onInit() {
         this.projectId = this.ProjectService.resolveParams.projectId;
-        this.browser = this.ProjectService.data;
+        this.filePath = this.ProjectService.resolveParams.filePath;
+        this.loading = true;
+        window.sessionStorage.clear();
+        // window.sessionStorage.setItem('projectData', JSON.stringify(this.browser.project));
 
+
+        /*
+        update uniqe file listing
+        we might want to consider a adding this to the
+        FilesListing service if we start using it in
+        multiple places...
+        */
         this.setEntitiesRel = (resp) => {
             this.browser.project.appendEntitiesRel(resp);
+            if (typeof this.browser.listing === 'undefined') {
+                _.each(this.browser.listing.children, (child) => {
+                child.href = this.$state.href('projects.view.data', {
+                        projectId: this.projectId,
+                        filePath: child.path,
+                        projectTitle: this.browser.project.value.projectTitle,
+                    });
+                    child.setEntities(this.projectId, resp);
+                });
+            }
             return resp;
         };
 
-        window.sessionStorage.clear();
-        // get project data if necessary (if a user refreshes or visits path directly)
-        if (!this.browser.project) {
-            this.ProjectService.get({uuid: this.projectId}).then((project) => {
-                this.browser.project = project;
-                this.ProjectEntitiesService.listEntities({uuid: this.projectId, name: 'all'}).then(this.setEntitiesRel);
-            }).then(() => {
-                window.sessionStorage.setItem('projectData', JSON.stringify(this.browser.project));
+        this.DataBrowserService.browse(
+            { system: 'project-' + this.projectId, path: this.filePath },
+            { query_string: this.$state.params.query_string }
+            ).then(() => {
+                this.browser.listing.href = this.$state.href('projects.view.data', {
+                    projectId: this.projectId,
+                    filePath: this.browser.listing.path,
+                    projectTitle: this.browser.project.value.projectTitle,
+                });
+                this.browser.showMainListing = true;
             });
-        } else {
-            window.sessionStorage.setItem('projectData', JSON.stringify(this.browser.project));
-        }
+        
+        this.ProjectService.get({uuid: this.projectId}).then((project) => {
+            this.browser.project = project;
+            this.ProjectEntitiesService.listEntities({uuid: this.projectId, name: 'all'})
+            .then(this.setEntitiesRel)
+            .then(() => {
+                var entities = this.browser.project.getAllRelatedObjects();
+                var allFilePaths = [];
+                this.browser.listings = {};
+                var apiParams = {
+                    fileMgr: 'agave',
+                    baseUrl: '/api/agave/files',
+                    searchState: 'projects.view.data',
+                };
+                _.each(entities, (entity) => {
+                    this.browser.listings[entity.uuid] = {
+                        name: this.browser.listing.name,
+                        path: this.browser.listing.path,
+                        system: this.browser.listing.system,
+                        trail: this.browser.listing.trail,
+                        children: [],
+                    };
+                    allFilePaths = allFilePaths.concat(entity._filePaths);
+                });
+        
+                this.setFilesDetails = (filePaths) => {
+                    filePaths = _.uniq(filePaths);
+                    var p = this.$q((resolve, reject) => {
+                        var results = [];
+                        var index = 0;
+                        var size = 5;
+                        var fileCalls = _.map(filePaths, (filePath) => {
+                            return this.FileListing.get(
+                                { system: 'project-' + this.browser.project.uuid, path: filePath }, apiParams
+                            ).then((resp) => {
+                                if (!resp) {
+                                    return;
+                                }
+                                var allEntities = this.browser.project.getAllRelatedObjects();
+                                var entities = _.filter(allEntities, (entity) => {
+                                    return _.contains(entity._filePaths, resp.path);
+                                });
+                                _.each(entities, (entity) => {
+                                    this.browser.listings[entity.uuid].children.push(resp);
+                                });
+                                return resp;
+                            }).then(() => {
+                                this.loading = false;
+                            });
+                        });
+        
+                        var step = () => {
+                            var calls = fileCalls.slice(index, (index += size));
+                            if (calls.length) {
+                                this.$q.all(calls)
+                                    .then((res) => {
+                                        results.concat(res);
+                                        step();
+                                        return res;
+                                    })
+                                    .catch(reject);
+                            } else {
+                                resolve(results);
+                            }
+                        };
+                        step();
+                    });
+                    return p.then(
+                        (results) => {
+                            return results;
+                        },
+                        (err) => {
+                            this.browser.ui.error = err;
+                        });
+                };
+                this.setFilesDetails(allFilePaths);
+            });
+        });
+        
     }
 
     matchingGroup(exp, model) {
@@ -48,27 +150,26 @@ class PipelineSelectionCtrl {
 
     goWork() {
         window.sessionStorage.clear();
-        this.$state.go('projects.view.data', {projectId: this.browser.project.uuid}, {reload: true});
+        this.$state.go('projects.view.data', {projectId: this.projectId}, {reload: true});
     }
 
     goPreview() {
-        this.$state.go('projects.preview', {projectId: this.browser.project.uuid}, {reload: true});
+        this.$state.go('projects.preview', {projectId: this.projectId}, {reload: true});
     }
 
     goProject() {
-        this.$state.go('projects.pipelineProject', {projectId: this.browser.project.uuid}, {reload: true});
+        window.sessionStorage.setItem('projectData', JSON.stringify(this.browser.project));
+        this.$state.go('projects.pipelineProject', {projectId: this.projectId}, {reload: true});
     }
 
     selectExperiment(exp) {
+        this.selectedExp = exp;
         window.sessionStorage.setItem('experimentData', JSON.stringify(exp));
     }
 
-    retrieveExperiment() {
-        this.experiment = JSON.parse(window.sessionStorage.getItem('experimentData'));
-    }
 }
 
-PipelineSelectionCtrl.$inject = ['ProjectEntitiesService', 'ProjectService', '$uibModal', '$state'];
+PipelineSelectionCtrl.$inject = ['ProjectEntitiesService', 'ProjectService', 'DataBrowserService', 'FileListing', '$uibModal', '$state', '$q'];
 
 export const PipelineSelectionComponent = {
     template: PipelineSelectionTemplate,
