ca56b878056df3edf8a2cf067f1254c47a533a61
Josue Balandrano Coronel <jbc@rmcomplexity.com>
2019-02-06 20:25:19 -0600

    Drawing tree.

---
+++ designsafe/static/scripts/projects/components/index.js
@@ -0,0 +1,5 @@
+import angular from 'angular';
+
+import { ProjectTreeComponent } from './project-tree/project-tree.component.js';
+
+let projectComponents = angular.module('dd.project.components', ['dd.components
