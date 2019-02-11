125baf7db63b2b83fc1afc391b8bac6aab61eb11
Strmiska <keith.strmiska@gmail.com>
2019-02-11 16:52:11 -0600

    individual file selection, bug fixes, request changes, and category revisions

--- designsafe/static/scripts/data-depot/components/projects/pipeline-experiment/pipeline-experiment.component.js
+++ designsafe/static/scripts/data-depot/components/projects/pipeline-experiment/pipeline-experiment.component.js
@@ -13,23 +13,20 @@ class PipelineExperimentCtrl {
     }
 
     $onInit() {
-        this.project = JSON.parse(window.sessionStorage.getItem('projectData'));
-        this.experiment = JSON.parse(window.sessionStorage.getItem('experimentData'));
+        this.projectId = this.ProjectService.resolveParams.projectId;
+        this.project = this.ProjectService.resolveParams.project;
+        this.experiment = this.ProjectService.resolveParams.experiment;
+        this.selectedListings = this.ProjectService.resolveParams.selectedListings;
 
-        /*
-        Currently having issues with storing data in sessionStorage.
-        The object methods are lost when parsing the data, but we can
-        use the data to restore the project info.
-        */
-        this.setEntitiesRel = (resp) => {
-            this.prjModel.appendEntitiesRel(resp);
-            return resp;
-        };
-
-        this.ProjectService.get({uuid: this.project.uuid}).then((project) => {
-            this.prjModel = project;
-            this.ProjectEntitiesService.listEntities({uuid: this.project.uuid, name: 'all'}).then(this.setEntitiesRel);
-        });
+        if (!this.project) {
+            /*
+            Try to pass selected listings into a simple object so that we can
+            rebuild the project and selected files if a refresh occurs...
+            for now we can send them back to the selection area
+            */
+            this.projectId = JSON.parse(window.sessionStorage.getItem('projectId'));
+            this.$state.go('projects.pipelineSelect', {projectId: this.projectId}, {reload: true});
+        }
     }
 
     goWork() {
@@ -38,15 +35,25 @@ class PipelineExperimentCtrl {
     }
 
     goProject() {
-        this.$state.go('projects.pipelineProject', {projectId: this.project.uuid}, {reload: true});
+        this.$state.go('projects.pipelineProject', {
+            projectId: this.projectId,
+            project: this.project,
+            experiment: this.experiment,
+            selectedListings: this.selectedListings,
+        }, {reload: true});
     }
 
     goCategories() {
-        this.$state.go('projects.pipelineCategories', {projectId: this.project.uuid}, {reload: true});
+        this.$state.go('projects.pipelineCategories', {
+            projectId: this.projectId,
+            project: this.project,
+            experiment: this.experiment,
+            selectedListings: this.selectedListings,
+        }, {reload: true});
     }
 
     editExp() {
-        this.ProjectService.manageExperiments({'experiments': this.prjModel.experiment_set, 'project': this.prjModel});
+        this.ProjectService.manageExperiments({'experiments': this.project.experiment_set, 'project': this.project});
     }
 
 }
