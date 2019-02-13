661daba1b03ba4478a25b839d2202ff2e135a5f4
Josue Balandrano Coronel <jbc@rmcomplexity.com>
2019-02-12 22:57:49 -0600

    Enable sim and hybrid sim trees.

--- designsafe/static/scripts/projects/components/project-tree/project-tree.component.js
+++ designsafe/static/scripts/projects/components/project-tree/project-tree.component.js
@@ -146,6 +146,378 @@ class ProjectTreeCtrl {
     /*
      * @method
      *
+     * Build tress for every Hybrid Simulation in this class' `this.project`.
+     * The hierarchy is build like so:
+     * + Simulation 1
+     * |
+     * -- + Report
+     * |
+     * -- + Global Model
+     * | |
+     * | -- + Master Simulator Coordinator
+     * |    |
+     * |    -- + Coordinator Output
+     * |    |
+     * |    -- + Numerical Substructure
+     * |    |  |
+     * |    |  -- + Simulation Output
+     * |    |
+     * |    -- + Experimental Substructure
+     * |       |
+     * |       -- + Experimental Output
+     * |
+     * -- + Analysis
+     *
+     * + Simulation 2
+     * [...]
+     */
+    buildHybridSimulationTree() {
+        let simulations = _.sortBy(this.project.hybridsimulation_set, (sim) => { return sim.value.title; });
+        let models = _.sortBy(this.project.globalmodel_set, (mod) => { return mod.value.title; });
+        let coordinators = _.sortBy(this.project.coordinator_set, (coor) => { return coor.value.title; });
+        let coordOutputs = _.sortBy(this.project.coordinatoroutput_set, (cout) => { return cout.value.title; });
+        let numericals = _.sortBy(this.project.simsubstructure_set, (num) => { return num.value.title; });
+        let simOutputs = _.sortBy(this.project.simoutput_set, (sout) => { return sout.value.title; });
+        let experimentals = _.sortBy(this.project.expsubstructure_set, (exp) => { return exp.value.title; });
+        let expOutputs = _.sortBy(this.project.expoutput_set, (eout) => { return eout.value.title; });
+        let reports = _.sortBy(this.project.report_set, (rep) => { return rep.value.title; });
+        let analysis = _.sortBy(this.project.analysis_set, (ana) => { return ana.value.title; });
+        let roots = [];
+        _.each(simulations, (sim) => {
+            let node = {
+                name: sim.value.title,
+                uuid: sim.uuid,
+                parent: null,
+                children: [],
+                rectStyle: 'stroke: none;'
+            };
+            _.each(reports, (rep) => {
+                if (!_.contains(rep.associationIds, node.uuid)){
+                    return;
+                }
+                let repNode = {
+                    name: rep.value.title,
+                    uuid: rep.uuid,
+                    parent: node.name,
+                    rectStyle: 'stroke: #3E3E3E; fill: #C4C4C4;'
+                };
+                node.children.push(repNode);
+            });
+            node.children.push(
+                {
+                    name: '-- Choose a Report --',
+                    attr: 'report_set',
+                    entityType: 'report',
+                }
+            );
+            _.each(models, (mod) => {
+                if (!_.contains(mod.associationIds, node.uuid)){
+                    return;
+                }
+                let modNode = {
+                    name: mod.value.title,
+                    uuid: mod.uuid,
+                    parent: node.name,
+                    children: [],
+                    rectStyle: 'stroke: #1568C9; fill: #C4D9F2;',
+                };
+                _.each(coordinators, (coord) => {
+                    if (_.difference([mod.uuid, node.uuid], coord.associationIds).length){
+                        return;
+                    }
+                    let coordNode = {
+                        name: coord.value.title,
+                        uuid: coord.uuid,
+                        parent: modNode.name,
+                        children: [],
+                        rectStyle: 'stroke: #43A59D; fill: #CAE9E6;',
+                    };
+                    _.each(coordOutputs, (output) => {
+                        if (_.difference([coord.uuid, mod.uuid, sim.uuid], output.associationIds).length){
+                            return;
+                        }
+                        let outNode = {
+                            name: output.value.title,
+                            uuid: output.uuid,
+                            parent: coordNode.name,
+                            children: [],
+                            rectStyle: 'stroke: #D04348; fill: #D04348;',
+                        };
+                        coordNode.children.push(outNode);
+                    });
+                    coordNode.children.push(
+                        {
+                            name: '-- Choose an Output --',
+                            attr: 'output_set',
+                            entityType: 'output',
+                        }
+                    );
+
+                    _.each(numericals, (num) => {
+                        if (_.difference([coord.uuid, mod.uuid, sim.uuid], num.associationIds).length){
+                            return;
+                        }
+                        let numNode = {
+                            name: num.value.title,
+                            uuid: num.uuid,
+                            parent: coordNode.name,
+                            children: [],
+                            rectStyle: 'stroke: #B59300; fill: #B59300;',
+                        };
+                        _.each(simOutputs, (out) => {
+                            if (_.difference([num.uuid, mod.uuid, sim.uuid], out.associationIds).length){
+                                return;
+                            }
+                            let outNode = {
+                                name: out.value.title,
+                                uuid: out.uuid,
+                                parent: num.name,
+                                children: [],
+                                rectStyle: 'stroke: #D04348; fill: #D04348;'
+                            };
+                            numNode.children.push(outNode);
+                        });
+                        numNode.children.push(
+                            {
+                                name: '-- Choose a Simulation Output --',
+                                attr: 'simoutput_set',
+                                entityType: 'simOutput',
+                            }
+                        );
+                        coordNode.children.push(numNode);
+                    });
+                    coordNode.children.push(
+                        {
+                            name: '-- Choose a Numerical Substructure --',
+                            attr: 'simsubstructure_set',
+                            entityType: 'simSubstructure',
+                        }
+                    );
+                    _.each(experimentals, (exp) => {
+                        if (_.difference([coord.uuid, mod.uuid, sim.uuid], exp.associationIds).length){
+                            return;
+                        }
+                        let expNode = {
+                            name: exp.value.title,
+                            uuid: exp.uuid,
+                            parent: coordNode.name,
+                            children: [],
+                            rectStyle: 'stroke: #865AA7; fill: #865AA7;',
+                        };
+                        _.each(expOutputs, (out) => {
+                            if (_.difference([exp.uuid, mod.uuid, sim.uuid], out.associationIds).length){
+                                return;
+                            }
+                            let outNode = {
+                                name: out.value.title,
+                                uuid: out.uuid,
+                                parent: exp.name,
+                                children: [],
+                                rectStyle: 'stroke: #D04348; fill: #D04348;'
+                            };
+                            expNode.children.push(outNode);
+                        });
+                        expNode.children.push(
+                            {
+                                name: '-- Choose a Experimental Output --',
+                                attr: 'expoutput_set',
+                                entityType: 'expOutput',
+                            }
+                        );
+                        coordNode.children.push(expNode);
+                    });
+                    coordNode.children.push(
+                        {
+                            name: '-- Choose a Experimental Substructure --',
+                            attr: 'expsubstructure_set',
+                            entityType: 'expSubstructure',
+                        }
+                    );
+
+                    modNode.children.push(coordNode);
+                });
+                modNode.children.push(
+                    {
+                        name: '-- Choose an Master Coordinator --',
+                        attr: 'input_set',
+                        entityType: 'input',
+                    }
+                );
+                node.children.push(modNode);
+            });
+            node.children.push(
+                {
+                    name: '-- Choose a Simulation Model --',
+                    attr: 'model_set',
+                    entityType: 'model',
+                }
+            );
+            _.each(analysis, (ana) => {
+                if (!_.contains(ana.associationIds, node.uuid)){
+                    return;
+                }
+                let anaNode = {
+                    name: ana.value.title,
+                    uuid: ana.uuid,
+                    parent: node.name,
+                    rectStyle: 'stroke: #56C0E0; fill: #CCECF6;'
+                };
+                node.children.push(anaNode);
+            });
+            node.children.push(
+                {
+                    name: '-- Choose an Analysis --',
+                    attr: 'analysis_set',
+                    entityType: 'analysis',
+                }
+            );
+            roots.push(node);
+        });
+        this.trees = roots;
+    }
+
+    /*
+     * @method
+     *
+     * Build tress for every experiment in this class' `this.project`.
+     * The hierarchy is build like so:
+     * + Simulation 1
+     * |
+     * -- + Report
+     * |
+     * -- + Simulation Model
+     * | |
+     * | -- + Simulation Input
+     * |    |
+     * |    -- + Simulation Output
+     * |
+     * -- + Analysis
+     *
+     * + Simulation 2
+     * [...]
+     */
+    buildSimulationTree() {
+        let simulations = _.sortBy(this.project.simulation_set, (sim) => { return sim.value.title; });
+        let models = _.sortBy(this.project.model_set, (mod) => { return mod.value.title; });
+        let inputs = _.sortBy(this.project.input_set, (inp) => { return inp.value.title; });
+        let outputs = _.sortBy(this.project.output_set, (out) => { return out.value.title; });
+        let reports = _.sortBy(this.project.report_set, (rep) => { return rep.value.title; });
+        let analysis = _.sortBy(this.project.analysis_set, (ana) => { return ana.value.title; });
+        let roots = [];
+        _.each(simulations, (sim) => {
+            let node = {
+                name: sim.value.title,
+                uuid: sim.uuid,
+                parent: null,
+                children: [],
+                rectStyle: 'stroke: none;'
+            };
+            _.each(reports, (rep) => {
+                if (!_.contains(rep.associationIds, node.uuid)){
+                    return;
+                }
+                let repNode = {
+                    name: rep.value.title,
+                    uuid: rep.uuid,
+                    parent: node.name,
+                    rectStyle: 'stroke: #3E3E3E; fill: #C4C4C4;'
+                };
+                node.children.push(repNode);
+            });
+            node.children.push(
+                {
+                    name: '-- Choose a Report --',
+                    attr: 'report_set',
+                    entityType: 'report',
+                }
+            );
+            _.each(models, (mod) => {
+                if (!_.contains(mod.associationIds, node.uuid)){
+                    return;
+                }
+                let modNode = {
+                    name: mod.value.title,
+                    uuid: mod.uuid,
+                    parent: node.name,
+                    children: [],
+                    rectStyle: 'stroke: #1568C9; fill: #C4D9F2;',
+                };
+                _.each(inputs, (input) => {
+                    if (_.difference([mod.uuid, node.uuid], input.associationIds).length){
+                        return;
+                    }
+                    let inpNode = {
+                        name: input.value.title,
+                        uuid: input.uuid,
+                        parent: modNode.name,
+                        children: [],
+                        rectStyle: 'stroke: #43A59D; fill: #CAE9E6;',
+                    };
+                    _.each(outputs, (output) => {
+                        if (_.difference([input.uuid, modNode.uuid, node.uuid], output.associationIds).length){
+                            return;
+                        }
+                        let outNode = {
+                            name: output.value.title,
+                            uuid: output.uuid,
+                            parent: inpNode.name,
+                            children: [],
+                            rectStyle: 'stroke: #B59300; fill: #B59300;',
+                        };
+                        inpNode.children.push(outNode);
+                    });
+                    inpNode.children.push(
+                        {
+                            name: '-- Choose an Output --',
+                            attr: 'output_set',
+                            entityType: 'output',
+                        }
+                    );
+                    modNode.children.push(inpNode);
+                });
+                modNode.children.push(
+                    {
+                        name: '-- Choose an Input --',
+                        attr: 'input_set',
+                        entityType: 'input',
+                    }
+                );
+                node.children.push(modNode);
+            });
+            node.children.push(
+                {
+                    name: '-- Choose a Simulation Model --',
+                    attr: 'model_set',
+                    entityType: 'model',
+                }
+            );
+            _.each(analysis, (ana) => {
+                if (!_.contains(ana.associationIds, node.uuid)){
+                    return;
+                }
+                let anaNode = {
+                    name: ana.value.title,
+                    uuid: ana.uuid,
+                    parent: node.name,
+                    rectStyle: 'stroke: #56C0E0; fill: #CCECF6;'
+                };
+                node.children.push(anaNode);
+            });
+            node.children.push(
+                {
+                    name: '-- Choose an Analysis --',
+                    attr: 'analysis_set',
+                    entityType: 'analysis',
+                }
+            );
+            roots.push(node);
+        });
+        this.trees = roots;
+    }
+
+    /*
+     * @method
+     *
      * Build tress for every experiment in this class' `this.project`.
      * The hierarchy is build like so:
      * + Experiment 1
