7d476f67ed4d100df43dfd1d435e64d269523b39
Elias Chalhoub <echalhoub@tacc.utexas.edu>
2019-02-21 10:06:17 -0600

    Created page_alerts.py and added import to workspace/admin

--- designsafe/apps/workspace/admin.py
+++ designsafe/apps/workspace/admin.py
@@ -1,4 +1,6 @@
 from django.contrib import admin
 from designsafe.apps.workspace.models.app_descriptions import AppDescription
+from designsafe.apps.workspace.models.page_alerts import PageAlerts
 
 admin.site.register(AppDescription)
+#admin.site.register(PageAlerts)
