8448c11a1303dc204366a38a2d72f3a010d94d00
Strmiska <keith.strmiska@gmail.com>
2019-02-25 11:07:00 -0600

    removed requirement for second date and disabled 123 buttons while loading

--- designsafe/static/scripts/data-depot/components/projects/pipeline-selection/pipeline-selection.component.js
+++ designsafe/static/scripts/data-depot/components/projects/pipeline-selection/pipeline-selection.component.js
@@ -134,6 +134,13 @@ class PipelineSelectionCtrl {
 
     }
 
+    hasEndDate(date) {
+        if (Date.parse(date)) {
+            return true;
+        }
+        return false;
+    }
+
     matchingGroup(exp, model) {
         if (!exp) {
             // if the category is related to the project level
