698891ba00636ad0b18636bcc2e6dcea9a010d43
Jake Rosenberg <jrosenberg@tacc.utexas.edu>
2019-03-08 11:03:54 -0600

    Task/des 879: Port indexer from CEP and fix duplicates/bad paths (#416)
    * Implement CEP indexer and reindexing workflow
    
    * fix limit in repair util function

---
+++ designsafe/apps/data/management/commands/swap_reindex.py
@@ -0,0 +1,76 @@
+import logging 
+from django.core.management import BaseCommand, CommandError
+from django.utils.six.moves import input
+from django.conf import settings
+import elasticsearch
+from elasticsearch import TransportError
+from elasticsearch_dsl import Index
+from elasticsearch_dsl.connections import connections
+from designsafe.libs.elasticsearch.indices import setup_index
+
+class Command(BaseCommand):
+    """
+    This command reindexes all documents in the default files index in order to 
+    apply new mappings/analyzers. It does NOT crawl Agave for file metadata, it
+    only uses data that already exists in the file index. Usage is as simple as 
+    running `./manage.py reindex-files`. 
+
+    This works by resetting the index aliased as settings.ES_REINDEX_INDEX_ALIAS
+    (applying any new mappings/analyzers defined in the portal.libs.elasticsearch.docs.base.IndexedFile
+     class) and reindexing from the default files index to this new index. The 
+     aliases are then swapped so that any Elasticsearch queries on the backend now 
+     target the reindexed documents. 
+    """
+
+    help = "Reindex all files into a fresh index, then swap aliases with the current default index."
+
+    def add_arguments(self, parser):
+        parser.add_argument('--index', help='key in settings.ES_INDICES to reindex')
+        parser.add_argument('--cleanup', help='Remove documents after swapping aliases to save space.', default=False, action='store_true')
+        parser.add_argument('--swap-only', help='Only swap index aliases without reindexing.', default=False, action='store_true')
+
+    def handle(self, *args, **options):
+        HOSTS = settings.ES_CONNECTIONS[settings.DESIGNSAFE_ENVIRONMENT]['hosts']
+        connections.configure(default={'hosts': HOSTS})
+        es_client = elasticsearch.Elasticsearch(hosts=HOSTS)
+        index = options.get('index')
+        cleanup = options.get('cleanup')
+        swap_only = options.get('swap-only')
+
+        index_config = settings.ES_INDICES[index]
+
+        default_index_alias = index_config['alias'][0]
+        reindex_index_alias = default_index_alias + '_reindex'
+
+        if not swap_only:
+            confirm = input('This will delete any documents in the index "{}" and recreate the index. Continue? (Y/n) '.format(reindex_index_alias))
+            if confirm != 'Y':
+                raise SystemExit
+            # Set up a fresh reindexing alias.
+            setup_index(index_config, force=True, reindex=True)
+
+        try:
+            default_index_name = Index(default_index_alias).get_alias().keys()[0]
+            reindex_index_name = Index(reindex_index_alias).get_alias().keys()[0]
+        except Exception as e:
+            self.stdout.write('Unable to lookup required indices by alias. Have you set up both a default and a reindexing index?')
+            raise SystemExit
+
+        if not swap_only:
+            # Reindex file metadata from the default index to the reindexing index
+            elasticsearch.helpers.reindex(es_client, default_index_name, reindex_index_name)
+
+        alias_body = {
+            'actions': [
+                {'remove': {'index': default_index_name, 'alias': default_index_alias}},
+                {'remove': {'index': reindex_index_name, 'alias': reindex_index_alias}},
+                {'add': {'index': default_index_name, 'alias': reindex_index_alias}},
+                {'add': {'index': reindex_index_name, 'alias': default_index_alias}},
+            ]
+        }
+        # Swap the aliases of the default and reindexing aliases.
+        es_client.indices.update_aliases(alias_body)
+
+        # Re-initialize the new reindexing index to save space.
+        if cleanup:
+            Index(reindex_index_alias).delete(ignore=404)
---
+++ designsafe/apps/data/management/commands/tests.py
@@ -0,0 +1,94 @@
+from mock import Mock, patch, MagicMock, PropertyMock, call
+from django.test import TestCase
+from django.contrib.auth import get_user_model
+from django.conf import settings
+from django.core.management import call_command
+import datetime
+
+
+class TestSwapReindex(TestCase):
+
+    def setUp(self):
+        self.patch_setup = patch('designsafe.apps.data.management.commands.swap_reindex.setup_index')
+        self.patch_connections = patch('designsafe.apps.data.management.commands.swap_reindex.connections')
+        self.patch_elasticsearch = patch('designsafe.apps.data.management.commands.swap_reindex.elasticsearch')
+
+        self.mock_setup = self.patch_setup.start()
+        self.mock_connections = self.patch_connections.start()
+        self.mock_elasticsearch = self.patch_elasticsearch.start()
+
+        self.addCleanup(self.patch_setup.stop)
+        self.addCleanup(self.patch_connections.stop)
+        self.addCleanup(self.patch_elasticsearch.stop)
+
+    @patch('designsafe.apps.data.management.commands.swap_reindex.Command.handle')
+    def test_working(self, mock_handle):
+        opts = {'index': 'files'}
+        call_command('swap_reindex', **opts)
+        self.assertEqual(mock_handle.call_count, 1)
+
+    @patch('designsafe.apps.data.management.commands.swap_reindex.input')
+    def test_raises_when_user_does_not_proceed(self, mock_input):
+        mock_input.return_value = 'n'
+        opts = {'index': 'files'}
+        with self.assertRaises(SystemExit):
+            call_command('swap_reindex', **opts)
+
+    @patch('designsafe.apps.data.management.commands.swap_reindex.Index')
+    @patch('designsafe.apps.data.management.commands.swap_reindex.input')
+    def test_raises_exception_when_no_index(self, mock_input, mock_index):
+        mock_input.return_value = 'Y'
+        
+        mock_index.return_value.get_alias.return_value.keys.side_effect = Exception
+        opts = {'index': 'files'}
+        
+        with self.assertRaises(SystemExit):
+            call_command('swap_reindex', **opts)
+
+    @patch('designsafe.apps.data.management.commands.swap_reindex.Index')
+    @patch('designsafe.apps.data.management.commands.swap_reindex.input')
+    def test_performs_reindex_from_default_to_reindex(self, mock_input, mock_index):
+        mock_input.return_value = 'Y'
+
+        mock_index.return_value.get_alias.return_value.keys.side_effect = [['DEFAULT_NAME'], ['REINDEX_NAME']]
+        opts = {'index': 'files'}
+
+        mock_client = MagicMock()
+        self.mock_elasticsearch.Elasticsearch.return_value = mock_client
+
+        call_command('swap_reindex', **opts)
+
+        self.mock_elasticsearch.helpers.reindex.assert_called_with(mock_client, 'DEFAULT_NAME', 'REINDEX_NAME')
+
+    @patch('designsafe.apps.data.management.commands.swap_reindex.Index')
+    @patch('designsafe.apps.data.management.commands.swap_reindex.input')
+    def test_performs_swap_with_correct_args(self, mock_input, mock_index):
+        mock_input.return_value = 'Y'
+
+        mock_index.return_value.get_alias.return_value.keys.side_effect = [['DEFAULT_NAME'], ['REINDEX_NAME']]
+        opts = {'index': 'files'}
+
+        call_command('swap_reindex', **opts)
+
+        mock_alias = {
+            'actions': [
+                {'remove': {'index': 'DEFAULT_NAME', 'alias': 'des-files'}},
+                {'remove': {'index': 'REINDEX_NAME', 'alias': 'des-files_reindex'}},
+                {'add': {'index': 'DEFAULT_NAME', 'alias': 'des-files_reindex'}},
+                {'add': {'index': 'REINDEX_NAME', 'alias': 'des-files'}},
+            ]
+        }
+        self.mock_elasticsearch.Elasticsearch().indices.update_aliases.assert_called_with(mock_alias)
+
+    @patch('designsafe.apps.data.management.commands.swap_reindex.Index')
+    @patch('designsafe.apps.data.management.commands.swap_reindex.input')
+    def test_cleanup(self, mock_input, mock_index):
+        mock_input.return_value = 'Y'
+
+        mock_index.return_value.get_alias.return_value.keys.side_effect = [['DEFAULT_NAME'], ['REINDEX_NAME']]
+        opts = {'index': 'files', 'cleanup': True}
+
+        call_command('swap_reindex', **opts)
+
+        self.assertEqual(mock_index.return_value.delete.call_count, 1)
+
