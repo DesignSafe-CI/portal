d65bf8f1a6f1129158fbbf5f543a9e3465079583
Strmiska <keith.strmiska@gmail.com>
2019-04-15 10:43:55 -0500

    fix for issues with tagging files of the same category relation

--- designsafe/static/scripts/projects/components/file-categories/file-categories.component.js
+++ designsafe/static/scripts/projects/components/file-categories/file-categories.component.js
@@ -69,15 +69,6 @@ class FileCategoriesCtrl {
         this._ui.busy = true;
         let tagName = this.selectedFileTag[entity.uuid];
         getFileUuid(this.file).then((file)=>{
-            entity.value.fileTags = _.filter(
-                entity.value.fileTags,
-                (ft) => {
-                    if(ft.fileUuid !== file.uuid() && ft.tagName !== tagName) {
-                        return false;
-                    }
-                    return true;
-                }
-            );
             entity.value.fileTags.push({
                 fileUuid: file.uuid(),
                 tagName: tagName
@@ -92,40 +83,37 @@ class FileCategoriesCtrl {
         }).then((res) => {
             let prjEntity = this.project.getRelatedByUuid(res.uuid);
             prjEntity.update(res);
-            this._ui.busy = false;
         }).finally(() => {
+            this._ui.busy = false;
             this.selectedFileTag[entity.uuid] = null;
             this.$scope.$apply();
         });
     }
 
     removeFileTag(entity, tag) {
-        this._ui.busy = false;
+        this._ui.busy = true;
         let tagName = tag.tagName;
-        getFileUuid(this.file).then(() => {
-            entity.value.fileTags = _.filter(
-                entity.value.fileTags,
-                (ft) => {
-                    if(ft.fileUuid === tag.fileUuid && ft.tagName === tagName){
-                        return false;
-                    }
-                    return true;
+        
+        entity.value.fileTags = _.filter(
+            entity.value.fileTags,
+            (ft) => {
+                if(ft.fileUuid === tag.fileUuid && ft.tagName === tagName){
+                    return false;
                 }
-            );
-            return entity;
-        }).then((ent) => {
-            return this.ProjectEntitiesService.update({
-                data: {
-                    uuid: ent.uuid,
-                    entity: ent,
-                }
-            }).then((res) => {
-                let prjEntity = this.project.getRelatedByUuid(res.uuid);
-                prjEntity.update(res);
-            }).finally(() => {
-                this._ui.busy = false;
-                this.$scope.$apply();
-            });
+                return true;
+            }
+        );
+    
+        this.ProjectEntitiesService.update({
+            data: {
+                uuid: entity.uuid,
+                entity: entity,
+            }
+        }).then((res) => {
+            let prjEntity = this.project.getRelatedByUuid(res.uuid);
+            prjEntity.update(res);
+        }).finally(() => {
+            this._ui.busy = false;
         });
     }
 
