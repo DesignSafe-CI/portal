2c1c3d57eb9b7dba298067aa42d95de7d2e9c341
Alex <afields@tacc.utexas.edu>
2019-05-30 16:00:46 -0500

    resolved naming issues, deleted file es_setup

--- designsafe/apps/data/models/taxonomy.py
+++ designsafe/apps/data/models/taxonomy.py
@@ -4,18 +4,16 @@ from django.contrib.auth.models import User
 from django.contrib.auth.validators import ASCIIUsernameValidator
 
 
-
-
 class CustomUser(User):
     username_validator = ASCIIUsernameValidator()
 
 class Vocab(models.Model):
-    Term = models.CharField(max_length=40, unique=True)
-    Definition = models.TextField(max_length=200)
-    Source_of_Definition = models.UrlField(max_length=1000, unique=True)
-    Date = DateTimeField(default=datetime.now(), auto_now_add=True)
-    VocabBuilder = CustomUser()
-    # Permalink = 
+    term = models.CharField(max_length=40, unique=True)
+    definition = models.TextField(max_length=200)
+    source_of_definition = models.UrlField(max_length=1000, unique=True)
+    date = DateTimeField(default=datetime.now(), auto_now_add=True)
+    vocab_builder = CustomUser()
+    user = models.ForeignKey(settings.ASCIIUsernameValidator)
 
 class Board(models.Model):
     display = models.CharField(max_length=30, unique=True)
