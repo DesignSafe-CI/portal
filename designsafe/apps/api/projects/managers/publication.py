a403a0980e90f6b44dc0add846d4d9689cf41266
Keith G Strmiska <keith.strmiska@gmail.com>
2019-08-29 09:31:36 -0500

    Check for datetime obj or date string when setting creation date (#614) - DES-1253
    * check for datetime obj or date string when setting creation date in publications
    
    * adjust exception log message

--- designsafe/apps/api/projects/managers/publication.py
+++ designsafe/apps/api/projects/managers/publication.py
@@ -180,9 +180,15 @@ def _project_required_xml(project, authors, created, doi=None):
     publisher = ET.SubElement(resource, 'publisher')
     publisher.text = 'Designsafe-CI'
 
-    now = dateutil.parser.parse(created)
     publication_year = ET.SubElement(resource, 'publicationYear')
-    publication_year.text = str(now.year)
+    try:
+        if isinstance(created, datetime.date):
+            publication_year.text = str(created.year)
+        elif isinstance(created, basestring):
+            now = dateutil.parser.parse(created)
+            publication_year.text = str(now.year)
+    except:
+        logger.exception('Error formatting creation date:', extra={'value': created, 'type': type(created)})
 
     resource_type = ET.SubElement(resource, 'resourceType')
     resource_type.text = "Project/{}".format(project.project_type.title())
@@ -235,9 +241,15 @@ def _experiment_required_xml(authors_details, experiment, created, exp_doi=None)
     publisher = ET.SubElement(resource, 'publisher')
     publisher.text = 'Designsafe-CI'
 
-    now = dateutil.parser.parse(created)
     publication_year = ET.SubElement(resource, 'publicationYear')
-    publication_year.text = str(now.year)
+    try:
+        if isinstance(created, datetime.date):
+            publication_year.text = str(created.year)
+        elif isinstance(created, basestring):
+            now = dateutil.parser.parse(created)
+            publication_year.text = str(now.year)
+    except:
+        logger.exception('Error formatting creation date:', extra={'value': created, 'type': type(created)})
 
     resource_type = ET.SubElement(resource, 'resourceType')
     resource_type.text = "Experiment/{}".format(experiment.experiment_type.title())
@@ -276,9 +288,15 @@ def _analysis_required_xml(users, analysis, created):
     publisher = ET.SubElement(resource, 'publisher')
     publisher.text = 'Designsafe-CI'
 
-    now = dateutil.parser.parse(created)
     publication_year = ET.SubElement(resource, 'publicationYear')
-    publication_year.text = str(now.year)
+    try:
+        if isinstance(created, datetime.date):
+            publication_year.text = str(created.year)
+        elif isinstance(created, basestring):
+            now = dateutil.parser.parse(created)
+            publication_year.text = str(now.year)
+    except:
+        logger.exception('Error formatting creation date:', extra={'value': created, 'type': type(created)})
 
     resource_type = ET.SubElement(resource, 'resourceType')
     resource_type.text = 'Analysis'
@@ -331,9 +349,15 @@ def _simulation_required_xml(authors_details, simulation, created, sim_doi=None)
     publisher = ET.SubElement(resource, 'publisher')
     publisher.text = 'Designsafe-CI'
 
-    now = dateutil.parser.parse(created)
     publication_year = ET.SubElement(resource, 'publicationYear')
-    publication_year.text = str(now.year)
+    try:
+        if isinstance(created, datetime.date):
+            publication_year.text = str(created.year)
+        elif isinstance(created, basestring):
+            now = dateutil.parser.parse(created)
+            publication_year.text = str(now.year)
+    except:
+        logger.exception('Error formatting creation date:', extra={'value': created, 'type': type(created)})
 
     resource_type = ET.SubElement(resource, 'resourceType')
     resource_type.text = "Simulation/{}".format(
@@ -388,9 +412,15 @@ def _mission_required_xml(authors_details, mission, created, mis_doi=None):
     publisher = ET.SubElement(resource, 'publisher')
     publisher.text = 'Designsafe-CI'
 
-    now = dateutil.parser.parse(created)
     publication_year = ET.SubElement(resource, 'publicationYear')
-    publication_year.text = str(now.year)
+    try:
+        if isinstance(created, datetime.date):
+            publication_year.text = str(created.year)
+        elif isinstance(created, basestring):
+            now = dateutil.parser.parse(created)
+            publication_year.text = str(now.year)
+    except:
+        logger.exception('Error formatting creation date:', extra={'value': created, 'type': type(created)})
 
     resource_type = ET.SubElement(resource, 'resourceType')
     resource_type.text = "Mission/{}".format(
@@ -746,7 +776,15 @@ def project_reserve_xml(publication, project, authors_details=None):
         doi=publication['project'].get('doi')
     )
 
-    now = dateutil.parser.parse(publication['created'])
+    try:
+        if isinstance(publication['created'], datetime.date):
+            now = publication['created']
+        elif isinstance(publication['created'], basestring):
+            now = dateutil.parser.parse(publication['created'])
+    except:
+        logger.exception('Error formatting creation date:',
+                extra={'value': publication['created'], 'type': type(publication['created'])})
+    
     if not publication['project'].get('doi', ''):
         reserve_resp = _reserve_doi(xml_obj, TARGET_BASE.format(project_id=project.project_id))
         doi = reserve_resp
