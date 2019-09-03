e95c7d547062c7ef6feeff73a4ac46cf66f22d0a
Josue Balandrano Coronel <jbc@rmcomplexity.com>
2019-09-03 17:14:49 -0500

    Move import future.

--- designsafe/__init__.py
+++ designsafe/__init__.py
@@ -1,8 +1,9 @@
 """Desingsafe.
 """
+from __future__ import absolute_import
+
 __version__ = "v4.6.1-20190901"
 
-from __future__ import absolute_import
 
 # This will make sure the app is always imported when
 # Django starts so that shared_task will use this app.
