9717fbabdac4609b441aa7b727723cc81ce70d46
Owais Jamil <47395902+owaisj@users.noreply.github.com>
2019-11-14 14:21:32 -0600

    Task/DES-1326: NEES Experiment DOIs listed on publication page (#674)
    * Citation modal for NEES publication experiments
    
    * List of DOIs for relevant NEES publications in modal
    
    * Added newlines to DOI modal component files; Moved button design from inline to css class
    
    * Changed DOI list to use citation format each item
    
    * Changed button label for all DOIs

---
+++ designsafe/static/scripts/data-depot/components/modals/nees-doi-list.component.js
@@ -0,0 +1,37 @@
+import NeesDoiListTemplate from './nees-doi-list.template.html';
+
+class NeesDoiListCtrl {
+    constructor () {
+    }
+
+    $onInit() {
+        this.project = this.resolve.project;
+        this.experimentDois = this.project.metadata.experiments.map((exp)=> {
+            const authors = exp.creators.map((author) => `${author.lastName}, ${author.firstName}`).join('; ');
+            const year = exp.endDate.split('T')[0].split('-')[0];
+            const doi = exp.doi;
+            const doiUrl = `https://doi.org/${exp.doi}`;
+            const title = exp.title;
+            return {
+                citationString:
+             `${authors}, (${year}), "${title}", DesignSafe-CI [publisher], doi: ${doi}`,
+                doiUrl
+            };
+        });
+    }
+    
+    close() {
+        return;
+    }
+}
+
+export const NeesDoiListComponent = {
+    template: NeesDoiListTemplate,
+    controller: NeesDoiListCtrl,
+    controllerAs: '$ctrl',
+    bindings: {
+        resolve: '<',
+        close: '&',
+        dismiss: '&'
+    },
+};
