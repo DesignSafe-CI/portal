8f52d0d547c7513cda7c4e290dc28c59b8c31e9d
Owais Jamil <47395902+owaisj@users.noreply.github.com>
2019-12-12 13:28:13 -0600

    Bug/DES-895: NEES URLs are forwarding to malformed DesignSafe URLs (#696)
    * Redirect old NEES.org urls to search
    
    * Removed extra dependency
    
    * Altered regex to capture last number from url
    
    * Fixed redirect view

---
+++ designsafe/tests.py
@@ -0,0 +1,14 @@
+from django.test import TestCase
+from mock import patch
+from views import redirect_old_nees
+
+class RedirectTest(TestCase):
+    """
+    Assert that the redirect function is called 
+    using a query string made with part of the NEES ID
+    """
+    @patch('django.shortcuts.redirect')
+    def test_redirect_old_nees(self, mock_redirect):
+        mock_redirect.return_value = True
+        redirect_old_nees(685)
+        self.assertTrue(mock_redirect.called_with('/search/?query_string=NEES 0685'))
