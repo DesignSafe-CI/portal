5e466d53936f9db6caf6b12ee760a1c5e516af35
Strmiska <keith.strmiska@gmail.com>
2020-01-07 09:26:26 -0600

    make users submit a ticket to republish or overwrite their publications

--- designsafe/static/scripts/data-depot/components/projects/pipeline-project/pipeline-project.component.js
+++ designsafe/static/scripts/data-depot/components/projects/pipeline-project/pipeline-project.component.js
@@ -46,7 +46,7 @@ class PipelineProjectCtrl {
             } else if (this.projType === 'simulation') {
                 this.placeholder = 'Simulation';
             } else if (this.projType === 'hybrid_simulation') {
-                this.placeholder = 'Hybrid Simulation';
+                this.placeholder = 'Hybrid Sim';
             } else if (this.projType === 'field_recon') {
                 this.placeholder = 'Mission';
             }
