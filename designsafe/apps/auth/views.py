18b397d28eccdeb57c71915951bbec9f91a67fee
Jarosenb <jrosenberg@tacc.utexas.edu>
2020-03-12 15:25:52 -0500

    Update auth_state key generation

--- designsafe/apps/auth/views.py
+++ designsafe/apps/auth/views.py
@@ -5,6 +5,7 @@ from django.core.urlresolvers import reverse
 from django.core.exceptions import ObjectDoesNotExist
 from django.http import HttpResponseRedirect, HttpResponseBadRequest
 from django.shortcuts import render
+import secrets
 
 from .models import AgaveOAuthToken, AgaveServiceStatus
 from agavepy.agave import Agave
@@ -53,7 +54,8 @@ def login_options(request):
                             'unavailable.'
                 }
     except Exception as e:
-        logger.warn('Unable to check AgaveServiceStatus: %s', e.message)
+        logger.warn('Unable to check AgaveServiceStatus')
+        logger.warn(e)
         agave_status = None
         designsafe_status = None
 
@@ -73,7 +75,7 @@ def agave_oauth(request):
     client_key = getattr(settings, 'AGAVE_CLIENT_KEY')
 
     session = request.session
-    session['auth_state'] = os.urandom(24).encode('hex')
+    session['auth_state'] = secrets.token_hex(24)
     next_page = request.GET.get('next')
     if next_page:
         session['next'] = next_page
