DesignSafe-CI/portal

commit 3243d034bf098064359570f6d27071be3f9b6c0f

remove apps.api.data and create new indexing task (#562)

designsafe/apps/data/views/base.py
@@ -1,8 +1,6 @@
 import json
 import logging
 
-from designsafe.apps.api.data import lookup_file_manager
-from designsafe.apps.api.data.sources import SourcesApi
 from designsafe.apps.api.exceptions import ApiException
 from designsafe.apps.notifications.views import get_number_unread_notifications
 from django.conf import settings

designsafe/apps/api/data/__init__.py
@@ -1,46 +1,0 @@
-from designsafe.apps.api.data.agave.filemanager import (
-    FileManager as AgaveFileManager)
-from designsafe.apps.api.data.agave.public_filemanager import (
-    FileManager as PublicFileManager)
-from designsafe.apps.api.data.box.filemanager import FileManager as BoxFileManager
-# from designsafe.apps.api.data.dropbox.filemanager import FileManager as DropboxFileManager
-from designsafe.apps.api.tasks import box_download, box_upload, copy_public_to_mydata
-
-
-def lookup_file_manager(resource):
-    if resource == 'public':
-        return PublicFileManager
-    elif resource == 'box':
-        return BoxFileManager
-    # elif resource == 'dropbox':
-        # return DropboxFileManager
-    elif resource == 'agave':
-        return AgaveFileManager
-    else:
-        return None
-
-
-def lookup_transfer_service(src_resource, dest_resource):
-    if src_resource == 'box':
-        if dest_resource == 'agave':
-            return box_download
-
-    if src_resource == 'agave':
-        if dest_resource == 'box':
-            return box_upload
-
-    # if src_resource == 'dropbox':
-    #     if dest_resource == 'agave':
-    #         return dropbox_download
-
-    # if src_resource == 'agave':
-    #     if dest_resource == 'dropbox':
-    #         return dropbox_upload
-
-    if src_resource == 'public':
-        if dest_resource == 'box':
-            return box_upload
-        elif dest_resource == 'agave':
-            return copy_public_to_mydata
-
-    return None

designsafe/apps/api/data/abstract/filemanager.py
@@ -1,146 +1,0 @@
-from abc import ABCMeta, abstractmethod
-
-
-class AbstractFileManager:
-    """
-    Abstract Class to implement a data File Manager for different resources.
-    A FileManager class should subclass this class.
-    Notes: 
-        - This is supported by python 2.6+ and in 3.2+ we can also 
-          use @abstractclassmethod
-        - Arguments are not enforced by abstract classes.
-        - Subclasses of this class are called from the main class based data 
-          views in `designsafe.apps.api.data.view` as such:
-        >>> #fm is an instance of the corresponding AbstracFileManager subclass
-        >>> op = getattr(fm, operation)
-        >>> op(**kwargs)
-
-        - All required arguments are passed as `kwargs` dictionary. This means 
-          that you can implement the required method adding the necessary 
-          method arguments, e.g.:
-        >>> def file(self, file_id, action, path, **kwargs):
-
-    References:
-        https://docs.python.org/2/library/abc.html
-        http://zaiste.net/2013/01/abstract_classes_in_python/
-    >>> from designsafe.apps.api.data.filemanager import AbstractFileManager
-    >>> MyFileManager(AbstractFileManager):
-    ...
-    """
-    __metaclass__ = ABCMeta
-
-    @abstractmethod
-    def parse_file_id(self, file_id):
-        """
-        Stuff
-        """
-        raise NotImplementedError()
-
-    @abstractmethod
-    def listing(self, file_id, **kwargs):
-        """
-        Lists contents of a folder or details of a file.
-        When the main data view calls this method it will send all the keyword 
-        arguments as **kwargs. This means that `file_id` will be the URL path 
-        call after the necessary routing parts have been removed, i.e. It will 
-        not contain the host, operation type or resource type. 
-        See designsafe.apps.api.urls comments for more information.
-
-        Args:
-            file_id: String of file path to list
-
-        Returns:
-            Dictionary with two keys;
-              - resource: File System resource that we're listing
-              - files: Array of Api file-like objects
-        """
-        raise NotImplementedError()
-
-    @abstractmethod
-    def file(self, file_id, action, path = None, **kwargs):
-        """
-        Main method to manage a folder or file.
-        See the `listing` method for reference of `file_id`.
-        When the main data view calls this method it will send all the keyword 
-        arguments as **kwargs. 
-        If the HTTP Request is of type POST or GET the `kwargs` dictionary will 
-        contain the `request.boy` data (given it's a valid JSON object) as well 
-        as the uploaded files in the key `files` if any.
-
-        Notes:
-            - This method should route all the necessary file specific operations:
-              - move
-              - rename
-              - copy
-              - delete
-              An example is given below. The type of action should be in the request 
-              body JSON object. If it's necessary to specify another filepath for the 
-              operation this should also be part of the request JSON body with the 
-              key `path`. For instance, when copying a file the request JSON 
-              body object should look like this:
-              {
-                  "action": "copy",
-                  "path": "path/to/new/file"
-              }
-
-        Args:
-            file_id: Url file path.
-            action: File action to execute.
-            path: File path if necessary.
-
-        Returns:
-            Dictionary with two keys:
-              - resource: File System resource.
-              - file: File on which the operation was executed.
-        """
-        #get the action from this class.
-        file_op = getattr(self, action)
-        #execute passing the necessary arguments
-        file_op(file_id, path, **kwargs)
-        raise NotImplementedError()
-
-    @abstractmethod
-    def download(self, file_id, **kwargs):
-        """
-        Method to create a download link which will be passed onto 
-        the client to download a file.
-
-        Args:
-            file_id: Url file path
-
-        Returns:
-            Dictionary with two keys:
-                - link: Short lived link to directly download a file.
-                - name: File name to use for the download.
-        """
-        raise NotImplementedError()
-
-    @abstractmethod
-    def search(self, q, **kwargs):
-        """
-        Method to return search results based on a query.
-        The main data view will pass the query string as well as the keyword 
-        arguments in the dictionary `kwargs`. 
-        The seach query string should be accessible in `q`.
-
-        Args:
-            q: Search query string.
-
-        Returns:
-            Dictionary with two keys:
-                - resource: Resource name.
-                - files: Array of serializabe Api file-like objects
-        """
-        raise NotImplementedError()
-
-    @abstractmethod
-    def is_search(self, file_path):
-        """Function to check if a file path is a search path
-        """
-        raise NotImplementedError()
-
-    @abstractmethod
-    def is_shared(self, file_path):
-        """Function to check if a file path is a shared path
-        """
-        raise NotImplementedError()

designsafe/apps/api/data/abstract/files.py
@@ -1,98 +1,0 @@
-from abc import ABCMeta, abstractmethod, abstractproperty
-
-class AbstractFile:
-    """
-    Abstract Class to implement an Api file-like object.
-    This class is mainly for a centrlize documentation
-    on what a serializabe Api file-like object should
-    look like.
-
-    It is not terribly necessary to subclass from this class
-    when creating a custom File class but noe should
-    follow the structure here showed.
-    """
-    __metaclass__ = ABCMeta
-
-    @abstractproperty
-    def id(self):
-        """
-        Method to return file id
-
-        Returns:
-            A string representing a unique id of a file.
-        """
-        raise NotImplementedError()
-
-    @abstractproperty
-    def parent_path(self):
-        """
-        Method to return parent path
-
-        Returns:
-            A string representing the parent path of a file.
-
-        """
-        raise NotImplementedError()
-
-    @abstractproperty
-    def trail(self, **kwargs):
-        """
-        Returns trail list of this file object.
-        A trail list is a list containing an dict object for each
-        folder on this file's path. Each one of these dicts should
-        have enough information to render the breadcrumb in the frontend
-        and to be able to construct the API call to list each directory
-        
-        Example:
-            If the file this class represents has a path `path/to/file.txt`
-            and a parent path `path/to` then the returned list should 
-            look like this:
-            [
-                {
-                    "source": "agave",
-                    "system": "designsafe.storage.default",
-                    "id": "designsafe.storage.default/path",
-                    "path": "/",
-                    "name": "path"
-                },
-                {
-                    "source": "agave",
-                    "system": "designsafe.storage.default",
-                    "id": "designsafe.storage.default/path/to",
-                    "path": "path",
-                    "name": "to"
-                }
-            ]
-        
-        Returns:
-            A list with a dict for every object in the file trail path.
-            Each dict in the returned list should contain enough information
-            to construct a `listing` api call to list each folder.
-        """
-        raise NotImplementedError()
-
-    @abstractmethod
-    def to_dict(self, **kwargs):
-        """
-        Method to return a JSON serializable dict.
-
-        Example:
-            {
-                'source': self.source,
-                'system': None,
-                'id': self.id,
-                'type': self.type,
-                'path': self.path,
-                'name': self.name,
-                'ext': self.ext,
-                'size': self.size,
-                'lastModified': self.last_modified,
-                '_trail': self.trail,
-                '_actions': [],
-                '_pems': [],
-            }
-
-        Returns:
-            A JSON serializable dict representation of this object.
-        """
-        raise NotImplementedError()

designsafe/apps/api/data/agave/agave_object.py
@@ -1,70 +1,0 @@
-from agavepy.agave import AgaveException, Agave
-from agavepy.async import AgaveAsyncResponse, TimeoutError, Error
-from requests.exceptions import HTTPError
-from designsafe.apps.api.exceptions import ApiException
-import logging
-logger = logging.getLogger(__name__)
-
-
-class AgaveObject(object):
-    """
-    Main class for agave objects
-    """
-
-    def __init__(self, wrap = {}, **kwargs):
-        self._wrap = wrap
-        self.agave_client = None
-
-    def get_operation(self, a, op):
-        o = reduce(getattr, op.split("."), a)
-        return o
-
-    def exec_operation(self, op, **kwargs):
-        response = op(**kwargs)
-        return response
-
-    def call_operation(self, operation, raise_agave=False, **kwargs):
-        a = self.agave_client
-        op = self.get_operation(a, operation)
-        try:
-            logger.debug('Agave: calling {}, args: {}'.format(operation, kwargs))
-            response = self.exec_operation(op, **kwargs)
-        except (AgaveException, HTTPError) as e:
-            logger.error(e, exc_info = True, extra = kwargs)
-            if e.response.status_code < 500 or raise_agave:
-                raise
-            else:
-                d = {'operation': op}
-                d.update(kwargs)
-                raise ApiException(e.message, e.response.status_code,
-                                   response=e.response, extra=d)
-        return response
-
-    def split_filepath(self, path):
-        path = path.strip('/')
-        path, name =  os.path.split(path)
-        if path == '':
-            path = '/'
-        return path, name
-
-    def __getattr__(self, name):
-        """
-        Overwrite so we can access the wrapped dictionary as class properties.
-        We have to check if the name is a key in the wrapped dictionary
-        because `dict.get` returns `None` if the key doesn't exists. This is not ideal.
-        if a non existent attribute is accessed the value returned is `None` instead of
-        an AttributeError. If the name is not a key in the wrapped dictionary
-        raise an AttributeError exception.
-        """
-        #check if the string has an underscore. Don't pay attention to the first character
-        #we don't care if it starts with an underscore. 
-        if name[1:].find('_') > 0:
-            #convert from underscore to camelcase
-            name = ''.join(l.capitalize() for l in name.split('_'))
-            name = name[0].lower() + name[1:]
-
-        if name in self._wrap:
-            return self._wrap.get(name)
-        else:
-            raise AttributeError('\'AgaveObject\' has no attribute \'%s\'' % (name))
-    

designsafe/apps/api/data/agave/elasticsearch/utils.py
@@ -1,42 +1,0 @@
-from elasticsearch_dsl.query import Q
-import os
-import re
-import logging
-
-logger = logging.getLogger(__name__)
-
-def sanitize_query_string(q):
-    q = re.sub(r'(["+\-=><!\(\)\{\}\[\]\^~\*\?\:\\\/])', r'\\\1', q)
-    q = q.replace('&&', '\&&')
-    return q
-
-def files_wildcard_query(q, fields, min_should_match = 1):
-    wildcards = []
-    for f in fields:
-        d = {}
-        qs = q
-        if not f.endswith('_exact'):
-            qs = q.lower()
-        d[f] = {'value': '*%s*' % qs}
-        wildcards.append(Q({'wildcard': d}))
-
-    qo = Q('bool',
-              should = wildcards,
-              minimum_should_match = min_should_match
-             )
-    return qo
-
-def files_access_filter(username, system = None, deleted = False):
-    must_queries = [
-            Q({'term': {'deleted': deleted}})
-          ]
-    if not system.startswith('project-'):
-        must_queries.append(Q({'term': {'permissions.username': username}}))
-
-    if system is not None:
-        must_queries.append(Q({'term': {'systemId': system}}))
-
-    q = Q('bool',
-          must = must_queries
-          )
-    return q

commit 7288449e48823e8047a0825f0f0ec300de2f3415

Fix undefined name error introduced by 3243d034 by removing unreferenced test class

designsafe/apps/data/views/base.py
@@ -67,88 +67,6 @@ class DataDepotView(BasePublicTemplate):
         return context
 
 
-class DataBrowserTestView(BasePublicTemplate):
-
-    def login_rediect(self, request):
-        path = request.get_full_path()
-        resolved_login_url = resolve_url(settings.LOGIN_URL)
-        from django.contrib.auth.views import redirect_to_login
-        return redirect_to_login(
-            path, resolved_login_url)
-
-    @method_decorator(ensure_csrf_cookie)
-    def dispatch(self, request, *args, **kwargs):
-        try:
-            return super(BasePublicTemplate, self).dispatch(request, *args, **kwargs)
-        except PermissionDenied:
-            return self.login_rediect(request)
-
-    def get_context_data(self, **kwargs):
-        context = super(DataBrowserTestView, self).get_context_data(**kwargs)
-        logger.info('Get context Data')
-
-        context['unreadNotifications'] = get_number_unread_notifications(self.request)
-
-        resource = kwargs.pop('resource', None)
-        if resource is None:
-            if self.request.user.is_authenticated:
-                resource = 'agave'
-            else:
-                resource = 'public'
-
-        fm_cls = lookup_file_manager(resource)
-        if fm_cls is None:
-            raise Http404('Unknown resource')
-
-        file_path = kwargs.pop('file_path', None)
-        try:
-            fm = fm_cls(self.request.user)
-            if not fm.is_search(file_path):
-                listing = fm.listing(file_path)
-            else:
-                d = {}
-                d.update(kwargs)
-                d.update(self.request.GET.dict())
-                listing = fm.search(**d)
-
-        except ApiException as e:
-            fm = None
-            action_url = e.extra.get('action_url', None)
-            action_label = e.extra.get('action_label', None)
-            if action_url is None and e.response.status_code == 403:
-                action_url = '{}?next={}'.format(reverse('login'), self.request.path)
-                action_label = 'Log in'
-            listing = {
-                'source': resource,
-                'id': file_path,
-                '_error': {
-                    'status': e.response.status_code,
-                    'message': e.response.reason,
-                    'action_url': action_url,
-                    'action_label': action_label
-                },
-            }
-
-        sources_api = SourcesApi()
-        source_id = resource
-        if source_id == 'agave':
-            if fm is not None and fm.is_shared(file_path):
-                source_id = '$share'
-            else:
-                source_id = 'mydata'
-        current_source = sources_api.get(source_id)
-        sources_list = sources_api.list()
-        context['angular_init'] = json.dumps({
-            'currentSource': current_source,
-            'sources': sources_list,
-            'listing': listing,
-            'state': {
-                'search': fm.is_search(file_path) if fm is not None else False
-            }
-        })
-        return context
-
-
 class FileMediaView(View):
     systems_mappings = {
         'designsafe.storage.default': 'shared',