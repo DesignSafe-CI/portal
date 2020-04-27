23da1d8b54322aa63d40a947b8955ea1ed9d3ae7
Jarosenb <jrosenberg@tacc.utexas.edu>
2020-04-27 13:16:29 -0500

    prevent serializer from making 1 API call per search result

--- designsafe/apps/api/search/searchmanager/publications.py
+++ designsafe/apps/api/search/searchmanager/publications.py
@@ -111,7 +111,8 @@ class PublicationsSearchManager(BaseSearchManager):
         for hit in res:
             try:
                 getattr(hit, 'projectId')
-                children.append(BaseESPublication(**hit.to_dict()).to_file())
+                hit_to_file = BaseESPublication.hit_to_file(hit)
+                children.append(hit_to_file)
             except AttributeError:
                 children.append(BaseESPublicationLegacy(**hit.to_dict()).to_file())
 
