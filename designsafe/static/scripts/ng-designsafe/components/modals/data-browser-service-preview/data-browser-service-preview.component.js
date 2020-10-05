ffd849eb02f47fa2f03eb77a74e7860232fc3ce9
Owais Jamil <47395902+owaisj@users.noreply.github.com>
2020-10-05 13:47:23 -0500

    Task/DES-1711 (#794)
    * completed data depot refactor
    
    * nginject bugfix
    
    * merge in hotfixes
    
    * merge in hotfixes
    
    * fix broken npm build
    
    * fix display bugs
    
    * fix error messages for external resources; restrict rxjs syntax to services
    
    * reintroduce python changes
    
    * remove references to DataBrowserService/FileListingService from components
    
    * re-deprecate javascript
    
    * add additional logging on file operations; fix nees listings; fix upload modal; fix abstract listings for empty projects; stop multiple image carousels from opening
    
    * add ngInject to upload modal
    
    * UI tweaks for advanced search and modals
    
    * enable pytest; get all tests working; set up github action for tests
    
    * fix test settings
    
    * pipeline fixes
    
    * pipeline tweaks: remove date from file metadata test, add pytest install
    
    * fix accounts unit tests
    
    * Add frontend tests and fixtures
    
    * consolidate views and handlers
    
    * completed data depot refactor
    
    * nginject bugfix
    
    * merge in hotfixes
    
    * merge in hotfixes
    
    * fix broken npm build
    
    * fix display bugs
    
    * fix error messages for external resources; restrict rxjs syntax to services
    
    * reintroduce python changes
    
    * remove references to DataBrowserService/FileListingService from components
    
    * re-deprecate javascript
    
    * add additional logging on file operations; fix nees listings; fix upload modal; fix abstract listings for empty projects; stop multiple image carousels from opening
    
    * add ngInject to upload modal
    
    * UI tweaks for advanced search and modals
    
    * enable pytest; get all tests working; set up github action for tests
    
    * fix test settings
    
    * pipeline fixes
    
    * pipeline tweaks: remove date from file metadata test, add pytest install
    
    * fix accounts unit tests
    
    * Add frontend tests and fixtures
    
    * consolidate views and handlers
    
    * Stop transfer endpoint from failing when user doesn't have all resource types set up.
    
    * Use ProjectService to resolve projects in the pipeline
    
    * fix dropbox client lookup
    
    * Render Leaflet map from GeoJSON
    
    * Removed extra render call
    
    * remove errant logging/debug statements
    
    * fix Shared With Me by passing username to listing ops
    
    * Added geojson validator
    
    * Linting fixes
    
    * Fixed conflicts
    
    * kwarg bug fix, reinstalled front-end deps
    
    Co-authored-by: Jarosenb <jrosenberg@tacc.utexas.edu>

--- designsafe/static/scripts/ng-designsafe/components/modals/data-browser-service-preview/data-browser-service-preview.component.js
+++ designsafe/static/scripts/ng-designsafe/components/modals/data-browser-service-preview/data-browser-service-preview.component.js
@@ -1,5 +1,8 @@
 import DataBrowserServicePreviewTemplate from './data-browser-service-preview.component.html';
-import _ from 'underscore';
+import 'leaflet/dist/leaflet.css';
+import 'leaflet/dist/images/marker-shadow.png';
+import * as L from 'leaflet';
+import { valid as validateGeoJson }from 'geojson-validation';
 
 class DataBrowserServicePreviewCtrl {
     constructor($sce, $http, $scope, $state, FileListingService, FileOperationService, ProjectService, Django) {
@@ -42,6 +45,14 @@ class DataBrowserServicePreviewCtrl {
                     oReq.onload = (e) =>
                         e.target.response.text().then((text) => {
                             this.textContent = text;
+                            const extension = this.resolve.file.name.split('.').pop();
+                            if (extension.includes('json')) {
+                                const body = JSON.parse(text);
+                                // Pretty Print JSON
+                                this.textContent = JSON.stringify(body, null, 4);
+                                const isGeoJson = validateGeoJson(body);
+                                if (isGeoJson) this.renderGeoJson(body);
+                            }
                             this.loading = false;
                             this.$scope.$apply();
                         });
@@ -59,6 +70,7 @@ class DataBrowserServicePreviewCtrl {
                     oReq.send();
                 }
             },
+            // eslint-disable-next-line
             (err) => {
                 this.error = true;
                 this.loading = false;
@@ -102,10 +114,10 @@ class DataBrowserServicePreviewCtrl {
     isJupyter() {
         if (this.resolve.api !== 'agave') {
             return false;
-        } else {
-            let fileExtension = this.resolve.file.name.split('.').pop();
-            return fileExtension === 'ipynb';
-        }
+        } 
+        const fileExtension = this.resolve.file.name.split('.').pop();
+        return fileExtension === 'ipynb';
+
     }
 
     openInJupyter() {
@@ -133,6 +145,49 @@ class DataBrowserServicePreviewCtrl {
         const jupyterPath = `http://jupyter.designsafe-ci.org/user/${this.Django.user}/notebooks/${fileLocation}`;
         window.open(jupyterPath);
     }
+
+    renderGeoJson(data) {
+        const mapWrapper = document.getElementById('preview_map_wrapper');
+        mapWrapper.style.display = 'block';
+        
+        this.mapElement = document.createElement('div');
+        this.mapElement.setAttribute('id', 'preview_map');
+        this.mapElement.style.height = '500px';
+        mapWrapper.appendChild(this.mapElement);
+
+        this.map = L.map(this.mapElement);
+        this.mapError = { show: false, message: '' };
+
+        // Fix marker icon 404
+        delete L.Icon.Default.prototype._getIconUrl;
+        L.Icon.Default.mergeOptions({
+            iconRetinaUrl: require('leaflet/dist/images/marker-icon-2x.png'),
+            iconUrl: require('leaflet/dist/images/marker-icon.png'),
+            shadowUrl: require('leaflet/dist/images/marker-shadow.png'),
+        });
+
+        try {
+            L.tileLayer(
+                'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
+                {
+                    attribution:
+                        '&copy; \
+                        <a href="http://osm.org/copyright">OpenStreetMap</a> \
+                        contributors',
+                }
+            ).addTo(this.map);
+            const geoJson = L.geoJson(data).addTo(this.map);
+            this.map.fitBounds(geoJson.getBounds());
+        } catch(e) {
+            this.map.off();
+            this.map.remove();
+            this.mapError = { show: true, message: e.message };
+            const el = document.querySelector('#preview_map');
+            const { parentElement } = el;
+            parentElement.removeChild(el);
+        }
+    }
+
 }
 
 export const DataBrowserServicePreviewComponent = {
