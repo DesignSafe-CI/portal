eabcf34cba18182fe33910699378a71af4d5b547
Ian Park <contact@duckonomy.com>
2020-11-17 11:50:02 -0600

    DES-773 - Add message to notify duplicate email in Edit Profile (#815)
    * DES-773 - Add warning on submitting existing emails when editing user profile.
    
    * DES-773 - Redirect to edit page on error.
    
    * Use different redirect.
    
    * Fix and add tests for email check.
    
    * Update designsafe/apps/accounts/views.py
    
    Co-authored-by: Jake Rosenberg <jrosenberg@tacc.utexas.edu>
    
    * Remove previous error logic.
    
    * Remove previous error logic.
    
    Co-authored-by: Ian Park <duckonomy@gmail.com>
    Co-authored-by: Jake Rosenberg <jrosenberg@tacc.utexas.edu>

--- designsafe/apps/accounts/views.py
+++ designsafe/apps/accounts/views.py
@@ -332,6 +332,11 @@ def profile_edit(request):
     if request.method == 'POST':
         form = forms.UserProfileForm(request.POST, initial=tas_user)
         if form.is_valid() and pro_form.is_valid():
+            existing_user = (tas.get_user(email=form.cleaned_data['email']))
+            if existing_user and (existing_user['email'] != tas_user['email']):
+                messages.error(request, 'The submitted email already exists! Your email has not been updated!')
+                return HttpResponseRedirect(reverse('designsafe_accounts:profile_edit'))
+
             pro_form.save()
 
             data = form.cleaned_data
@@ -353,7 +358,7 @@ def profile_edit(request):
                 ds_profile.website = pro_data['website']
                 ds_profile.orcid_id = pro_data['orcid_id']
                 ds_profile.professional_level = pro_data['professional_level']
-                
+
             except ObjectDoesNotExist as e:
                 logger.info('exception e: {} {}'.format(type(e), e ))
                 ds_profile = DesignSafeProfile(
