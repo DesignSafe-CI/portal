f94594f919d2ca49e74103b490eb40bbe71daa0b
Keith G Strmiska <keith.strmiska@gmail.com>
2021-03-24 16:38:46 -0500

    Set up calls to back end for publication pipeline for versioning/amends (#856)

--- designsafe/static/scripts/data-depot/components/projects/pipeline-version/pipeline-version.component.js
+++ designsafe/static/scripts/data-depot/components/projects/pipeline-version/pipeline-version.component.js
@@ -5,15 +5,19 @@ class PipelineVersionCtrl {
     constructor(
         FileOperationService,
         FileListingService,
+        PublicationService,
         ProjectService,
         $state,
+        $http,
         $q
     ) {
         'ngInject';
         this.FileOperationService = FileOperationService;
         this.FileListingService = FileListingService;
+        this.PublicationService = PublicationService;
         this.ProjectService = ProjectService;
         this.$state = $state;
+        this.$http = $http;
         this.$q = $q;
     }
 
@@ -23,21 +27,31 @@ class PipelineVersionCtrl {
         };
         this.projectId = this.ProjectService.resolveParams.projectId;
         this.filePath = this.ProjectService.resolveParams.filePath;
-        this.selectedListing = null; // for handling selected files...
-        this.$q.all([
-            this.ProjectService.get({ uuid: this.projectId }),
-            this.FileListingService.browse({
-                section: 'main',
-                api: 'agave',
-                scheme: 'private',
-                system: 'project-' + this.projectId,
-                path: this.filePath,
-            }),
-        ]).then(([project, listing]) => {
-            this.project = project;
-            this.listing = listing;
-            this.ui.loading = false;
-        });
+        this.publication = this.ProjectService.resolveParams.publication;
+        this.selectedListing = this.ProjectService.resolveParams.selectedListing; // for handling selected files...
+        this.revisionText = '';
+        if (!this.publication) {
+            this.goStart();
+        } else {
+            this.$q.all([
+                this.ProjectService.get({ uuid: this.projectId }),
+                this.FileListingService.browse({
+                    section: 'main',
+                    api: 'agave',
+                    scheme: 'private',
+                    system: 'project-' + this.projectId,
+                    path: this.filePath,
+                }),
+            ]).then(([project, listing]) => {
+                this.project = project;
+                this.listing = listing;
+                this.pubData = {
+                    project: { uuid: this.project.uuid, value: { projectId: this.project.value.projectId } },
+                    license: this.publication.licenses,
+                };
+                this.ui.loading = false;
+            });
+        }
     }
 
     onBrowse(file) {
@@ -47,6 +61,28 @@ class PipelineVersionCtrl {
         else {
             this.FileOperationService.openPreviewModal({api: 'agave', scheme: 'private', file})
         }
+    }
+
+    submitVersion() {
+        // need a notification banner for issues...
+        // ex: missing selections/changelog or submission failure response
+        if (!this.revisionText.length) {
+            return this.ui.change = true;
+        }
+        this.ui.loading = true;
+        this.$http.post(
+            '/api/projects/publication/',
+            {
+                publication: this.pubData,
+                status: 'publishing',
+                revision: true,
+                revision_text: this.revisionText
+            }
+        ).then((resp) => {
+            this.ui.submitted = true;
+        }).finally( () => {
+            this.ui.loading = false;
+        });
     }
 
     saveSelections() {
@@ -70,7 +106,11 @@ class PipelineVersionCtrl {
     }
 
     goVersionChanges() {
-        this.$state.go('projects.pipelineVersionChanges', { projectId: this.projectId }, { reload: true });
+        this.$state.go('projects.pipelineVersionChanges', {
+            projectId: this.projectId,
+            publication: this.publication,
+            selectedListing: this.selectedListing
+        }, { reload: true });
     }
 }
 
