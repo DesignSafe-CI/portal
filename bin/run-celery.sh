0756f8ea4e1cb73caffdf7fe50af92351ed65e1b
Jarosenb <jrosenberg@tacc.utexas.edu>
2021-03-26 10:56:17 -0500

    bump Celery version and use unique hostnames for workers.

--- bin/run-celery.sh
+++ bin/run-celery.sh
@@ -2,5 +2,5 @@
 
 # Run Celery as the DesignSafe Community Account
 celery -A designsafe beat -l info --pidfile= --schedule=/tmp/celerybeat-schedule &
-celery -A designsafe worker -l info --autoscale=15,5 -Q indexing,files &
-celery -A designsafe worker -l info --autoscale=10,3 -Q default,api
+celery -A designsafe worker -l info --autoscale=15,5 -Q indexing,files -n designsafe_worker01 &
+celery -A designsafe worker -l info --autoscale=10,3 -Q default,api -n designsafe_worker02
