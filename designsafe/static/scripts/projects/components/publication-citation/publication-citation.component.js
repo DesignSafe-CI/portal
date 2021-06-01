bd45719428c4c2265ad3bc69ca16e4f1301be33a
Strmiska <keith.strmiska@gmail.com>
2021-06-01 17:27:12 -0500

    hotfix for citation modal regarding early publications

--- designsafe/static/scripts/projects/components/publication-citation/publication-citation.component.js
+++ designsafe/static/scripts/projects/components/publication-citation/publication-citation.component.js
@@ -15,33 +15,59 @@ class PublicationCitationCtrl {
         this.doi = '';
 
         if (this.entity && this.version !== 1) {
-            // entity
             this.auths = angular.copy(this.entity.authors);
             this.doi = this.entity.doi;
         } else if (this.version === 1) {
+            /* TODO: This needs some cleaning up.
+            Things to consider when refactoring:
+            1) First and Second generation publications
+            2) Do we need the publication object, or can we
+               simplify the parameters for this modal?
+            3) Different publishable entities
+            4) Version information
+            5) Missing or undefined fields
+            */
             this.doi = this.entity.doi
-            let authIds = [];
-            var publishers = this.publication.users.filter( (usr) => {
-                if (this.entity.name === 'designsafe.project' || this.entity.name === 'designsafe.project.analysis' || this.entity[0]) {
-                  return this.publication.project.value.coPis.includes(usr.username) ||
-                    usr.username === this.publication.project.value.pi;
+            var publishers = this.publication.users.filter((usr) => {
+                // early publications automatically set the pi and copi users as authors
+                if (
+                    this.entity.name === 'designsafe.project' ||
+                    this.entity.name === 'designsafe.project.analysis' ||
+                    this.entity[0]
+                ) {
+                    return ('coPis' in this.publication.project.value
+                        ? this.publication.project.value.coPis
+                            .concat(this.publication.project.value.pi)
+                            .includes(usr.username)
+                        : this.publication.project.value.pi === usr.username
+                    )
                 } else {
-                  return this.entity.value.authors.includes(usr.username);
+                    return this.entity.value.authors.includes(usr.username);
                 }
-              });
-              if (typeof this.entity.value.projectType !== 'undefined' && this.entity.value.projectType === 'other') {
-                publishers = this.publication.users;
-              }
-              publishers = publishers.sort ((p) => {
-                if (typeof p._ui[this.entity.uuid] !== 'undefined') {
-                  return p._ui[this.entity.uuid];
-                } else {
-                  return p._ui.order;
-                }
-              });
-    
-              this.auths = publishers.map(p => ({order: p._ui.order, fname: p.first_name, lname: p.last_name, authorship: true}))
+            });
 
+
+            if (
+                typeof this.entity.value.projectType !== 'undefined' &&
+                this.entity.value.projectType === 'other'
+            ) {
+                publishers = this.publication.users;
+            }
+
+
+            publishers = publishers.sort ((p) => {
+                if (typeof p._ui[this.entity.uuid] !== 'undefined') {
+                    return p._ui[this.entity.uuid];
+                } else {
+                    return p._ui.order;
+                }
+            });
+            this.auths = publishers.map((p) => ({
+                order: p._ui.order,
+                fname: p.first_name,
+                lname: p.last_name,
+                authorship: true
+            }));
         } else if (this.publication.project.value.projectType !== 'other') {
             // exp,hyb,sim,field 
             let authIds = [];
