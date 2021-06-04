5c6b31efa6eac29bc5185245505469dd0be94f0e
rstijerina <r.sal.tijerina@gmail.com>
2021-06-04 00:19:13 -0500

    form touch ups; added user data

--- designsafe/static/scripts/projects/components/leave-feedback-modal/leave-feedback-modal.component.js
+++ designsafe/static/scripts/projects/components/leave-feedback-modal/leave-feedback-modal.component.js
@@ -1,42 +1,50 @@
 import LeaveFeedbackModalTemplate from './leave-feedback-modal.template.html';
 
 class LeaveFeedbackModalCtrl {
-    constructor(TicketsService) {
+    constructor(TicketsService, Django) {
         'ngInject';
 
         this.TicketsService = TicketsService;
-        this.feedbackForm = {
-            name: '',
-            email: '',
-            body: '',
-        };
-        this.error = '';
-        this.submitted = false;
+        this.Django = Django;
+        this.success = false;
+        this.error = false;
+        this.submitting = false;
     }
 
     $onInit() {
         this.project = this.resolve.project;
+        this.resetForm();
+    }
+
+    resetForm() {
+        this.feedbackForm = {
+            name: `${this.Django.first_name} ${this.Django.last_name}`,
+            email: this.Django.email,
+            body: '',
+            subject: `Project Feedback for ${this.project.value.projectId}`,
+            projectId: this.project.value.projectId,
+            title: this.project.value.title,
+        };
     }
 
     onSubmit() {
-        this.submitted = false;
-        this.error = '';
-        this.feedbackForm.subject = `Project Feedback for ${this.project.value.projectId}`;
-        this.feedbackForm.projectId = this.project.value.projectId;
-        this.feedbackForm.title = this.project.value.title;
-        this.TicketsService.feedback(this.feedbackForm).then(
-            (resp) => {
-                this.submitted = true;
-                this.feedbackForm = {
-                    name: '',
-                    email: '',
-                    body: '',
-                };
-            },
-            (err) => {
-                this.error = err;
-            }
-        );
+        this.success = false;
+        this.error = false;
+        this.submitting = true;
+
+        this.TicketsService.feedback(this.feedbackForm)
+            .then(
+                () => {
+                    this.success = true;
+                    this.resetForm();
+                },
+                () => {
+                    this.error = true;
+                }
+            )
+            .finally(() => {
+                this.submitting = false;
+            });
     }
 }
 
