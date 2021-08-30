4a1d35921b28d407dcbd2e8e64cdb94bd9ea07e8
Jarosenb <jrosenberg@tacc.utexas.edu>
2021-08-30 16:46:22 -0500

    ingest works for experiment tree structure

--- designsafe/libs/fedora/fedora_operations.py
+++ designsafe/libs/fedora/fedora_operations.py
@@ -48,6 +48,12 @@ FEDORA_CONTEXT = {
     "issued": {
         "@id": "http://purl.org/dc/elements/1.1/issued"
     },
+    "date": {
+        "@id": "http://purl.org/dc/elements/1.1/date"
+    },
+    "_created": {
+        "@id": "http://purl.org/dc/elements/1.1/created"
+    },
     "publisher": {
         "@id": "http://purl.org/dc/elements/1.1/publisher"
     },
@@ -56,6 +62,12 @@ FEDORA_CONTEXT = {
     },
     "relation": {
         "@id": "http://purl.org/dc/elements/1.1/relation"
+    },
+    "isPartOf": {
+        "@id": "http://purl.org/dc/terms/isPartOf"
+    },
+    "hasPart": {
+        "@id": "http://purl.org/dc/terms/hasPart"
     },
     "entity": {
         "@id": "https://www.w3.org/TR/2013/REC-prov-o-20130430/#Entity"
@@ -130,7 +142,7 @@ def fedora_post(container_path, data_container=True):
         raise error
 
 
-def fedora_update(project_id, update_body={}):
+def fedora_update(container_path, update_body={}):
     """"
     Patches a Fedora container with updated metadata.
 
@@ -144,26 +156,25 @@ def fedora_update(project_id, update_body={}):
     -------
     dict: Publication metadata in compact JSON format.
     """
-    initial_data = fedora_get(project_id)
+    initial_data = fedora_get(container_path)
     updated_data = {**initial_data, **update_body}
     updated_data['@context'] = {**initial_data['@context'], **FEDORA_CONTEXT}
     updated_data['@type'] = initial_data['@type'] + ['http://purl.org/dc/dcmitype/Dataset']
-
     request = requests.put('{}{}/{}'.format(settings.FEDORA_URL,
-                                            PUBLICATIONS_CONTAINER, project_id),
+                                            PUBLICATIONS_CONTAINER, container_path),
                            auth=(settings.FEDORA_USERNAME,
                                  settings.FEDORA_PASSWORD),
                            headers=FEDORA_HEADERS,
                            data=json.dumps(updated_data))
     request.raise_for_status()
-    return fedora_get(project_id)
+    return fedora_get(container_path)
 
 
 def format_metadata_for_fedora(project_id, version=None):
     """
     Format a publication's metadata so that it can be ingested into Fedora.
     """
-    doc = IndexedPublication.from_id(project_id)
+    doc = IndexedPublication.from_id(project_id, revision=version)
     pub_meta = doc.project.value
 
     author_list = []
@@ -188,6 +199,10 @@ def format_metadata_for_fedora(project_id, version=None):
     identifiers += getattr(pub_meta, 'dois', [])
     identifiers += [getattr(doc.project, 'doi', None)]
 
+    project_type = pub_meta.projectType
+    if project_type == 'other':
+        project_type = getattr(pub_meta, 'dataType', "other"),
+
     fc_meta = {
         'title': pub_meta.title,
         'entity': pub_meta.title,
@@ -199,7 +214,7 @@ def format_metadata_for_fedora(project_id, version=None):
         'issued': doc.project.created.isoformat(),
         'generatedAtTime': doc.project.created.isoformat(),
         'contributor': contributors,
-        'type': getattr(pub_meta, 'dataType', "other"),
+        'type': project_type,
         'publisher': 'Designsafe',
     }
 
@@ -210,7 +225,10 @@ def format_metadata_for_fedora(project_id, version=None):
     associated_projects = getattr(pub_meta, 'associatedProjects', None)
     if associated_projects:
         references = list(map(lambda assoc: assoc['title'], associated_projects))
-        relation = list(map(lambda assoc: assoc['href'], associated_projects))
+        try:
+            relation = list(map(lambda assoc: assoc['href'], associated_projects))
+        except KeyError:
+            relation = []
 
         fc_meta['references'] = references
         fc_meta['relation'] = relation
@@ -286,7 +304,7 @@ def ingest_project(project_id, upload_files=True, version=None):
     if version:
         container_path = '{}v{}'.format(container_path, str(version))
     fedora_post(container_path)
-    project_meta = format_metadata_for_fedora(project_id)
+    project_meta = format_metadata_for_fedora(project_id, version=version)
     res = fedora_update(container_path, project_meta)
     if upload_files:
         ingest_files_other(container_path)
@@ -317,68 +335,202 @@ def walk_experimental(project_id):
     Returns
     -------
     dict: dict in form {'uuid-ex-1':
-                        {children: ['uuid-ex-2'],
-                        parent: 'uuid-ex-3'}}
+                        {'children': ['title of child 1', ...],
+                        'parent': 'title of parent',
+                        'container_path': 'path/relative/to/fcroot',
+                        'fedora_mapping': {}}}
     """
     from urllib import parse
     doc = IndexedPublication.from_id(project_id)
-    relation_map = {doc.project.uuid: {'children': []}}
+    relation_map = {doc.project.uuid: {'parent': None, 'children': []}}
+    relation_map[doc.project.uuid]['container_path'] = project_id
+    relation_map[doc.project.uuid]['fedora_mapping'] = format_metadata_for_fedora(project_id)
+
     experiments_list = doc.experimentsList
     for expt in experiments_list:
         # Do stuff with experiment.
+        expt_container_path = "{}/{}".format(project_id, expt.uuid)
         print('experiment ' + expt.value.title)
         relation_map[expt.uuid] = {'children': []}
-        relation_map[expt.uuid]['parent'] = doc.project.uuid
-        relation_map[doc.project.uuid]['children'].append(expt.uuid)
+        relation_map[expt.uuid]['parent'] = doc.project.value.title
+        relation_map[doc.project.uuid]['children'].append(expt.value.title)
+
+        relation_map[expt.uuid]['container_path'] = expt_container_path
+        relation_map[expt.uuid]['fedora_mapping'] = format_experiment(expt)
 
         reports = filter(
             lambda report: expt.uuid in report.value.experiments,
             getattr(doc, 'reportsList', []))
         for report in reports:
             # Do stuff with report.
+            container_path = "{}/{}".format(expt_container_path, report.uuid)
             print('\treport ' + report.value.title)
-            relation_map[report.uuid] = {}
-            relation_map[report.uuid]['parent'] = expt.uuid
-            relation_map[expt.uuid]['children'].append(report.uuid)
+            relation_map[report.uuid] = {'children': []}
+            relation_map[report.uuid]['parent'] = expt.value.title
+            relation_map[expt.uuid]['children'].append(report.value.title)
+
+            relation_map[report.uuid]['container_path'] = container_path
+            relation_map[report.uuid]['fedora_mapping'] = format_report(report)
 
         analysis_list = filter(
             lambda analysis: expt.uuid in analysis.value.experiments,
             getattr(doc, 'analysisList', []))
         for analysis in analysis_list:
             # Do stuff with analysis.
+            container_path = "{}/{}".format(expt_container_path, analysis.uuid)
             print('\tanalysis ' + analysis.value.title)
-            relation_map[analysis.uuid] = {}
-            relation_map[analysis.uuid]['parent'] = expt.uuid
-            relation_map[expt.uuid]['children'].append(analysis.uuid)
+            relation_map[analysis.uuid] = {'children': []}
+            relation_map[analysis.uuid]['parent'] = expt.value.title
+            relation_map[expt.uuid]['children'].append(analysis.value.title)
+
+            relation_map[analysis.uuid]['container_path'] = container_path
+            relation_map[analysis.uuid]['fedora_mapping'] = format_analysis(analysis)
 
         model_configs = filter(
             lambda model_config: expt.uuid in model_config.value.experiments,
             getattr(doc, 'modelConfigs', []))
         for mc in model_configs:
             # Do stuff with model config.
+            configs_container_path = "{}/{}".format(expt_container_path, mc.uuid)
             print('\tmodel config ' + mc.value.title)
             relation_map[mc.uuid] = {'children': []}
-            relation_map[mc.uuid]['parent'] = expt.uuid
-            relation_map[expt.uuid]['children'].append(mc.uuid)
+            relation_map[mc.uuid]['parent'] = expt.value.title
+            relation_map[expt.uuid]['children'].append(mc.value.title)
+
+            relation_map[mc.uuid]['container_path'] = configs_container_path
+            relation_map[mc.uuid]['fedora_mapping'] = format_model_config(mc)
 
             sensor_lists = filter(
                 lambda sensor_list: mc.uuid in sensor_list.value.modelConfigs,
                 getattr(doc, 'sensorLists', []))
             for sl in sensor_lists:
                 # Do stuff with sensor list.
+                sl_container_path = "{}/{}".format(configs_container_path, sl.uuid)
                 print('\t\tsensor list ' + sl.value.title)
                 relation_map[sl.uuid] = {'children': []}
-                relation_map[sl.uuid]['parent'] = mc.uuid
-                relation_map[mc.uuid]['children'].append(sl.uuid)
+                relation_map[sl.uuid]['parent'] = mc.value.title
+                relation_map[mc.uuid]['children'].append(sl.value.title)
+
+                relation_map[sl.uuid]['container_path'] = sl_container_path
+                relation_map[sl.uuid]['fedora_mapping'] = format_sensor_info(sl)
 
                 events = filter(
                     lambda event: sl.uuid in event.value.sensorLists,
                     getattr(doc, 'eventsList', []))
                 for event in events:
                     # Do stuff with events.
+                    evt_container_path = "{}/{}".format(sl_container_path, event.uuid)
                     print('\t\t\tevent ' + event.value.title)
-                    relation_map[event.uuid] = {}
-                    relation_map[event.uuid]['parent'] = mc.uuid
-                    relation_map[sl.uuid]['children'].append(event.uuid)
+                    relation_map[event.uuid] = {'children': []}
+                    relation_map[event.uuid]['parent'] = mc.value.title
+                    relation_map[sl.uuid]['children'].append(event.value.title)
+
+                    relation_map[event.uuid]['container_path'] = evt_container_path
+                    relation_map[event.uuid]['fedora_mapping'] = format_event(event)
+
+    for key in relation_map.keys():
+        relation_map[key]['fedora_mapping']['isPartOf'] = relation_map[key]['parent']
+        relation_map[key]['fedora_mapping']['hasPart'] = relation_map[key]['children']
 
     return relation_map
+
+
+def format_experiment(expt):
+    """
+    Map experiment to Datacite fields for Fedora.
+    """
+    meta = expt.value
+    equipment_type = meta.equipmentType
+    if equipment_type == 'other':
+        equipment_type = getattr(meta, 'equipmentTypeOther', 'other')
+    experiment_type = meta.experimentType
+    if experiment_type == 'other':
+        experiment_type = getattr(meta, 'experimentTypeOther', 'other')
+    experimental_facility = meta.experimentalFacility
+    if experimental_facility == 'other':
+        experimental_facility = getattr(meta, 'experimentalFacilityOther', 'other')
+
+    publication_date = expt.created.isoformat()
+
+    try:
+        start_date = meta.procedureStart
+    except AttributeError:
+        start_date = None
+
+    try:
+        authors = expt.authors
+        creator = list(map(lambda author: "{lname}, {fname}".format(**author.to_dict()), authors)),
+    except AttributeError:
+        creator = list(map(lambda username: _get_user_by_username(expt, username), meta.authors))
+
+    try:
+        dois = list(meta.dois)
+    except AttributeError:
+        dois = [expt.doi]
+
+    return {
+        'type': experiment_type,
+        'identifier': dois,
+        'contributor': experimental_facility,
+        'creator': creator,
+        'title': meta.title,
+        'description': meta.description,
+        'subject': equipment_type,
+        '_created': start_date,
+        'date': publication_date,
+        'publisher': 'Designsafe',
+    }
+
+
+def format_report(report):
+    return {
+        'type': 'report',
+        'title': report.value.title,
+        'description': report.value.description,
+    }
+
+
+def format_model_config(mc):
+    return {
+        'type': 'model configuration',
+        'title': mc.value.title,
+        'description': mc.value.description
+    }
+
+
+def format_sensor_info(si):
+    return {
+        'type': 'sensor information',
+        'title': si.value.title,
+        'description': si.value.description
+    }
+
+
+def format_event(event):
+    return {
+        'type': 'event',
+        'title': event.value.title,
+        'description': event.value.description
+    }
+
+
+def format_analysis(analysis):
+    return {
+        'type': 'analysis',
+        'title': analysis.value.title,
+        'description': analysis.value.description
+    }
+
+
+def ingest_project_experimental(project_id, upload_files=False, version=None):
+    """
+    Ingest a project into Fedora by creating a record in the repo, updating it
+    with the published metadata, and uploading its files.
+    """
+    container_path = project_id
+
+    walk_result = walk_experimental(project_id)
+    for entity in walk_result.values():
+        container_path = entity['container_path']
+        fedora_post(container_path)
+        res = fedora_update(container_path, entity['fedora_mapping'])
