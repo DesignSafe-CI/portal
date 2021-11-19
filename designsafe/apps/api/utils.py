8102af445ce2f7d95483fe5cc57226ebea05cea0
Jarosenb <jrosenberg@tacc.utexas.edu>
2021-11-19 15:10:45 -0600

    Log IP address and user agent in metrics

--- designsafe/apps/api/utils.py
+++ designsafe/apps/api/utils.py
@@ -21,4 +21,13 @@ def is_jwt(request):
     if val:
         return True
 
-    return False
+    return Fal
+
+
+def get_client_ip(request):
+    x_forwarded_for = request.META.get('HTTP_X_FORWARDED_FOR')
+    if x_forwarded_for:
+        ip = x_forwarded_for.split(',')[-1].strip()
+    else:
+        ip = request.META.get('REMOTE_ADDR')
+    return ip
--- designsafe/settings/common_settings.py
+++ designsafe/settings/common_settings.py
@@ -383,7 +383,7 @@ LOGGING = {
         },
         'metrics': {
             'format': '[METRICS] %(levelname)s %(module)s %(name)s.%(funcName)s:%(lineno)s:'
-                      ' %(message)s user=%(user)s sessionId=%(sessionId)s op=%(operation)s'
+                      ' %(message)s user=%(user)s ip=%(ip)s agent=%(agent)s sessionId=%(sessionId)s op=%(operation)s'
                       ' info=%(info)s'
         },
     },
