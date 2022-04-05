9d687a954047cf80a40cee8181d071905842fe8f
rstijerina <r.sal.tijerina@gmail.com>
2022-04-05 16:57:24 -0500

    remove token refresh attempt

--- designsafe/apps/api/decorators.py
+++ designsafe/apps/api/decorators.py
@@ -107,10 +107,6 @@ def agave_jwt_login(func):
             user.backend = 'django.contrib.auth.backends.ModelBackend',
             login(request, user)
 
-            # Refresh agave oauth token
-            if user.agave_oauth.expired:
-                user.agave_oauth.client.token.refresh()
-
         return func(request, *args, **kwargs)
 
     return decorated_function
