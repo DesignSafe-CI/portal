ae7390a60798c2cb0b7fd3d441e975820d39983c
Strmiska <keith.strmiska@gmail.com>
2022-04-28 18:30:38 -0500

    upgrades to amends process for experimental and fix notification styles

--- designsafe/static/scripts/data-depot/components/projects/pipeline-amend/pipeline-amend.component.js
+++ designsafe/static/scripts/data-depot/components/projects/pipeline-amend/pipeline-amend.component.js
@@ -34,74 +34,154 @@ class PipelineAmendCtrl {
             this.goStart();
         }
         if (!this.amendment) {
-            this.amendment = JSON.parse(JSON.stringify(this.publication));
-            const prj_type = this.publication.project.value.projectType;
-            const unamendableFields = {
-                'project': ['pi', 'coPis', 'teamMembers', 'guestMembers', 'projectId', 'projectType', 'title', 'teamOrder', 'fileTags', 'dois'],
-                'entity': ['title', 'dois', 'authors', 'fileTags', 'files', 'project'],
-                'experimentEntity': ['project', 'experiments', 'modelConfigs', 'sensorLists', 'files']
-            }
-            let prjEnts = this.project.getAllRelatedObjects();
-            let mapping = []
-            if (prj_type == 'experimental') {
-                mapping = [
-                    'experimentsList',
-                    'modelConfigs',
-                    'sensorLists',
-                    'eventsList',
-                    'analysisList',
-                    'reportsList'
-                ]
-            } // else if (prj_type == 'simulation') ...
-            mapping.forEach((fieldName) => {
-                this.amendment[fieldName].forEach((amendEntity) => {
-                    let prjEntity = prjEnts.find(ent => ent.uuid == amendEntity.uuid);
-                    if (!prjEntity) {
-                        this.ui.missing[amendEntity.uuid] = { 'title': amendEntity.value.title };
-                    } else {
-                        if ('dois' in amendEntity.value) {
-                            Object.keys(amendEntity.value).forEach((entKey) => {
-                                if (!unamendableFields.entity.includes(entKey)) {
-                                    amendEntity.value[entKey] = prjEntity.value[entKey];
-                                }
-                            });
-                        } else {
-                            Object.keys(amendEntity.value).forEach((entKey) => {
-                                if (!unamendableFields.experimentEntity.includes(entKey)) {
-                                    amendEntity.value[entKey] = prjEntity.value[entKey];
-                                }
-                            });
-                        }
-                    }
-                });
-            });
-            Object.keys(this.amendment.licenses).forEach((key) => {
-                if (this.amendment.licenses[key]) {
-                    this.ui.license = this.amendment.licenses[key];
-                    if (key === 'datasets') {
-                        this.ui.licenseType = 'curation-odc';
-                    } else if (key === 'software') {
-                        this.ui.licenseType = 'curation-gpl';
-                    } else if (key === 'works') {
-                        let subtype = (this.ui.license.includes('Attribution') ? 'share' : 'zero');
-                        this.ui.licenseType = `curation-cc-${subtype}`;
-                    }
-                }
-            });
+            /* Amendment Preview:
+            the amendment preview (this.amendment) is a combination of
+            the latest published version of the project with metadata from
+            the workspace project backfilled into the fields that are
+            amendable.
+            */
+            this.configureAmendment();
         }
         this.ui.loading = false;
     }
 
-    amendSubEntity(entity) {
-        this.$uibModal.open({
-            component: 'amendEntityModal',
+    configureAmendment(update='all') {
+        const prj_type = this.publication.project.value.projectType;
+        const unamendableFields = {
+            'project': ['pi', 'coPis', 'teamMembers', 'guestMembers', 'projectId', 'projectType', 'title', 'teamOrder', 'fileTags', 'dois'],
+            'entity': ['title', 'dois', 'authors', 'fileTags', 'files', 'project'],
+            'experimentEntity': ['project', 'experiments', 'modelConfigs', 'sensorLists', 'files']
+        }
+        let prjEnts = this.project.getAllRelatedObjects();
+
+        let primaryEntNames = [];
+        let secondaryEntNames = [];
+        if (prj_type == 'experimental') {
+            this.ui.primaryModalName = 'manageExperimentsModal';
+            this.ui.secondaryModalName = 'manageCategories';
+            primaryEntNames = ['experimentsList']
+            secondaryEntNames = [
+                'modelConfigs',
+                'sensorLists',
+                'eventsList',
+                'analysisList',
+                'reportsList'
+            ]
+        } // else if (prj_type == 'simulation') ...
+
+        if (update === 'all') {
+            this.amendment = JSON.parse(JSON.stringify(this.publication));
+        }
+        if (update === 'all' || update === 'project') { // update project fields...
+            Object.keys(this.amendment.project.value).forEach((prjKey) => {
+                if (!unamendableFields.project.includes(prjKey)) {
+                    this.amendment.project.value[prjKey] = this.project.value[prjKey];
+                }
+            });
+        }
+        if (update === 'all' || update === 'primary') { // update primary entity fields...
+            primaryEntNames.forEach((fieldName) => {
+                this.amendment[fieldName].forEach((amendEntity) => {
+                    let prjEntity = prjEnts.find(ent => ent.uuid === amendEntity.uuid);
+                    if (!prjEntity) {
+                        this.ui.missing[amendEntity.uuid] = { 'title': amendEntity.value.title };
+                    } else {
+                        Object.keys(amendEntity.value).forEach((entKey) => {
+                            if (!unamendableFields.entity.includes(entKey)) {
+                                amendEntity.value[entKey] = prjEntity.value[entKey];
+                            }
+                        });
+                    }
+                });
+            });
+        }
+        if (update === 'all' || update === 'secondary') { // update sub entity fields...
+            secondaryEntNames.forEach((fieldName) => {
+                this.amendment[fieldName].forEach((amendEntity) => {
+                    // find project ent that matches amended uuid
+                    let prjEntity = prjEnts.find(ent => ent.uuid === amendEntity.uuid);
+                    if (!prjEntity && update === 'all') {
+                        this.ui.missing[amendEntity.uuid] = { 'title': amendEntity.value.title };
+                    } else {
+                        Object.keys(amendEntity.value).forEach((entKey) => {
+                            if (!unamendableFields.experimentEntity.includes(entKey)) {
+                                amendEntity.value[entKey] = prjEntity.value[entKey];
+                            }
+                        });
+                    }
+                });
+            });
+        }
+        Object.keys(this.amendment.licenses).forEach((key) => {
+            if (this.amendment.licenses[key]) {
+                this.ui.license = this.amendment.licenses[key];
+                if (key === 'datasets') {
+                    this.ui.licenseType = 'curation-odc';
+                } else if (key === 'software') {
+                    this.ui.licenseType = 'curation-gpl';
+                } else if (key === 'works') {
+                    let subtype = (this.ui.license.includes('Attribution') ? 'share' : 'zero');
+                    this.ui.licenseType = `curation-cc-${subtype}`;
+                }
+            }
+        });
+    }
+
+    amendProject() {
+        return this.$uibModal.open({
+            component: 'amendProject',
             resolve: {
-                entity: () => entity,
-                missing: () => this.ui.missing,
+                project: () => this.project,
             },
             backdrop: 'static',
             size: 'lg',
+        }).closed.then((_) => {
+            this.configureAmendment('project');
         });
+    }
+
+    amendPrimaryEntity(amendedEnt) {
+        let prjEntity = this.project.getAllRelatedObjects()
+            .find(ent => ent.uuid == amendedEnt.uuid);
+        this.$uibModal.open({
+            component: this.ui.primaryModalName,
+            resolve: {
+                project: () => this.project,
+                edit: () => prjEntity,
+            },
+            backdrop: 'static',
+            size: 'lg',
+        }).closed.then((_) => {
+            this.configureAmendment('primary');
+        });
+    }
+
+    amendSecondaryEntity(amendedEnt) {
+        let prjEntity = this.project.getAllRelatedObjects()
+            .find(ent => ent.uuid == amendedEnt.uuid);        
+        if (prjEntity) {
+            this.$uibModal.open({
+                component: this.ui.secondaryModalName,
+                resolve: {
+                    project: () => this.project,
+                    edit: () => prjEntity,
+                },
+                backdrop: 'static',
+                size: 'lg',
+            }).closed.then((_) => {
+                this.configureAmendment('secondary');
+            });
+        } else {
+            this.$uibModal.open({
+                component: 'amendEntityModal',
+                resolve: {
+                    entity: () => amendedEnt,
+                    missing: () => this.ui.missing,
+                },
+                backdrop: 'static',
+                size: 'lg',
+            });
+        }
     }
 
     goCitation() {
