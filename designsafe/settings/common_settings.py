1a3934afcf5c9db5f229d95dd52020e40897fc4a
Sarah Gray <sgray@tacc.utexas.edu>
2022-05-16 11:55:53 -0500

    adding new celery task to end of publication pipeline with logs and email alerts if publication has missing files or empty folders

--- designsafe/settings/common_settings.py
+++ designsafe/settings/common_settings.py
@@ -431,6 +431,7 @@ MEETING_REQUEST_EMAIL = os.environ.get('MEETING_REQUEST_EMAIL', 'info@designsafe
 
 NEW_ACCOUNT_ALERT_EMAILS = os.environ.get('NEW_ACCOUNT_ALERT_EMAILS', 'no-reply@designsafe-ci.org,')
 PROJECT_ADMINS_EMAIL = ['maria@tacc.utexas.edu', 'gendlerk@tacc.utexas.edu']
+DEV_PROJECT_ADMINS_EMAIL = ['tbrown@tacc.utexas,edu','sgray@tacc.utexas.edu','keiths@tacc.utexas.edu']
 
 ###
 # Terms and Conditions
