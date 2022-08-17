8fa3a4e97a76688af8ef940b7de9947ac754fca8
Sarah Gray <sgray@tacc.utexas.edu>
2022-08-17 15:57:05 -0500

    initial change

--- designsafe/static/scripts/data-depot/components/projects/pipeline-start/pipeline-start.component.js
+++ designsafe/static/scripts/data-depot/components/projects/pipeline-start/pipeline-start.component.js
@@ -55,7 +55,10 @@ class PipelineStartCtrl {
                 }
                 case 'field_recon': {
                     this.ui.publicationComp = 'projects.pipelineSelectField'
+                    this.ui.amendComp = 'projects.amendFieldRecon'
                     this.ui.previewComp = 'projects.previewFieldRecon'
+                    this.ui.versionComp = ''
+                    this.ui.showAmendVersion = true;
                     break;
                 }
                 case 'other': {
