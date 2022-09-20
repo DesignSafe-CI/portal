0788941465e017badbf4ddc1b064dbdbb00667f0
Garrett Edmonds <edmondsgarrett@gmail.com>
2022-09-20 16:24:24 -0500

    Simplified license grab in view and generalized view to both MATLAB and LS-DYNA, determined now via URL

--- designsafe/apps/api/licenses/urls.py
+++ designsafe/apps/api/licenses/urls.py
@@ -1,7 +1,6 @@
 from django.conf.urls import url, include
-from designsafe.apps.api.licenses.views import MATLABLicenseView, LSDYNALicenseView
+from designsafe.apps.api.licenses.views import LicenseView
 
 urlpatterns = [
-    url(r'^matlablicense/(?P<username>[a-zA-Z0-9\-_\.]+)/$', MATLABLicenseView.as_view(), name='MATLABLicense'),
-    url(r'^lsdynalicense/(?P<username>[a-zA-Z0-9\-_\.]+)/$', LSDYNALicenseView.as_view(), name='LSDYNALicense')
+    url(r'^(?P<app_name>[a-zA-Z]+)/(?P<username>[a-zA-Z0-9\-_\.]+)/$', LicenseView.as_view(), name='License')
 ]
--- designsafe/apps/api/licenses/views.py
+++ designsafe/apps/api/licenses/views.py
@@ -2,7 +2,7 @@ from designsafe.apps.api.views import BaseApiView
 from designsafe.apps.api.mixins import SecureMixin
 from designsafe.libs.common.decorators import profile as profile_fn
 from django.contrib.auth import get_user_model
-from django.http.response import HttpResponseForbidden
+from django.http.response import HttpResponseForbidden, HttpResponseNotFound
 from django.http import JsonResponse
 from django.apps import apps
 from designsafe.apps.licenses.models import MATLABLicense, LSDYNALicense
@@ -13,36 +13,18 @@ logger = logging.getLogger(__name__)
 
 
 
-class MATLABLicenseView(SecureMixin, BaseApiView):
+class LicenseView(SecureMixin, BaseApiView):
     @profile_fn
-    def get(self, request, username):
+    def get(self, request, app_name, username):
         if not request.user.is_superuser:
             return HttpResponseForbidden()
+        
+        if app_name not in ['MATLAB','LSDYNA']:
+            return HttpResponseNotFound()
+        app_license = apps.get_model('designsafe_licenses', '{}License'.format(app_name))
 
-        MATLABLicense = apps.get_model('designsafe_licenses', 'MATLABLicense')
-        active_licenses = MATLABLicense.objects.all()
-        users_with_matlab_license = list(set([l.user for l in active_licenses]))
-        licenses = []
-        for user in users_with_matlab_license:
-            if user.username == username:
-                licenses = MATLABLicense.objects.filter(user=user)
+        user = get_user_model().objects.get(username=username)
+        licenses = app_license.objects.filter(user=user)
 
-        matlab_license = licenses[0].license_as_str() if len(licenses) > 0 else '' 
-        return JsonResponse({'license': matlab_license}, encoder=AgaveJSONEncoder)
-
-class LSDYNALicenseView(SecureMixin, BaseApiView):
-    @profile_fn
-    def get(self, request, username):
-        if not request.user.is_superuser:
-            return HttpResponseForbidden()
-
-        LSDYNALicense = apps.get_model('designsafe_licenses', 'LSDYNALicense')
-        active_licenses = LSDYNALicense.objects.all()
-        users_with_lsdyna_license = list(set([l.user for l in active_licenses]))
-        licenses = []
-        for user in users_with_lsdyna_license:
-            if user.username == username:
-                licenses = LSDYNALicense.objects.filter(user=user)
-
-        lsdyna_license = licenses[0].license_as_str() if len(licenses) > 0 else ''
-        return JsonResponse({'license': lsdyna_license}, encoder=AgaveJSONEncoder)
+        user_license = licenses[0].license_as_str() if len(licenses) > 0 else '' 
+        return JsonResponse({'license': user_license}, encoder=AgaveJSONEncoder)
