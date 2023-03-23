0aa8ae24a8047b01469272182dd9926094617960
Van Go <35277477+van-go@users.noreply.github.com>
2023-03-23 16:48:43 -0500

    des-2418-changes to hazmapper link

--- designsafe/static/scripts/data-depot/components/projects/_common/hazmapper-maps/hazmapper-maps.component.js
+++ designsafe/static/scripts/data-depot/components/projects/_common/hazmapper-maps/hazmapper-maps.component.js
@@ -11,22 +11,41 @@ class HazmapperMapsCtrl {
     filteredHazmapperMaps(readOnly) {
         const maps = this.maps ? this.maps : [];
 
-        maps.forEach((map) => {
-            switch (map.deployment) {
-                case 'production':
-                    map.href = `https://hazmapper.tacc.utexas.edu/hazmapper/project/${map.uuid}`;
-                    break;
-                case 'staging':
-                    map.href = `https://hazmapper.tacc.utexas.edu/staging/project/${map.uuid}`;
-                    break;
-                default:
-                    map.href = `http://hazmapper.local:4200/project/${map.uuid}`;
-            }
-            });
-
-            if (window.location.origin.includes('designsafe-ci.org')) {
-                return maps.filter((map) => map.deployment === 'production');
-            }  
+        if (!readOnly){
+            maps.forEach((map) => {
+              switch (map.deployment) {
+                  case 'production':
+                      map.href = `https://hazmapper.tacc.utexas.edu/hazmapper/project/${map.uuid}`;
+                      break;
+                  case 'staging':
+                      map.href = `https://hazmapper.tacc.utexas.edu/staging/project/${map.uuid}`;
+                      break;
+                  default:
+                      map.href = `http://hazmapper.local:4200/project/${map.uuid}`;
+              }
+              });
+  
+              if (window.location.origin.includes('designsafe-ci.org')) {
+                  return maps.filter((map) => map.deployment === 'production');
+              }  
+          } else {
+              maps.forEach((map) => {
+                  switch (map.deployment) {
+                      case 'production':
+                          map.href = `https://hazmapper.tacc.utexas.edu/hazmapper/project-public/${map.uuid}`;
+                          break;
+                      case 'staging':
+                          map.href = `https://hazmapper.tacc.utexas.edu/staging/project-public/${map.uuid}`;
+                          break;
+                      default:
+                          map.href = `http://hazmapper.local:4200/project-public/${map.uuid}`;
+                  }
+                  });
+  
+                  if (window.location.origin.includes('designsafe-ci.org')) {
+                      return maps.filter((map) => map.deployment === 'production');
+                  } 
+          }
     
         return maps;
     }
