e05e9f41242987e045ed0c77b1264b9089c55871
Jake Rosenberg <jrosenberg@MacBook-Pro.lan>
2023-10-30 21:48:46 -0500

    fix imports and add more stable debugging

--- manage.py
+++ manage.py
@@ -7,17 +7,9 @@ if __name__ == "__main__":
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "designsafe.settings")
 
     from django.core.management import execute_from_command_line
-    from django.conf import settings
 
     # This will make sure the app is always imported when
     # Django starts so that shared_task will use this app.
     from designsafe.celery import app as celery_app  # pylint:disable=unused-import
-
-    if settings.DEBUG:
-        if os.environ.get("RUN_MAIN"):
-            import debugpy
-
-            debugpy.listen(("0.0.0.0", 5678))
-            # debugpy.wait_for_client()
 
     execute_from_command_line(sys.argv)
