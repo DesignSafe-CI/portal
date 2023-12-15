17a1531ec72edbea02acea7c06427ab598958704
Jake Rosenberg <jrosenberg@MacBook-Pro.lan>
2023-12-15 13:14:24 -0600

    validate dropdown options as id/name pairs for type Other

--- designsafe/apps/projects_v2/schema_models/base.py
+++ designsafe/apps/projects_v2/schema_models/base.py
@@ -157,6 +157,111 @@ def handle_legacy_authors(author_list: list):
     return author_list
 
 
+nh_options = [
+    "Drought",
+    "Earthquake",
+    "Extreme Temperatures",
+    "Wildfire",
+    "Flood",
+    "Hurricane/Tropical Storm",
+    "Landslide",
+    "Tornado",
+    "Tsunami",
+    "Thunderstorm",
+    "Storm Surge",
+    "Pandemic",
+    "Wind",
+]
+nh_options_map = {val: {"id": val.lower(), "name": val} for val in nh_options}
+nh_options_map["Fire"] = {"id": "Fire".lower(), "name": "Wildfire"}
+nh_options_map["Wildfire"] = {"id": "Fire".lower(), "name": "Wildfire"}
+nh_options_map["Hurricane"] = {
+    "id": "Hurricane/Tropical Storm".lower(),
+    "name": "Hurricane/Tropical Storm",
+}
+nh_options_map["Tropical Storm"] = {
+    "id": "Hurricane/Tropical Storm".lower(),
+    "name": "Hurricane/Tropical Storm",
+}
+
+
+def handle_nh_types(nh_types: list):
+    """Convert nh_types from list of strings to list of id/name pairs"""
+    nh_types_validated = []
+    for name in nh_types:
+        if isinstance(name, str):
+            nh_value = nh_options_map.get(name, {"id": "other", "name": name})
+            nh_types_validated.append(nh_value)
+        else:
+            nh_types_validated.append(name)
+    return nh_types_validated
+
+
+fr_type_options = [
+    "Engineering",
+    "Geosciences",
+    "Public Health",
+    "Social Sciences",
+    "Interdisciplinary",
+    "Field Experiment",
+    "Cross-Sectional Study",
+    "Longitudinal Study",
+    "Reconnaissance",
+    "Other",
+]
+fr_type_options_map = {val: {"id": val.lower(), "name": val} for val in fr_type_options}
+
+
+def handle_fr_types(fr_types: list):
+    """Convert nh_types from list of strings to list of id/name pairs"""
+    fr_types_validated = []
+    for name in fr_types:
+        if isinstance(name, str):
+            fr_value = fr_type_options_map.get(name, {"id": "other", "name": name})
+            fr_types_validated.append(fr_value)
+        else:
+            fr_types_validated.append(name)
+    return fr_types_validated
+
+
+data_type_options = [
+    "Archival Materials",
+    "Audio",
+    "Benchmark Dataset",
+    "Check Sheet",
+    "Code",
+    "Database",
+    "Dataset",
+    "Engineering",
+    "Image",
+    "Interdisciplinary",
+    "Jupyter Notebook",
+    "Learning Object",
+    "Model",
+    "Paper",
+    "Proceeding",
+    "Poster",
+    "Presentation",
+    "Report",
+    "Research Experience for Undergraduates",
+    "SimCenter Testbed",
+    "Social Sciences",
+    "Survey Instrument",
+    "Testbed",
+    "Video",
+]
+data_type_options_map = {
+    val: {"id": val.lower(), "name": val} for val in data_type_options
+}
+
+
+def handle_data_type(data_type):
+    """Convert data_type from string to id/name pair"""
+    if isinstance(data_type, str):
+        return data_type_options_map.get(data_type, {"id": "other", "name": data_type})
+    return data_type
+
+
 class BaseProject(MetadataModel):
     """Model for project root metadata."""
 
@@ -180,7 +285,7 @@ class BaseProject(MetadataModel):
     ] = []
     pi: str
     co_pis: list[str] = []
-    data_type: str = ""
+    data_type: str = Annotated(DropdownValue, BeforeValidator(handle_data_type))
     team_order: list[ProjectUser] = []
     award_number: Annotated[
         list[ProjectAward], BeforeValidator(handle_award_number)
@@ -198,12 +303,9 @@ class BaseProject(MetadataModel):
     nh_latitude: str = ""
     nh_longitude: str = ""
 
-    nh_types: list[str] = []
-    # Deprecated NH type fields
-    nh_type: Optional[str] = None
-    nh_type_other: Optional[str] = None
+    nh_types: Annotated[list[DropdownValue], BeforeValidator(handle_nh_types)] = []
 
-    fr_types: list[str] = []
+    fr_types: Annotated[list[DropdownValue], BeforeValidator(handle_fr_types)] = []
     dois: list[str] = []
 
     file_tags: list[FileTag] = []
