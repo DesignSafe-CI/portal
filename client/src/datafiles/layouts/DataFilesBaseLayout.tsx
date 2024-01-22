461c5d268fe13869b38ff767f39b9868dc49b7ad
Jake Rosenberg <jrosenberg@tacc.utexas.edu>
2024-01-22 13:40:17 -0600

    CSS/layout tweaks

--- client/src/datafiles/layouts/DataFilesBaseLayout.tsx
+++ client/src/datafiles/layouts/DataFilesBaseLayout.tsx
@@ -3,25 +3,27 @@ import { Outlet } from 'react-router-dom';
 import { Layout } from 'antd';
 import { DatafilesSideNav } from '@client/datafiles';
 
-const { Sider, Content } = Layout;
+const { Sider } = Layout;
 
 const DataFilesRoot: React.FC = () => {
   return (
-    <Layout style={{ backgroundColor: 'white' }}>
-      <Sider
-        style={{ paddingLeft: '20px', paddingRight: '20px' }}
-        width={250}
-        theme="light"
-      >
+    <Layout
+      hasSider
+      style={{
+        backgroundColor: 'transparent',
+        gap: '20px',
+        paddingLeft: '20px',
+        paddingRight: '20px',
+      }}
+    >
+      <Sider width={250} theme="light">
         <h1 className="headline headline-research" id="headline-data-depot">
           <span className="hl hl-research">Data Depot</span>
         </h1>
 
         <DatafilesSideNav />
       </Sider>
-      <Content>
-        <Outlet />
-      </Content>
+      <Outlet />
     </Layout>
   );
 };
--- client/src/main.tsx
+++ client/src/main.tsx
@@ -2,7 +2,7 @@ import { StrictMode } from 'react';
 import * as ReactDOM from 'react-dom/client';
 import { RouterProvider } from 'react-router-dom';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
-import workspaceRouter from './workspace/router';
+import workspaceRouter from './workspace/workspaceRouter';
 import datafilesRouter from './datafiles/datafilesRouter';
 
 const queryClient = new QueryClient();
