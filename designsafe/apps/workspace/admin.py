90b33fe49f4c1bf59d3dcdae5052543eaf5b5490
Sal Tijerina <r.sal.tijerina@gmail.com>
2024-01-25 14:46:49 -0600

    squash appitem and appbundle

--- designsafe/apps/workspace/admin.py
+++ designsafe/apps/workspace/admin.py
@@ -1,102 +1,100 @@
+"""Admin layout for Tools & Applications workspace models.
+"""
+
 from django.contrib import admin
 from django.db import models
 from django.forms import CheckboxSelectMultiple
 from designsafe.apps.workspace.models.app_descriptions import AppDescription
 from designsafe.apps.workspace.models.app_entries import (
-    AppItem,
-    AppBundle,
+    AppTrayEntry,
     AppTrayCategory,
 )
 
 admin.site.register(AppDescription)
+admin.site.register(AppTrayCategory)
 
 
-@admin.register(AppTrayCategory)
-class AppTrayCategoryAdmin(admin.ModelAdmin):
-    fields = (
-        "category",
-        "priority",
-    )
+@admin.register(AppTrayEntry)
+class AppTrayEntryAdmin(admin.ModelAdmin):
+    """Admin layout for AppTrayEntry items."""
 
+    def get_fieldsets(self, request, obj=None):
+        default_fieldset = [
+            ("Tapis App Type", {"fields": ("app_type",)}),
+            (
+                "Display Options",
+                {
+                    "fields": [
+                        "app_id",
+                        "category",
+                        "label",
+                        "icon",
+                        "available",
+                    ],
+                },
+            ),
+        ]
+        tapis_fieldset = [
+            (
+                "Tapis App Specification for app_type: Tapis",
+                {
+                    "classes": ["collapse"],
+                    "fields": ["version"],
+                },
+            ),
+        ]
 
-@admin.register(AppItem)
-class AppItemAdmin(admin.ModelAdmin):
-    fieldsets = (
-        ("Tapis App Type", {"fields": ("app_type",)}),
-        (
-            "Display Options",
-            {
-                "fields": (
-                    "app_id",
-                    "category",
-                    "label",
-                    "icon",
-                    "available",
-                ),
-            },
-        ),
-        (
-            "Tapis App Specification for app_type: Tapis",
-            {
-                "fields": ("version",),
-            },
-        ),
-        (
-            "HTML App Body for app_type: HTML",
-            {
-                "fields": ["html"],
-            },
-        ),
-        (
-            "CMS Display Options",
-            {
-                "fields": (
-                    "related_apps",
-                    "popular",
-                    "license_type",
-                    "overview",
-                )
-            },
-        ),
-    )
-    formfield_overrides = {
-        models.ManyToManyField: {"widget": CheckboxSelectMultiple},
-    }
+        html_fieldset = [
+            (
+                "HTML App Body for app_type: HTML",
+                {
+                    "classes": ["collapse"],
+                    "fields": ["html"],
+                },
+            ),
+        ]
 
+        bundle_fieldset = [
+            (
+                "Bundled Apps",
+                {
+                    "classes": ["collapse"],
+                    "fields": ["bundled_apps"],
+                },
+            )
+        ]
+        cms_fieldset = [
+            (
+                "CMS Display Options",
+                {
+                    "classes": ["collapse"],
+                    "fields": [
+                        "related_apps",
+                        "popular",
+                        "license_type",
+                        "overview",
+                    ],
+                },
+            ),
+        ]
 
-@admin.register(AppBundle)
-class AppBundleAdmin(admin.ModelAdmin):
-    fieldsets = (
-        (
-            "Display Options",
-            {
-                "fields": (
-                    "app_id",
-                    "category",
-                    "label",
-                    "icon",
-                    "available",
-                ),
-            },
-        ),
-        (
-            "Bundled Apps",
-            {
-                "fields": ("bundled_apps",),
-            },
-        ),
-        (
-            "CMS Display Options",
-            {
-                "fields": (
-                    "related_apps",
-                    "popular",
-                    "license_type",
-                    "overview",
-                )
-            },
-        ),
-    )
+        if obj is not None:
+            match obj.app_type:
+                case "tapis":
+                    return default_fieldset + tapis_fieldset + cms_fieldset
+                case "html":
+                    return default_fieldset + html_fieldset + cms_fieldset
+                case "bundle":
+                    return default_fieldset + bundle_fieldset + cms_fieldset
+
+        return (
+            default_fieldset
+            + tapis_fieldset
+            + html_fieldset
+            + bundle_fieldset
+            + cms_fieldset
+        )
+
     formfield_overrides = {
         models.ManyToManyField: {"widget": CheckboxSelectMultiple},
     }
--- designsafe/apps/workspace/models/app_entries.py
+++ designsafe/apps/workspace/models/app_entries.py
@@ -1,37 +1,12 @@
+"""Models for the Tools & Applications workspace.
+"""
+
 from django.db import models
 
 
-APP_ICONS = [
-    "rWHALE",
-    "Hazmapper",
-    "Compress",
-    "OpenSees-STKO",
-    "STKO",
-    "OpenFOAM",
-    "Blender",
-    "MATLAB",
-    "NGL",
-    "Paraview",
-    "VisIt",
-    "Jupyter",
-    "QGIS",
-    "ADCIRC",
-    "OpenSees",
-    "LS-DYNA",
-    "LS-Pre/Post",
-    "Dakota",
-    "Clawpack",
-    "Ansys",
-    "swbatch",
-    "Extract",
-]
-
-
-def _get_icon_choices():
-    return [(i.lower(), i) for i in APP_ICONS]
-
-
 class AppTrayCategory(models.Model):
+    """Categories in which AppTrayEntry items are organized."""
+
     category = models.CharField(
         help_text="A category for the app tray.", max_length=64, unique=True
     )
@@ -41,13 +16,58 @@ class AppTrayCategory(models.Model):
     )
 
     def __str__(self):
-        return "%s" % (self.category)
+        return f"{self.category}"
 
 
 class AppTrayEntry(models.Model):
+    """Entries for the Tools & Applications workspace, including Tapis, HTML, and Bundled apps.
+
+    ENTRY_TYPES:
+        A Tapis App is corresponds to a valid app id registered in the Tapis tenant.
+
+        An HTML or External app is typically an HTML body with a link to an external resource.
+
+        An App Bundle is both:
+            1) a card on the apps CMS layout page that links to an overview page, and
+            2) a binned app in the apps workspace, where each app in bundled_apps is a dropdown item.
+    """
+
+    ENTRY_TYPES = [
+        ("tapis", "Tapis App"),
+        ("html", "HTML or External app"),
+        ("bundle", "App Bundle"),
+    ]
+    APP_ICONS = [
+        ("rwhale", "rWHALE"),
+        ("hazmapper", "Hazmapper"),
+        ("compress", "Compress"),
+        ("opensees", "OpenSees/STKO"),
+        ("openfoam", "OpenFOAM"),
+        ("blender", "Blender"),
+        ("matlab", "MATLAB"),
+        ("ngl", "NGL"),
+        ("paraview", "Paraview"),
+        ("visit", "VisIt"),
+        ("jupyter", "Jupyter"),
+        ("qgis", "QGIS"),
+        ("adcirc", "ADCIRC"),
+        ("opensees", "OpenSees"),
+        ("ls-dyna", "LS-DYNA"),
+        ("dakota", "Dakota"),
+        ("clawpack", "Clawpack"),
+        ("ansys", "Ansys"),
+        ("swbatch", "swbatch"),
+        ("extract", "Extract"),
+    ]
     LICENSE_TYPES = [("OS", "Open Source"), ("LS", "Licensed")]
 
     # Basic display options
+    app_type = models.CharField(
+        help_text="Application type.",
+        max_length=10,
+        choices=ENTRY_TYPES,
+        default="tapis",
+    )
     app_id = models.CharField(
         help_text="The id of this app or app bundle. The id appears in the unique url path to the app.",
         max_length=64,
@@ -65,33 +85,11 @@ class AppTrayEntry(models.Model):
     icon = models.CharField(
         help_text="The icon associated with this app.",
         max_length=64,
-        choices=_get_icon_choices(),
+        choices=APP_ICONS,
         blank=True,
     )
     available = models.BooleanField(
         help_text="App visibility in app tray", default=True
-    )
-
-    # CMS specific display options
-    popular = models.BooleanField(
-        help_text="Mark as popular on tools & apps overview.", default=False
-    )
-    license_type = models.CharField(
-        max_length=2, choices=LICENSE_TYPES, help_text="License Type.", default="OS"
-    )
-    overview = models.CharField(
-        help_text="Link to overview page for this app.", max_length=128, blank=True
-    )
-
-    class Meta:
-        abstract = True
-
-
-class AppItem(AppTrayEntry):
-    APP_TYPES = [("tapis", "Tapis"), ("html", "HTML")]
-
-    app_type = models.CharField(
-        help_text="Application type.", max_length=10, choices=APP_TYPES, default="tapis"
     )
 
     # Tapis Apps
@@ -108,11 +106,25 @@ class AppItem(AppTrayEntry):
         blank=True,
     )
 
-    # CMS Display Option
+    # Bundled Apps
+    bundled_apps = models.ManyToManyField(
+        "self", help_text="Apps that will be bundled together.", blank=True
+    )
+
+    # CMS Display Options
     related_apps = models.ManyToManyField(
         "self",
         help_text="Related apps that will display on app overview page.",
         blank=True,
+    )
+    popular = models.BooleanField(
+        help_text="Mark as popular on tools & apps overview.", default=False
+    )
+    license_type = models.CharField(
+        max_length=2, choices=LICENSE_TYPES, help_text="License Type.", default="OS"
+    )
+    overview = models.CharField(
+        help_text="Link to overview page for this app.", max_length=128, blank=True
     )
 
     class Meta:
@@ -124,42 +136,10 @@ class AppItem(AppTrayEntry):
         ]
 
     def __str__(self):
-        if self.app_type == "html":
-            return "%s: %s (HTML)" % (self.label, self.app_id)
-        return "%s%s%s" % (
-            f"{self.label}: " if self.label else "",
-            self.app_id,
-            f"-{self.version}" if self.version else "",
+        return (
+            f"{self.app_type}: "
+            f"{self.label + ': ' if self.label else ''}"
+            f"{self.app_id}"
+            f"{'-' + self.version if self.version else ''}"
+            f" ({'ENABLED' if self.available else 'DISABLED'})"
         )
-
-
-class AppBundle(AppTrayEntry):
-    """
-    An AppBundle is :
-    1) a card on the apps CMS layout page that links to an overview page, and
-    2) a binned app in the apps workspace, where each app in bundled_apps is a dropdown item.
-    """
-
-    bundled_apps = models.ManyToManyField(
-        AppItem,
-        related_name="app_bundle",
-        help_text="Apps that will be bundled together.",
-    )
-
-    # CMS Display Option
-    related_apps = models.ManyToManyField(
-        AppItem,
-        related_name="+",
-        help_text="Related apps that will display on app overview page.",
-        blank=True,
-    )
-
-    class Meta:
-        constraints = [
-            models.UniqueConstraint(
-                fields=["category", "app_id"], name="unique_category_and_id"
-            )
-        ]
-
-    def __str__(self):
-        return "%s%s" % (f"{self.label}: " if self.label else "", self.app_id)
