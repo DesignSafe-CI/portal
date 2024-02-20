2bd6ad796920a61d6c3cc7ea90bfa2748d254b66
Jake Rosenberg <jrosenberg@tacc.utexas.edu>
2024-02-20 14:49:12 -0600

    Use an inline admin form to manage app variants within bundles (#1164)
    Co-authored-by: Sal Tijerina <r.sal.tijerina@gmail.com>

--- designsafe/apps/workspace/models/app_entries.py
+++ designsafe/apps/workspace/models/app_entries.py
@@ -3,6 +3,31 @@
 
 from django.db import models
 
+APP_ICONS = [
+    ("rwhale", "rWHALE"),
+    ("hazmapper", "Hazmapper"),
+    ("compress", "Compress"),
+    ("opensees", "OpenSees/STKO"),
+    ("openfoam", "OpenFOAM"),
+    ("blender", "Blender"),
+    ("matlab", "MATLAB"),
+    ("ngl", "NGL"),
+    ("paraview", "Paraview"),
+    ("visit", "VisIt"),
+    ("jupyter", "Jupyter"),
+    ("qgis", "QGIS"),
+    ("adcirc", "ADCIRC"),
+    ("opensees", "OpenSees"),
+    ("ls-dyna", "LS-DYNA"),
+    ("dakota", "Dakota"),
+    ("clawpack", "Clawpack"),
+    ("ansys", "Ansys"),
+    ("swbatch", "swbatch"),
+    ("extract", "Extract"),
+]
+
+LICENSE_TYPES = [("OS", "Open Source"), ("LS", "Licensed")]
+
 
 class AppTrayCategory(models.Model):
     """Categories in which AppTrayEntry items are organized."""
@@ -18,8 +43,11 @@ class AppTrayCategory(models.Model):
     def __str__(self):
         return f"{self.category}"
 
+    class Meta:
+        verbose_name_plural = "App Categories"
 
-class AppTrayEntry(models.Model):
+
+class AppListingEntry(models.Model):
     """Entries for the Tools & Applications workspace, including Tapis, HTML, and Bundled apps.
 
     ENTRY_TYPES:
@@ -32,53 +60,15 @@ class AppTrayEntry(models.Model):
             2) a binned app in the apps workspace, where each app in bundled_apps is a dropdown item.
     """
 
-    ENTRY_TYPES = [
-        ("tapis", "Tapis App"),
-        ("html", "HTML or External app"),
-        ("bundle", "App Bundle"),
-    ]
-    APP_ICONS = [
-        ("rwhale", "rWHALE"),
-        ("hazmapper", "Hazmapper"),
-        ("compress", "Compress"),
-        ("opensees", "OpenSees/STKO"),
-        ("openfoam", "OpenFOAM"),
-        ("blender", "Blender"),
-        ("matlab", "MATLAB"),
-        ("ngl", "NGL"),
-        ("paraview", "Paraview"),
-        ("visit", "VisIt"),
-        ("jupyter", "Jupyter"),
-        ("qgis", "QGIS"),
-        ("adcirc", "ADCIRC"),
-        ("opensees", "OpenSees"),
-        ("ls-dyna", "LS-DYNA"),
-        ("dakota", "Dakota"),
-        ("clawpack", "Clawpack"),
-        ("ansys", "Ansys"),
-        ("swbatch", "swbatch"),
-        ("extract", "Extract"),
-    ]
-    LICENSE_TYPES = [("OS", "Open Source"), ("LS", "Licensed")]
-
     # Basic display options
-    app_type = models.CharField(
-        help_text="Application type.",
-        max_length=10,
-        choices=ENTRY_TYPES,
-        default="tapis",
-    )
-    app_id = models.CharField(
-        help_text="The id of this app or app bundle. The id appears in the unique url path to the app.",
-        max_length=64,
-    )
+
     category = models.ForeignKey(
         AppTrayCategory,
         help_text="The category for this app entry.",
         on_delete=models.CASCADE,
     )
     label = models.CharField(
-        help_text="The display name of this app in the Apps Tray.",
+        help_text="The display name of this bundle in the Apps Tray.",
         max_length=64,
         blank=True,
     )
@@ -88,22 +78,8 @@ class AppTrayEntry(models.Model):
         choices=APP_ICONS,
         blank=True,
     )
-    available = models.BooleanField(
-        help_text="App visibility in app tray", default=True
-    )
-
-    # Tapis Apps
-    version = models.CharField(
-        help_text="The version number of the app. The app id + version denotes a unique app.",
-        max_length=64,
-        blank=True,
-    )
-
-    # HTML Apps
-    html = models.TextField(
-        help_text="HTML definition to display when app is loaded.",
-        default="",
-        blank=True,
+    enabled = models.BooleanField(
+        help_text="Bundle visibility in app tray", default=True
     )
 
     # Bundled Apps
@@ -120,26 +96,82 @@ class AppTrayEntry(models.Model):
     popular = models.BooleanField(
         help_text="Mark as popular on tools & apps overview.", default=False
     )
-    license_type = models.CharField(
-        max_length=2, choices=LICENSE_TYPES, help_text="License Type.", default="OS"
+
+    not_bundled = models.BooleanField(
+        help_text="Select if this entry represents a single app ID and not a bundle",
+        default=False,
     )
-    overview = models.CharField(
+
+    href = models.CharField(
         help_text="Link to overview page for this app.", max_length=128, blank=True
     )
 
-    class Meta:
-        constraints = [
-            models.UniqueConstraint(
-                fields=["category", "app_id", "version"],
-                name="unique_category_and_id_and_version",
-            )
-        ]
-
     def __str__(self):
         return (
-            f"{self.app_type}: "
+            f"{self.category} "
             f"{self.label + ': ' if self.label else ''}"
-            f"{self.app_id}"
-            f"{'-' + self.version if self.version else ''}"
-            f" ({'ENABLED' if self.available else 'DISABLED'})"
+            f" ({'ENABLED' if self.enabled else 'DISABLED'})"
         )
+
+    class Meta:
+        verbose_name_plural = "App Listing Entries"
+
+
+class AppVariant(models.Model):
+    """Model to represent a variant of an app, e.g. a software version or execution environment"""
+
+    ENTRY_TYPES = [
+        ("tapis", "Tapis App"),
+        ("html", "HTML or External app"),
+    ]
+
+    app_id = models.CharField(
+        help_text="The id of this app or app bundle. The id appears in the unique url path to the app.",
+        max_length=64,
+    )
+
+    app_type = models.CharField(
+        help_text="Application type.",
+        max_length=10,
+        choices=ENTRY_TYPES,
+        default="tapis",
+    )
+
+    label = models.CharField(
+        help_text="The display name of this app in the Apps Tray.",
+        max_length=64,
+    )
+
+    license_type = models.CharField(
+        max_length=2, choices=LICENSE_TYPES, help_text="License Type.", default="OS"
+    )
+
+    bundle = models.ForeignKey(
+        AppListingEntry,
+        help_text="bundle that the app belongs to",
+        on_delete=models.CASCADE,
+        blank=True,
+        null=True,
+    )
+
+    # HTML Apps
+    html = models.TextField(
+        help_text="HTML definition to display when app is loaded.",
+        default="",
+        blank=True,
+    )
+
+    # Tapis Apps
+    version = models.CharField(
+        help_text="The version number of the app. The app id + version denotes a unique app.",
+        default="",
+        max_length=64,
+        blank=True,
+    )
+
+    enabled = models.BooleanField(
+        help_text="Bundle visibility in app tray", default=True
+    )
+
+    def __str__(self):
+        return f"{self.bundle.label} {self.app_id} {self.version}  ({'ENABLED' if self.enabled else 'DISABLED'})"
