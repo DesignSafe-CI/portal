2771edc96f9fc972fe344b623facc5d264129993
Sal Tijerina <r.sal.tijerina@gmail.com>
2024-02-28 15:21:00 -0600

    add docstrings to test methods

--- designsafe/apps/auth/unit_test.py
+++ designsafe/apps/auth/unit_test.py
@@ -1,20 +1,23 @@
+
+import pytest
 from django.test import TransactionTestCase, override_settings
 from django.contrib.auth import get_user_model
 from mock import patch, MagicMock
-from portal.apps.auth.backends import TapisOAuthBackend
 from requests import Response
-from portal.apps.auth.views import launch_setup_checks
-import pytest
+from designsafe.apps.auth.backends import TapisOAuthBackend
+# from designsafe.apps.auth.views import launch_setup_checks
+from tapipy.tapis import TapisResult
+
 
 pytestmark = pytest.mark.django_db
 
 
-def test_launch_setup_checks(mocker, regular_user, settings):
-    mocker.patch("portal.apps.auth.views.new_user_setup_check")
-    mock_execute = mocker.patch("portal.apps.auth.views.execute_setup_steps")
-    regular_user.profile.setup_complete = False
-    launch_setup_checks(regular_user)
-    mock_execute.apply_async.assert_called_with(args=["username"])
+# def test_launch_setup_checks(mocker, regular_user, settings):
+#     mocker.patch("designsafe.apps.auth.views.new_user_setup_check")
+#     mock_execute = mocker.patch("designsafe.apps.auth.views.execute_setup_steps")
+#     regular_user.profile.setup_complete = False
+#     launch_setup_checks(regular_user)
+#     mock_execute.apply_async.assert_called_with(args=["username"])
 
 
 class TestTapisOAuthBackend(TransactionTestCase):
@@ -23,12 +26,12 @@ class TestTapisOAuthBackend(TransactionTestCase):
         self.backend = TapisOAuthBackend()
         self.mock_response = MagicMock(autospec=Response)
         self.mock_requests_patcher = patch(
-            "portal.apps.auth.backends.requests.get", return_value=self.mock_response
+            "designsafe.apps.auth.backends.Tapis.authenticator.get_userinfo", return_value=self.mock_response
         )
         self.mock_requests = self.mock_requests_patcher.start()
 
         self.mock_user_data_patcher = patch(
-            "portal.apps.auth.backends.get_user_data",
+            "designsafe.apps.auth.backends.get_user_data",
             return_value={
                 "username": "testuser",
                 "firstName": "test",
@@ -64,7 +67,7 @@ class TestTapisOAuthBackend(TransactionTestCase):
 
     @override_settings(PORTAL_USER_ACCOUNT_SETUP_STEPS=[])
     def test_new_user(self):
-        # Test that a new user is created and returned
+        """Test that a new user is created and returned"""
         self.mock_response.json.return_value = {
             "status": "success",
             "result": {"username": "testuser"},
@@ -74,8 +77,7 @@ class TestTapisOAuthBackend(TransactionTestCase):
 
     @override_settings(PORTAL_USER_ACCOUNT_SETUP_STEPS=[])
     def test_update_existing_user(self):
-        # Test that an existing user's information is
-        # updated with from info from the Tapis backend response
+        """Test that an existing user's information is updated with from info from the Tapis backend response"""
 
         # Create a pre-existing user with the same username
         user = get_user_model().objects.create_user(
