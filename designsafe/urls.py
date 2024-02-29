4783ced742fb96bd2e61cf15cdbb2cb427f6c334
Sal Tijerina <r.sal.tijerina@gmail.com>
2024-02-29 14:43:26 -0600

    fix login url

--- designsafe/urls.py
+++ designsafe/urls.py
@@ -28,7 +28,8 @@ from django.conf.urls.static import static
 from django.contrib import admin
 from django.views.generic import RedirectView, TemplateView
 from django.urls import reverse, path
-from django.http import HttpResponse, HttpResponseRedirect
+from django.http import HttpResponseRedirect
+from designsafe.apps.auth.views import tapis_oauth as login
 from django.contrib.auth.views import LogoutView as des_logout
 from designsafe.views import project_version as des_version, redirect_old_nees
 from impersonate import views as impersonate_views
@@ -147,6 +148,7 @@ urlpatterns = [
     # auth
     url(r'^auth/', include(('designsafe.apps.auth.urls', 'designsafe.apps.auth'), namespace='designsafe_auth')),
 
+    url(r'^login/$', login, name='login'),
     url(r'^logout/$', des_logout.as_view(), name='logout'),
 
     # help
