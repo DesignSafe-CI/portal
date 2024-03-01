58ab3e7ff0be0c394e13ae8ce43e019618a7714c
Sal Tijerina <r.sal.tijerina@gmail.com>
2024-03-01 12:22:05 -0600

    formatting

--- designsafe/apps/auth/migrations/0003_tapisoauthtoken_delete_agaveoauthtoken.py
+++ designsafe/apps/auth/migrations/0003_tapisoauthtoken_delete_agaveoauthtoken.py
@@ -6,7 +6,6 @@ import django.db.models.deletion
 
 
 class Migration(migrations.Migration):
-
     dependencies = [
         migrations.swappable_dependency(settings.AUTH_USER_MODEL),
         ("designsafe_auth", "0002_auto_20160209_0427"),
