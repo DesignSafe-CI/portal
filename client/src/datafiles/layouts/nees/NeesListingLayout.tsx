155fcac7185e22ad6e36e6fad03941a7e21a963c
Jake Rosenberg <jrosenberg@tacc.utexas.edu>
2024-03-01 12:56:49 -0600

    formatting/type errors

--- client/src/datafiles/layouts/nees/NeesListingLayout.tsx
+++ client/src/datafiles/layouts/nees/NeesListingLayout.tsx
@@ -3,11 +3,10 @@ import { Layout } from 'antd';
 import { NeesListing } from '@client/datafiles';
 
 export const NEESListingLayout: React.FC = () => {
-
   return (
     <Layout>
       <div>Placeholder for the NEES listing view.</div>
-      <div style={{ flex: '1 0 0 ', height: '100%', overflow:'auto'}}>
+      <div style={{ flex: '1 0 0 ', height: '100%', overflow: 'auto' }}>
         <NeesListing />
       </div>
     </Layout>
