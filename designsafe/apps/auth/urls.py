094ec40f903bcc2509b1498b4e0bf1d919f977d3
Sal Tijerina <r.sal.tijerina@gmail.com>
2024-03-05 14:33:56 -0600

    remove unnecessary commented code

--- designsafe/apps/auth/urls.py
+++ designsafe/apps/auth/urls.py
@@ -13,30 +13,3 @@ urlpatterns = [
     path("tapis/", views.tapis_oauth, name="tapis_oauth"),
     path("tapis/callback/", views.tapis_oauth_callback, name="tapis_oauth_callback"),
 ]
-
-
-# from django.urls import reverse
-# from django.utils.translation import gettext_lazy as _
-
-# def menu_items(**kwargs):
-#     if 'type' in kwargs and kwargs['type'] == 'account':
-#         return [
-#             {
-#                 'label': _('Login'),
-#                 'url': reverse('login'),
-#                 'children': [],
-#                 'visible': False
-#             },
-#             {
-#                 'label': _('Login'),
-#                 'url': reverse('designsafe_auth:login'),
-#                 'children': [],
-#                 'visible': False
-#             },
-#             {
-#                 'label': _('Agave'),
-#                 'url': reverse('designsafe_auth:agave_session_error'),
-#                 'children': [],
-#                 'visible': False
-#             }
-#         ]
