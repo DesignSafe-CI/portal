5e00108f6f943c05c253163b9b8a4a8266f1379f
Sal Tijerina <r.sal.tijerina@gmail.com>
2024-03-07 23:44:52 -0600

    tests

--- client/modules/_test-fixtures/src/handlers.ts
+++ client/modules/_test-fixtures/src/handlers.ts
@@ -2,7 +2,7 @@ import { http, HttpResponse } from 'msw';
 import appsListingJson from './fixtures/workspace/apps-tray-listing.json';
 
 const handlers = [
-  http.get('/applications/api/meta', () => {
+  http.get('/api/workspace/tray', () => {
     return HttpResponse.json(appsListingJson);
   }),
 ];
