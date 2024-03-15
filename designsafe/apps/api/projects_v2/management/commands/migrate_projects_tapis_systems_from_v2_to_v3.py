0df9935b58f77b9a781d32b83578a054fabf33eb
Nathan Franklin <nathan.franklin@gmail.com>
2024-03-15 16:27:41 -0500

    Fix pylint issues

--- designsafe/apps/api/projects_v2/management/commands/migrate_projects_tapis_systems_from_v2_to_v3.py
+++ designsafe/apps/api/projects_v2/management/commands/migrate_projects_tapis_systems_from_v2_to_v3.py
@@ -1,10 +1,22 @@
+"""Migrate Projects Tapis Systems from V2 to V3.
+
+This module contains a Django management command which migrates Tapis systems associated with
+projects from Tapis V2 to Tapis V3.
+"""
+
+# pylint: disable=logging-fstring-interpolation
+# pylint: disable=no-member
+
 import logging
 import os
 
-from tapipy.tapis import Tapis
 from django.conf import settings
-from designsafe.apps.api.projects_v2.tests.schema_integration import iterate_entities
 from django.core.management.base import BaseCommand
+
+from tapipy.tapis import Tapis
+
+from designsafe.apps.api.projects_v2.tests.schema_integration import iterate_entities
+
 
 
 logger = logging.getLogger(__name__)
@@ -59,8 +71,10 @@ def set_workspace_permissions(client: Tapis, username: str, system_id: str, role
     )
 
 
-def create_or_update_workspace_system(create, client, system_id: str, title: str, description, projectRootDir,
-                                      owner=None) -> str:
+def create_or_update_workspace_system(create, client, system_id: str, title: str, description: str,  # pylint: disable=too-many-arguments
+                                      project_root_dir: str , owner=None) -> str:
+    """Create or update a system."""
+
     system_args = {
         "id": system_id,
         "host": HOST,
@@ -68,7 +82,7 @@ def create_or_update_workspace_system(create, client, system_id: str, title: str
         "systemType": "LINUX",
         "defaultAuthnMethod": "PKI_KEYS",
         "canExec": False,
-        "rootDir": projectRootDir,
+        "rootDir": project_root_dir,
         "effectiveUserId": TG_USER,
         "authnCredential": {
             "privateKey": TG_USER_PRIVATE_KEY,
@@ -84,15 +98,18 @@ def create_or_update_workspace_system(create, client, system_id: str, title: str
         client.systems.patchSystem(systemId=system_id, **system_args)
 
 
-HOST = settings.PROJECT_STORAGE_SYSTEM_TEMPLATE["storage"]["host"]  # ie. cloud.corral.tacc.utexas.edu
+# pylint: disable=invalid-name
+HOST = settings.PROJECT_STORAGE_SYSTEM_TEMPLATE["storage"]["host"]  # cloud.corral.tacc.utexas.edu
 PORT = 22
 TG_USER = settings.PROJECT_STORAGE_SYSTEM_TEMPLATE["storage"]["auth"]["username"]  # i.e. tg458981
 TG_USER_PRIVATE_KEY = settings.PROJECT_STORAGE_SYSTEM_TEMPLATE["storage"]["auth"]["privateKey"]
 TG_USER_PUBLIC_KEY = settings.PROJECT_STORAGE_SYSTEM_TEMPLATE["storage"]["auth"]["publicKey"]
 OWNER = "wma_prtl"
+# pylint: enable=invalid-name
 
 
 class Command(BaseCommand):
+    """ Command for migrating projects from Tapis v2 to v3"""
     help = ('Facilitates the migration of project systems from Tapis v2 to v3,'
             ' with options for a dry run and updating existing systems.')
 
@@ -100,57 +117,62 @@ class Command(BaseCommand):
         parser.add_argument(
             '--dry-run',
             action='store_true',
-            help='Executes the command in a simulation mode, logging actions without applying any changes '
-                 'to the systems or data.',
+            help='Executes the command in a simulation mode, logging actions '
+                 'without applying any changes to the systems or data.',
         )
 
         parser.add_argument(
             '--update-existing',
             action='store_true',
-            help='Allows the command to update systems that already exist in Tapis V3, ensuring they are'
-                 'synchronized with their V2 counterparts.',
+            help='Allows the command to update systems that already exist in Tapis V3,'
+                 'ensuring they are synchronized with their V2 counterparts.',
         )
 
-    def handle(self, *args, **options):
+    def handle(self, *args, **options):  # pylint: disable=too-many-statements disable=too-many-locals
         dry_run = options['dry_run']
         update_existing = options['update_existing']
 
-        TAPIS_TENANT_BASEURL = os.environ.get('TAPIS_TENANT_BASEURL')
-        TAPIS_ADMIN_JWT = os.environ.get('TAPIS_ADMIN_JWT')
+        TAPIS_TENANT_BASEURL = os.environ.get('TAPIS_TENANT_BASEURL') # pylint: disable=invalid-name
+        TAPIS_ADMIN_JWT = os.environ.get('TAPIS_ADMIN_JWT') # pylint: disable=invalid-name
         client = Tapis(base_url=TAPIS_TENANT_BASEURL, access_token=TAPIS_ADMIN_JWT)
 
         if dry_run:
             logger.info('Running in dry-run mode. No changes will be made.')
 
-        total_number_projects = len([p for p in iterate_entities()])
+        total_number_projects = len(list(iterate_entities()))
 
         for i, project in enumerate(iterate_entities()):
             uuid = project["uuid"]
-            projectId = project["value"]["projectId"]
+            project_id = project["value"]["projectId"]
             system = f"project-{uuid}"
-            projectRootDir = f"/corral-repl/projects/NHERI/projects/{uuid}"
+            project_root_dir = f"/corral-repl/projects/NHERI/projects/{uuid}"
             title = project["value"]["title"]
-            description = project["value"]["description"] if "description" in project["value"] else ""
-            pi = project['value']['pi']
-            coPis = project['value']['coPis'] if 'coPis' in project['value'] else []
-            teamMembers = project['value']['teamMembers'] if 'teamMembers' in project['value'] else []
-            guestMembers = project['value']['guestMembers'] if 'guestMembers' in project['value'] else []
-            guestMembers = [u['user'] for u in guestMembers if u is not None]
+            description = project["value"]["description"] \
+                if "description" in project["value"] else ""
+            pi = project['value']['pi'] # pylint: disable=invalid-name
+            co_pis = project['value']['coPis'] if 'coPis' in project['value'] else []
+            team_members = project['value']['teamMembers'] \
+                if 'teamMembers' in project['value'] else []
+            guest_members = project['value']['guestMembers'] \
+                if ('guestMembers' in project['value']) else []
+            guest_members = [u['user'] for u in guest_members if u is not None]
 
-            all_writers = coPis + teamMembers + guestMembers
+            all_writers = co_pis + team_members + guest_members
             if pi is not None:
                 all_writers.append(pi)
 
-            all_readers = guestMembers
-            all_users = all_readers + all_writers
-
             logger.info(
-                f"Migrating {i}/{total_number_projects} {projectId} ({uuid}) ('{title}') pi:{pi} coPis:{coPis} teamMembers:{teamMembers} guestMembers:{guestMembers}")
+                f"Migrating {i}/{total_number_projects} {project_id} ({uuid}) "
+                f"('{title}') pi:{pi} coPis:{co_pis} teamMembers:{team_members} "
+                f"guestMembers:{guest_members}")
+
+            all_readers = guest_members
+            all_users = all_readers + all_writers
 
             system_exists = True
             try:
                 client.systems.getSystem(systemId=system)
-            except Exception:
+            except Exception:  # pylint: disable=broad-exception-caught
                 system_exists = False
 
             if not dry_run:
@@ -160,26 +182,29 @@ class Command(BaseCommand):
                 if system_exists:
                     shared_information = client.systems.getShareInfo(systemId=system)
                     user_to_remove = set(shared_information.get("users")) - set(all_users)
-                    for u in user_to_remove:
-                        logger.info(f"removing user: {u}")
-                        remove_user(client, system, u)
+                    for user in user_to_remove:
+                        logger.info(f"removing user: {user}")
+                        remove_user(client, system, user)
 
                 create = not system_exists
 
                 try:
-                    create_or_update_workspace_system(create, client, system_id=system, title=title,
-                                                      description=description, projectRootDir=projectRootDir, owner=OWNER)
+                    create_or_update_workspace_system(create, client, system_id=system,
+                                                      title=title, description=description,
+                                                      project_root_dir=project_root_dir,
+                                                      owner=OWNER)
                     client.systems.shareSystem(systemId=system, users=all_users)
 
-                    for u in all_writers:
-                        set_workspace_permissions(client, u, system, "writer")
+                    for user in all_writers:
+                        set_workspace_permissions(client, user, system, "writer")
 
-                    for u in all_readers:
-                        set_workspace_permissions(client, u, system, "reader")
-                except Exception:
+                    for user in all_readers:
+                        set_workspace_permissions(client, user, system, "reader")
+                except Exception: # pylint: disable=broad-exception-caught
                     logger.exception(f"Error for system:{system}")
             else:
-                system_exists_text = "System already exists" if system_exists else "System does not exist"
-                logger.info(f'Running in dry-run mode. No changes will be made. ' f'Note: {system_exists_text}')
-
+                system_exists_text = "System already exists" if system_exists \
+                    else "System does not exist"
+                logger.info(f'Running in dry-run mode. No changes will be made. '
+                            f'' f'Note: {system_exists_text}')
         logger.info('Successfully migrated systems')
