87164b6ca70e125fdf9eb3c813d0659d8cbf03fe
Van Go <35277477+van-go@users.noreply.github.com>
2024-03-21 17:24:47 -0500

    Task/des 2676 add file folder button (#1179)
    * commit to main
    
    * Add AddFileFolder component and import it in DataFilesBaseLayout
    
    * Add useNewFolder hook and NewFolderModal component
    
    * Add UploadFileModal and useUploadFile hook
    
    * Fix file upload method
    
    * Refactor useUploadFile and import statement in UploadFileModal
    
    * Add scheme parameter to useUploadFile and UploadFileModal
    
    * Add file upload functionality
    
    * Refactor file upload functionality
    
    * Enable multiple file selection and add selected file to fileList
    
    * Add UploadFolderModal and useUploadFolder hook
    
    * Update file and folder upload functionality
    
    * Fix formatting issues and update dependencies
    
    * Refactor AddFileFolder component and fix variable name in UploadFileModal
    
    * Refactor AddFileFolder component
    
    * format check
    
    * Add CSS styles for upload modals and add file/folder module
    
    * Fix link click event in AddFileFolder component
    
    * Update CustomUploadFile interface in UploadFolderModal.tsx
    
    * Update path formatting in UploadFileModal and UploadFolderModal
    
    * Refactor path replacement in UploadFileModal and UploadFolderModal
    
    * Add full-width button styles to AddFileFolder module
    
    * Update CSS styles for AddFileFolder and NewFolderModal components
    
    * Update CSS styles in AddFileFolder, NewFolderModal, and UploadFolderModal modules
    
    * Refactor upload modals
    
    ---------
    
    Co-authored-by: Jake Rosenberg <jrosenberg@tacc.utexas.edu>

--- client/modules/_hooks/src/datafiles/index.ts
+++ client/modules/_hooks/src/datafiles/index.ts
@@ -16,6 +16,10 @@ export {
 export { useFileCopy } from './useFileCopy';
 export { useTrash } from './useTrash';
 export { useRename } from './useRename';
+export { useNewFolder } from './useNewFolder';
+export { useUploadFile } from './useUploadFile';
+export { useUploadFolder } from './useUploadFolder';
+
 export {
   usePathDisplayName,
   getSystemRootDisplayName,
