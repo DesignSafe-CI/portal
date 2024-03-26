245e34d38659535993b2446cf9fccb4b15070c48
Sal Tijerina <r.sal.tijerina@gmail.com>
2024-03-26 12:30:11 -0500

    add spinner; cleanup

--- client/modules/workspace/src/AppsWizard/AppsWizard.tsx
+++ client/modules/workspace/src/AppsWizard/AppsWizard.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { useAuthenticatedUser } from '@client/hooks';
 import styles from './AppsWizard.module.css';
 import { useGetApps } from '@client/hooks';
+import { Spin } from 'antd';
 
 export const AppsWizard: React.FC<{ appId: string; appVersion?: string }> = ({
   appId,
@@ -11,6 +12,7 @@ export const AppsWizard: React.FC<{ appId: string; appVersion?: string }> = ({
   const { data, isLoading } = useGetApps({ appId, appVersion });
   return (
     <ul className={styles.navList}>
+      {isLoading && <Spin className={styles.spinner} />}
       {user && data && !isLoading && (
         <div>{data.definition.notes.label || data.definition.id}</div>
       )}
