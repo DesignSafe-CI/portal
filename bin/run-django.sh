a6a9fec48d9f3a72e9a214717bc734bd7dd88157
Jake Rosenberg <jrosenberg@tacc.utexas.edu>
2024-03-27 15:15:21 -0500

    Use Django to serve static files in debug mode (#1186)
    * Use Django to serve static files in debug mode
    
    * clean up volumes; remove local nginx static and media locations
    
    * add back /data/media mount
    
    * add media back to nginx; same locations as main
    
    ---------
    
    Co-authored-by: Sal Tijerina <r.sal.tijerina@gmail.com>

--- bin/run-django.sh
+++ bin/run-django.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 # run django dev server as designsafe community account
-python -m debugpy --listen 0.0.0.0:5678 manage.py runserver --noasgi 0.0.0.0:8000
+python -m debugpy --listen 0.0.0.0:5678 manage.py runserver 0.0.0.0:8000
