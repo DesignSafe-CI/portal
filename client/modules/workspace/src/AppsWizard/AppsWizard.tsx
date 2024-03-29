09e352a044e95c27d426bb4aa4f8e59df4b0005f
Sal Tijerina <r.sal.tijerina@gmail.com>
2024-03-28 23:49:49 -0500

    wip: appswizard and layout

--- client/modules/workspace/src/AppsWizard/AppsWizard.tsx
+++ client/modules/workspace/src/AppsWizard/AppsWizard.tsx
@@ -1,8 +1,41 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useAuthenticatedUser } from '@client/hooks';
 import styles from './AppsWizard.module.css';
 import { useGetApps } from '@client/hooks';
-import { Spin } from 'antd';
+import { Button, Form, Input, List, message, Steps, theme, Spin } from 'antd';
+
+const steps = [
+  {
+    title: 'First',
+    content: (
+      <Form.Item
+        name="steptest1"
+        label="Stepper Field 1"
+        rules={[{ required: true }]}
+        // hidden={current !== 0}
+      >
+        <Input />
+      </Form.Item>
+    ),
+  },
+  {
+    title: 'Second',
+    content: (
+      <Form.Item
+        name="steptest2"
+        label="Stepper Field 2"
+        rules={[{ required: true }]}
+        // hidden={current !== 1}
+      >
+        <Input />
+      </Form.Item>
+    ),
+  },
+  {
+    title: 'Last',
+    content: 'Last-content',
+  },
+];
 
 export const AppsWizard: React.FC<{ appId: string; appVersion?: string }> = ({
   appId,
@@ -10,12 +43,60 @@ export const AppsWizard: React.FC<{ appId: string; appVersion?: string }> = ({
 }) => {
   const { user } = useAuthenticatedUser();
   const { data, isLoading } = useGetApps({ appId, appVersion });
+  const { token } = theme.useToken();
+  const [current, setCurrent] = useState(0);
+  const next = () => {
+    setCurrent(current + 1);
+  };
+  const prev = () => {
+    setCurrent(current - 1);
+  };
+  const items = steps.map((item) => ({
+    key: item.title,
+    title: item.title,
+  }));
+  const contentStyle = {
+    lineHeight: '260px',
+    textAlign: 'center',
+    color: token.colorTextTertiary,
+    backgroundColor: token.colorFillAlter,
+    borderRadius: token.borderRadiusLG,
+    border: `1px dashed ${token.colorBorder}`,
+    marginTop: 16,
+  };
+
   return (
-    <ul className={styles.navList}>
+    <>
       {isLoading && <Spin className={styles.spinner} />}
       {user && data && !isLoading && (
-        <div>{data.definition.notes.label || data.definition.id}</div>
+        <>
+          {/* <Steps current={current} items={items} /> */}
+          <div
+            style={{
+              marginTop: 24,
+            }}
+          >
+            <Button
+              style={{
+                margin: '0 8px',
+              }}
+              onClick={() => prev()}
+              disabled={!(current > 0)}
+            >
+              Back
+            </Button>
+            <Button
+              type="primary"
+              onClick={() => next()}
+              disabled={!(current < steps.length - 1)}
+            >
+              Continue
+            </Button>
+          </div>
+          <div style={contentStyle}>{steps[current].content}</div>
+          <div>{data.definition.notes.label || data.definition.id}</div>
+        </>
       )}
-    </ul>
+    </>
   );
 };
--- client/src/workspace/layouts/AppsViewLayout.tsx
+++ client/src/workspace/layouts/AppsViewLayout.tsx
@@ -1,19 +1,51 @@
-import { AppsWizard } from '@client/workspace';
+import { AppsWizard, AppsSubmissionForm } from '@client/workspace';
 import { TAppParamsType } from '@client/hooks';
-import { Layout } from 'antd';
-import React from 'react';
+import { Layout, Form, List, message } from 'antd';
+import React, { useState } from 'react';
 import styles from './layout.module.css';
 import { useParams } from 'react-router-dom';
 
 export const AppsViewLayout: React.FC = () => {
   const { appId, appVersion } = useParams() as TAppParamsType;
+  const [formValues, setFormValues] = useState({
+    steptest1: undefined,
+    steptest2: undefined,
+  });
+  const onFinish = () => message.success('Form validated');
+  const onValuesChange = (_, allValues) => {
+    setFormValues(allValues);
+  };
+
+  const onValidationError = ({ errorFields }) => {
+    errorFields
+      .map((errField) => errField.errors)
+      .forEach((error) => {
+        message.error({ content: error, style: { marginTop: 20 } });
+      });
+  };
+
   return (
-    <Layout style={{ gap: '5px', minWidth: '500px' }}>
-      <Layout.Content className={styles['listing-main']}>
-        <div className={styles['listing-container']}>
+    <Form
+      name="rootForm"
+      onFinish={onFinish}
+      onValuesChange={onValuesChange}
+      onFinishFailed={onValidationError}
+    >
+      <Layout style={{ gap: '5px', minWidth: '500px' }}>
+        <Layout.Content className={styles['listing-main']}>
           <AppsWizard appId={appId} appVersion={appVersion} />
-        </div>
-      </Layout.Content>
-    </Layout>
+        </Layout.Content>
+        <Layout.Sider
+          width="250"
+          style={{
+            backgroundColor: 'transparent',
+            paddingLeft: '1em',
+            paddingTop: '3em',
+          }}
+        >
+          <AppsSubmissionForm formValues={formValues} />
+        </Layout.Sider>
+      </Layout>
+    </Form>
   );
 };
