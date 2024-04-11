b878bc27649bae980b35b57f4f559de3f35c7767
Sal Tijerina <r.sal.tijerina@gmail.com>
2024-04-11 16:43:47 -0500

    add workspace utils; apps submission form

--- client/modules/workspace/src/utils/jobs.ts
+++ client/modules/workspace/src/utils/jobs.ts
@@ -1,4 +1,4 @@
-import { STATUS_TEXT_MAP } from './constants';
+import { STATUS_TEXT_MAP } from '../constants';
 
 export function getStatusText({ status }: { status: string }) {
   if (status in STATUS_TEXT_MAP) {
