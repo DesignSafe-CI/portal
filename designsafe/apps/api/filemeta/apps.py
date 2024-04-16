4452aec6a51aab71863c9cb5b7da9eaa92a18972
Nathan Franklin <nathan.franklin@gmail.com>
2024-04-16 13:59:47 -0500

    Add app to INSTALLED_APPS

---
+++ designsafe/apps/api/filemeta/apps.py
@@ -0,0 +1,9 @@
+from django.apps import AppConfig
+
+
+class FileMetaAppConfig(AppConfig):
+    """App config for File Meta"""
+
+    name = "designsafe.apps.api.filemeta"
+    label = "filemeta_api"
+    verbose_name = "Designsafe File Meta"
