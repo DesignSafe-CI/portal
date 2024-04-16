d577f16695d4166c496b5ab174cef47ebd81e583
Sophia Massie <smmassie92@gmail.com>
2024-04-16 16:55:00 -0500

    -Adds accordion ant sidebar -Need to update to allow more than one category to be open at once -Needs styles

--- client/modules/workspace/src/AppsSideNav/AppsSideNav.tsx
+++ client/modules/workspace/src/AppsSideNav/AppsSideNav.tsx
@@ -1,4 +1,5 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { Collapse } from 'antd';
 import { NavLink, useRouteLoaderData } from 'react-router-dom';
 import styles from './AppsSideNav.module.css';
 import { useAuthenticatedUser } from '@client/hooks';
@@ -11,40 +12,50 @@ const AppsNavLink: React.FC<React.PropsWithChildren<{ to: string }>> = ({
   return (
     <li>
       <NavLink to={to} className={styles.navLink}>
-        <div>{children}</div>
+        {children}
       </NavLink>
     </li>
   );
 };
+const { Panel } = Collapse;
 
 export const AppsSideNav: React.FC = () => {
   const { user } = useAuthenticatedUser();
   const data = useRouteLoaderData('root') as AppCategories;
+  const [activeApp, setActiveApp] = useState<string | string[]>([]);
+
+  const toggle = (tab: string) => {
+    const newActiveKey = Array.isArray(tab) ? tab[0] : tab;
+    setActiveApp(prevActivePanel =>
+      prevActivePanel === newActiveKey
+        ? []
+        : [newActiveKey]
+    );
+  };
 
   return (
-    <ul className={styles.navList}>
-      {user && data && (
-        <>
-          {data.categories.map((category) => (
-            <div key={category.title}>
-              <h3>{category.title}</h3>
-              <ul>
-                {category.apps.map((app) => (
-                  <AppsNavLink
-                    key={`${app.app_id}-${app.version}`}
-                    to={
-                      `applications/${app.app_id}` +
-                      (app.version ? `?appVersion=${app.version}` : '')
-                    }
-                  >
-                    {app.app_id} {app.version}
-                  </AppsNavLink>
-                ))}
-              </ul>
-            </div>
-          ))}
-        </>
-      )}
-    </ul>
+    <Collapse
+      accordion
+      activeKey={activeApp}
+      onChange={(key) => toggle(key)}
+    >
+      {user && data && data.categories.map((category) => (
+        <Panel header={category.title} key={category.title}>
+          <ul>
+            {category.apps.map((app) => (
+              <AppsNavLink
+                key={`${app.app_id}-${app.version}`}
+                to={
+                  `applications/${app.app_id}` +
+                  (app.version ? `?appVersion=${app.version}` : '')
+                }
+              >
+                {app.label}
+              </AppsNavLink>
+            ))}
+          </ul>
+        </Panel>
+      ))}
+    </Collapse>
   );
 };
