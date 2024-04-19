eb40bde2803ad12de56f6630721b787a3d93ab7d
Sophia Massie <smmassie92@gmail.com>
2024-04-19 14:03:30 -0500

    -Adds ant menu and submenus with children rather than with item prop passed to menu

--- client/modules/workspace/src/AppsSideNav/AppsSideNav.tsx
+++ client/modules/workspace/src/AppsSideNav/AppsSideNav.tsx
@@ -3,48 +3,85 @@ import { Menu } from 'antd';
 import { NavLink } from 'react-router-dom';
 import styles from './AppsSideNav.module.css';
 import { useAuthenticatedUser, useAppsListing } from '@client/hooks';
+import MenuDivider from 'antd/es/menu/MenuDivider';
 
 const AppsNavLink: React.FC<React.PropsWithChildren<{ to: string }>> = ({
   to,
   children,
 }) => {
   return (
-    <Menu.Item key={to}>
+    <Menu.Item>
       <NavLink to={to} className={styles.navLink}>
-        {children}
+        <div>{children}</div>
       </NavLink>
-    </Menu.Item>
+  </Menu.Item>
   );
 };
 
 export const AppsSideNav: React.FC = () => {
   const { user } = useAuthenticatedUser();
   const { data } = useAppsListing();
+  const [activeApp, setActiveApp] = useState<string[]>([]); 
+
+  const toggle = (tab: any) => {
+    setActiveApp(tab.keyPath)
+  };
+
+  const uniqueBundleLabels = new Set<string>();
+
+  if (user && data) {
+    data.categories.forEach((category) => {
+      category.apps.forEach((app) => {
+        uniqueBundleLabels.add(app.bundle_label);
+      });
+    });
+  }
 
   return (
-    <ul className={styles.navList}>
+    <Menu
+      mode="inline"
+      onClick={toggle}
+      selectedKeys={activeApp}
+      className={styles.apps}
+    >
+
+      <h3>Applications:</h3>
       {user && data && (
         <>
           {data.categories.map((category) => (
-            <div key={category.title}>
-              <h3>{category.title}</h3>
-              <ul>
-                {category.apps.map((app) => (
-                  <AppsNavLink
-                    key={`${app.app_id}-${app.version}`}
-                    to={
-                      `${app.app_id}` +
-                      (app.version ? `?appVersion=${app.version}` : '')
-                    }
+            <Menu.SubMenu
+              key={category.title}
+              title={`${category.title} [${category.apps.length}]`}
+            >
+              {category.apps.length > 0 && (
+                Array.from(new Set(category.apps.map((app) => app.bundle_label))).map((bundleLabel) => (
+                  <Menu.SubMenu
+                    key={bundleLabel}
+                    title={`${bundleLabel} [${
+                      category.apps.filter((app) => app.bundle_label === bundleLabel).length
+                    }]`}
                   >
-                    {app.app_id} {app.version}
-                  </AppsNavLink>
-                ))}
-              </ul>
-            </div>
+                    {category.apps
+                      .filter((app) => app.bundle_label === bundleLabel) 
+                      .map((app) => (
+                        <AppsNavLink
+                          key={`${app.app_id}-${app.version}`}
+                          to={
+                            `${app.app_id}` +
+                            (app.version ? `?appVersion=${app.version}` : '')
+                          }
+                        >
+                          {app.label}
+                        </AppsNavLink>
+                      ))}
+                  </Menu.SubMenu>
+                ))
+              )}
+            </Menu.SubMenu>
           ))}
         </>
       )}
-    </ul>
+      <MenuDivider/>
+    </Menu>
   );
 };
