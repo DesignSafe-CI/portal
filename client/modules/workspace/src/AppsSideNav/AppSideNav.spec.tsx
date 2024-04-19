379453a6dc4fc2929bcf7ef5f3553a0a137f539f
Sophia Massie <smmassie92@gmail.com>
2024-04-19 14:11:21 -0500

    Adds unit test to app AppSideNav

---
+++ client/modules/workspace/src/AppsSideNav/AppSideNav.spec.tsx
@@ -0,0 +1,15 @@
+import { render } from '@client/test-fixtures';
+
+import { AppsSideNav } from './AppsSideNav';
+
+describe('AppsSideNav', () => {
+  it('should render successfully', () => {
+    const { baseElement } = render(<AppsSideNav />);
+    expect(baseElement).toBeTruthy();
+  });
+
+  it('should have nav text', () => {
+    const { getByText } = render(<AppsSideNav />);
+    expect(getByText(/Applications:/gi)).toBeTruthy();
+  });
+});
