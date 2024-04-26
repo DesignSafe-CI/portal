414e5ec4c816bae3f0961c9ac620a7e4d41c42c9
Sal Tijerina <r.sal.tijerina@gmail.com>
2024-04-26 12:19:01 -0500

    side nav submenus

--- client/modules/workspace/src/AppsSideNav/AppsSideNav.tsx
+++ client/modules/workspace/src/AppsSideNav/AppsSideNav.tsx
@@ -1,24 +1,18 @@
 import React, { useState } from 'react';
-import { Menu, MenuProps, MenuItem } from 'antd';
+import { Menu, MenuProps } from 'antd';
 import { NavLink } from 'react-router-dom';
 import styles from './AppsSideNav.module.css';
 import MenuDivider from 'antd/es/menu/MenuDivider';
-import {
-  useAuthenticatedUser,
-  useAppsListing,
-  TAppCategory,
-} from '@client/hooks';
+import { TAppCategory } from '@client/hooks';
 
 const AppsNavLink: React.FC<React.PropsWithChildren<{ to: string }>> = ({
   to,
   children,
 }) => {
   return (
-    <Menu.Item>
-      <NavLink to={to} className={styles.navLink}>
-        <div>{children}</div>
-      </NavLink>
-    </Menu.Item>
+    <NavLink to={to} className={styles.navLink}>
+      <div>{children}</div>
+    </NavLink>
   );
 };
 
@@ -41,17 +35,84 @@ export const AppsSideNav: React.FC<{ categories: TAppCategory[] }> = ({
     });
   });
 
-  //type MenuItem = Required<MenuProps>['items'][number];
+  type MenuItem = Required<MenuProps>['items'][number];
 
-  // function getItem(label, key, icon, children, type) {
-  //   return {
-  //     key,
-  //     icon,
-  //     children,
-  //     label,
-  //     type,
-  //   };
-  // }
+  function getItem(
+    label: React.ReactNode,
+    key?: React.Key | null,
+    children?: MenuItem[],
+    type?: 'group'
+  ): MenuItem {
+    return {
+      label,
+      children,
+      key,
+      type,
+    } as MenuItem;
+  }
+
+  const getCategoryApps = (category) => {
+    const bundle = {};
+
+    category.apps.map((app) => {
+      if (app.is_bundled) {
+        if (bundle[app.bundle_id]) {
+          bundle[app.bundle_id].apps.push(
+            getItem(
+              <AppsNavLink
+                to={
+                  `${app.app_id}` +
+                  (app.version ? `?appVersion=${app.version}` : '')
+                }
+              >
+                {app.label}
+              </AppsNavLink>,
+              `${app.app_id}${app.version}`
+            )
+          );
+        } else {
+          bundle[app.bundle_id] = {
+            apps: [
+              getItem(
+                <AppsNavLink
+                  to={
+                    `${app.app_id}` +
+                    (app.version ? `?appVersion=${app.version}` : '')
+                  }
+                >
+                  {app.label}
+                </AppsNavLink>,
+                `${app.app_id}${app.version}`
+              ),
+            ],
+            label: app.bundle_label,
+          };
+        }
+      } else {
+        return getItem(
+          <AppsNavLink
+            to={
+              `${app.app_id}` +
+              (app.version ? `?appVersion=${app.version}` : '')
+            }
+          >
+            {app.label}
+          </AppsNavLink>,
+          `${app.app_id}${app.version}`
+        );
+      }
+    });
+
+    return Object.entries(bundle).map(([bundle_id, bundle]) =>
+      getItem(bundle.label, bundle_id, bundle.apps)
+    );
+  };
+
+  const items: MenuItem[] = categories.map((category) => {
+    console.log(getCategoryApps(category));
+    return getItem(category.title, category.title, getCategoryApps(category));
+  });
+
   // const items = [
   //   {categories.map((category) => {
   //     getItem
@@ -75,11 +136,7 @@ export const AppsSideNav: React.FC<{ categories: TAppCategory[] }> = ({
       selectedKeys={activeApp}
       className={styles.apps}
       items={items}
-      inlineCollapsed={activeApp}
-    >
-      <MenuDivider />
-      <h3>Applications:</h3>
-      <MenuDivider />
-    </Menu>
+      // inlineCollapsed={activeApp}
+    />
   );
 };
