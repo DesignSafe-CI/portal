d48f038c8c23baddd0acb2f0aec6cf1caf487208
Sal Tijerina <r.sal.tijerina@gmail.com>
2024-05-20 18:38:11 -0500

    fix persistent previous app state in step

--- client/modules/workspace/src/AppsSubmissionForm/AppsSubmissionForm.tsx
+++ client/modules/workspace/src/AppsSubmissionForm/AppsSubmissionForm.tsx
@@ -173,8 +173,23 @@ export const AppsSubmissionForm: React.FC = () => {
   });
   const { handleSubmit, reset, setValue, getValues, watch } = methods;
 
+  // Make step part of state to allow steps
+  // to handle field changes
+  const [steps, setSteps] = useState<TStep>({
+    inputs: getInputsStep(fileInputs.fields),
+    parameters: getParametersStep(parameterSet.fields),
+    configuration: getConfigurationStep(configuration.fields),
+    outputs: getOutputsStep(outputs.fields),
+  });
+
   useEffect(() => {
     reset(initialValues);
+    setSteps({
+      inputs: getInputsStep(fileInputs.fields),
+      parameters: getParametersStep(parameterSet.fields),
+      configuration: getConfigurationStep(configuration.fields),
+      outputs: getOutputsStep(outputs.fields),
+    });
   }, [initialValues]);
 
   const [current, setCurrent] = useState('inputs');
@@ -246,15 +261,6 @@ export const AppsSubmissionForm: React.FC = () => {
       content: JSX.Element;
     };
   }
-
-  // Make step part of state to allow steps
-  // to handle field changes
-  const [steps, setSteps] = useState<TStep>({
-    inputs: getInputsStep(fileInputs.fields),
-    parameters: getParametersStep(parameterSet.fields),
-    configuration: getConfigurationStep(configuration.fields),
-    outputs: getOutputsStep(outputs.fields),
-  });
 
   // Note: currently configuration is the only
   // step that needs. This can be more generic
