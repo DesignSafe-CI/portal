c3417fa85e2522fa54eff6b06869c1f2c634ff9d
Garrett Edmonds <gedmonds@gedmonds-mbp24.local>
2024-05-22 14:16:00 -0500

    Pylint errors fixes

--- designsafe/apps/workspace/models/allocations.py
+++ designsafe/apps/workspace/models/allocations.py
@@ -1,36 +1,12 @@
 from django.db import models
 from django.contrib.auth.models import User
 
+"""
+Model linking django db user to their TAS allocations + corresponding hosts
+
+"""
+
 class UserAllocations(models.Model):
-    """
-    Elasticsearch document representing cached allocations. Thin wrapper around
-    `elasticsearch_dsl.Document`.
-    """
 
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     value = models.JSONField()
-
-    '''@classmethod
-    def from_username(cls, username):
-        """
-        Fetches cached allocations for a user.
-
-        Parameters
-        ----------
-        username: str
-            TACC username to fetch allocations for.
-        Returns
-        -------
-        UserAllocations
-
-        Raises
-        ------
-        elasticsearch.exceptions.NotFoundError
-        """
-        es_client = new_es_client()
-        uuid = get_sha256_hash(username)
-        return cls.get(uuid, using=es_client)
-
-    class Index:
-        name = settings.ES_INDEX_PREFIX.format('allocations')
-    '''
