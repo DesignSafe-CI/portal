a5f39ab103cc37a4f40a5a203001602c315024e5
Garrett Edmonds <gedmonds@gedmonds-mbp24.local>
2024-05-24 15:08:28 -0500

    Pylint fixes for allocations model

--- designsafe/apps/workspace/models/allocations.py
+++ designsafe/apps/workspace/models/allocations.py
@@ -1,12 +1,13 @@
-from django.db import models
-from django.contrib.auth.models import User
-
 """
 Model linking django db user to their TAS allocations + corresponding hosts
 
 """
 
-class UserAllocations(models.Model):
+from django.db import models
+from django.contrib.auth.models import User
 
+class UserAllocations(models.Model):
+    """ Model establishes one-to-one relation to a user's Django User model + their TAS allocations """
+    
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     value = models.JSONField()
