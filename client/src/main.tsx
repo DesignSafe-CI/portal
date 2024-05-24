460f0ef86f9ad6801950a31aadaffde8b8c401b4
Sal Tijerina <r.sal.tijerina@gmail.com>
2024-05-24 15:52:16 -0500

    revert helvetica default

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -14,7 +14,6 @@ const themeConfig: ThemeConfig = {
     colorPrimary: '#337ab7',
     colorError: '#d9534f',
     colorPrimaryTextHover: 'black',
-    fontFamily: 'Helvetica Neue',
   },
   components: {
     Table: {
