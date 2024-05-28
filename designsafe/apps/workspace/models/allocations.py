3e8c6f4a2ed06ed5418019b32373cb3072d228cb
Garrett Edmonds <gedmonds@gedmonds-mbp24.local>
2024-05-28 10:56:19 -0500

    Fix poetry black reformatting checks

--- designsafe/apps/workspace/models/allocations.py
+++ designsafe/apps/workspace/models/allocations.py
@@ -6,8 +6,9 @@ Model linking django db user to their TAS allocations + corresponding hosts
 from django.db import models
 from django.contrib.auth import models as auth_models
 
+
 class UserAllocations(models.Model):
-    """ Model establishes one-to-one relation to a user's Django User model + their TAS allocations """
+    """Model establishes one-to-one relation to a user's Django User model + their TAS allocations"""
 
     user = models.OneToOneField(auth_models.User, on_delete=models.CASCADE)
     value = models.JSONField()
