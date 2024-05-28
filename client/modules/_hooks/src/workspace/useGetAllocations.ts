fff51a6fad587db75ec676f9581074571a81597b
Garrett Edmonds <gedmonds@gedmonds-mbp24.local>
2024-05-28 10:48:43 -0500

    Fix client linting errors

--- client/modules/_hooks/src/workspace/useGetAllocations.ts
+++ client/modules/_hooks/src/workspace/useGetAllocations.ts
@@ -1,38 +1,39 @@
-import {
-    useQuery
-} from '@tanstack/react-query';
+import { useQuery } from '@tanstack/react-query';
 import apiClient from '../apiClient';
 import { TTasAllocations } from './types';
 
 export type TAllocParamsType = {
-    username: string;
-}
+  username: string;
+};
 
 export type TGetAllocationsResponse = {
-    response: TTasAllocations;
-    status: number;
-}
+  response: TTasAllocations;
+  status: number;
+};
 
 async function getAllocations(
-    { signal }: { signal: AbortSignal },
-    params: TAllocParamsType
-  ) {
-    const res = await apiClient.get<TGetAllocationsResponse>(`/api/workspace/allocations`, {
+  { signal }: { signal: AbortSignal },
+  params: TAllocParamsType
+) {
+  const res = await apiClient.get<TGetAllocationsResponse>(
+    `/api/workspace/allocations`,
+    {
       signal,
       params,
-    });
-    return res.data.response;
-  }
+    }
+  );
+  return res.data.response;
+}
+
 const getAllocationsQuery = (queryParams: TAllocParamsType) => ({
-    queryKey: ['workspace', 'getAllocations', queryParams],
-    queryFn: ({ signal }: { signal: AbortSignal }) =>
-        getAllocations({ signal }, queryParams),
-    staleTime: 5000,
+  queryKey: ['workspace', 'getAllocations', queryParams],
+  queryFn: ({ signal }: { signal: AbortSignal }) =>
+    getAllocations({ signal }, queryParams),
+  staleTime: 5000,
 });
 
 const useGetAllocations = (queryParams: TAllocParamsType) => {
-    return useQuery(getAllocationsQuery(queryParams));
+  return useQuery(getAllocationsQuery(queryParams));
 };
 
 export default useGetAllocations;
-  
