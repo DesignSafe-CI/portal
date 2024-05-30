ad836d3d5159e7e5a4c55f9caeff74ea4f088d57
Van Go <35277477+van-go@users.noreply.github.com>
2024-05-30 14:44:16 -0500

    Refactor ProjectPreview module, fix useState declaration

--- client/modules/datafiles/src/projects/ProjectPreview/ProjectPreview.tsx
+++ client/modules/datafiles/src/projects/ProjectPreview/ProjectPreview.tsx
@@ -113,7 +113,7 @@ export const PublishedEntityDisplay: React.FC<{
   defaultOpenChildren = false,
 }) => {
   const [active, setActive] = useState<boolean>(defaultOpen);
-  const [ , setIsModalVisible] = useState(false);
+  const [, setIsModalVisible] = useState(false);
   const sortedChildren = useMemo(
     () => [...(treeData.children ?? [])].sort((a, b) => a.order - b.order),
     [treeData]
