480911c8d3ea90750c38c22b11804d871e9073f8
Van Go <35277477+van-go@users.noreply.github.com>
2024-05-30 21:41:43 -0500

    Update index.ts

--- client/modules/_hooks/src/datafiles/publications/index.ts
+++ client/modules/_hooks/src/datafiles/publications/index.ts
@@ -1,5 +1,9 @@
 export { usePublishedListing } from './usePublishedListing';
 export type { TPublicationListingItem } from './usePublishedListing';
+export type { TPublicationTree } from './usePublicationDetail';
 export { usePublicationDetail } from './usePublicationDetail';
 export { usePublicationVersions } from './usePublicationVersions';
 export { useCitationMetrics } from './useCitationMetrics';
+export { usePublishProject } from './usePublishProject';
+export { useVersionProject } from './useVersionProject';
+export { useAmendProject } from './useAmendProject';
