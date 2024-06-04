281dc937d77274f8ee0db543b9d7a17e9493a31b
Chandra Y <cyemparala@tacc.utexas.edu>
2024-06-04 12:28:59 -0500

    Bug DES-2876: Select Modal bug fixes (#1284)
    * Select Modal fixes
    
    * Update usePathDisplayName.ts
    
    * Fix from merge issue - getPathName
    
    * Breadcrumb use system label

--- client/modules/_hooks/src/datafiles/usePathDisplayName.ts
+++ client/modules/_hooks/src/datafiles/usePathDisplayName.ts
@@ -1,7 +1,11 @@
 import { useCallback } from 'react';
 import { useAuthenticatedUser } from '../useAuthenticatedUser';
 
-export function getSystemRootDisplayName(api: string, system: string): string {
+export function getSystemRootDisplayName(
+  api: string,
+  system: string,
+  label: string = 'Data Files'
+): string {
   if (api === 'googledrive') return 'Google Drive';
   if (api === 'box') return 'Box';
   if (api === 'dropbox') return 'Dropbox';
@@ -10,7 +14,7 @@ export function getSystemRootDisplayName(api: string, system: string): string {
       'designsafe.storage.default': 'My Data',
       'designsafe.storage.frontera.work': 'My Data (Work)',
       'designsafe.storage.community': 'Community Data',
-    }[system] ?? 'Data Files'
+    }[system] ?? label
   );
 }
 
@@ -18,11 +22,12 @@ function _getPathDisplayName(
   api: string,
   system: string,
   path: string,
+  label: string,
   username?: string
 ) {
   const usernamePath = encodeURIComponent('/' + username);
 
-  if (!path) return getSystemRootDisplayName(api, system);
+  if (!path) return getSystemRootDisplayName(api, system, label);
   if (api === 'googledrive' && !path) return 'Google Drive';
   if (api === 'dropbox' && !path) return 'Dropbox';
   if (api === 'box' && !path) return 'Box';
@@ -34,15 +39,15 @@ function _getPathDisplayName(
     return 'HPC Work';
   }
 
-  return decodeURIComponent(path).split('/').slice(-1)[0] || 'Data Files';
+  return decodeURIComponent(path).split('/').slice(-1)[0] || label;
 }
 
 export function usePathDisplayName() {
   const { user } = useAuthenticatedUser();
 
   const getPathDisplayName = useCallback(
-    (api: string, system: string, path: string) =>
-      _getPathDisplayName(api, system, path, user?.username),
+    (api: string, system: string, path: string, label: string = 'Data Files') =>
+      _getPathDisplayName(api, system, path, label, user?.username),
     [user]
   );
 
