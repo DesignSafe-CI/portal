ade75160b9e58a063163cba1f44fe482cb0bff20
Van Go <35277477+van-go@users.noreply.github.com>
2024-06-14 14:30:49 -0500

    Remove file from the pull request

--- client/modules/datafiles/src/projects/modals/index.ts
+++
@@ -1,7 +0,0 @@
-export { BaseProjectUpdateModal } from './BaseProjectUpdateModal';
-export { RelateDataModal } from './RelateDataModal';
-export { ManageCategoryModal } from './ManageCategoryModal';
-export { ManagePublishableEntityModal } from './ManagePublishableEntityModal';
-export { ChangeProjectTypeModal } from './ChangeProjectTypeModal';
-export { MetricsModal } from './MetricsModal';
-export { ProjectBestPracticesModal } from './ProjectBestPracticesModal';
