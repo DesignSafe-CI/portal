98b341a5803425a48f1056f2a6dfb3ed2ce3253e
Sal Tijerina <r.sal.tijerina@gmail.com>
2024-06-17 12:03:33 -0500

    accept jwt auth for license view (#1300)

--- designsafe/apps/api/licenses/views.py
+++ designsafe/apps/api/licenses/views.py
@@ -1,31 +1,32 @@
-from designsafe.apps.api.views import BaseApiView
-from designsafe.apps.api.mixins import SecureMixin
-from designsafe.libs.common.decorators import profile as profile_fn
+"""Views for the licenses API."""
+
 from django.contrib.auth import get_user_model
 from django.http.response import HttpResponseForbidden, HttpResponseNotFound
 from django.http import JsonResponse
 from django.apps import apps
-from designsafe.apps.data.models.agave.util import AgaveJSONEncoder
+from designsafe.apps.api.views import AuthenticatedAllowJwtApiView
 import logging
 
 logger = logging.getLogger(__name__)
 
 
-class LicenseView(SecureMixin, BaseApiView):
-    @profile_fn
+class LicenseView(AuthenticatedAllowJwtApiView):
+    """View for retrieving licenses for a specific app."""
+
     def get(self, request, app_name):
+        """Return the license for the given app."""
         if not request.user.is_staff:
             return HttpResponseForbidden()
 
         try:
-            app_license = apps.get_model('designsafe_licenses', '{}License'.format(app_name))
+            app_license = apps.get_model("designsafe_licenses", f"{app_name}License")
         except LookupError:
             return HttpResponseNotFound()
-        username = request.GET.get('username', None)
+        username = request.GET.get("username", None)
         if not username:
             return HttpResponseNotFound()
         user = get_user_model().objects.get(username=username)
         licenses = app_license.objects.filter(user=user)
 
-        user_license = licenses[0].license_as_str() if len(licenses) > 0 else ''
-        return JsonResponse({'license': user_license}, encoder=AgaveJSONEncoder)
+        user_license = licenses[0].license_as_str() if len(licenses) > 0 else ""
+        return JsonResponse({"license": user_license})
