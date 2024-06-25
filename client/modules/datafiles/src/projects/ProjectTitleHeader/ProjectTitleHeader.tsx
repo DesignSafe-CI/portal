63ac63165d07bebb2d7bd08919b6d2be949899ca
Jake Rosenberg <jrosenberg@tacc.utexas.edu>
2024-06-25 13:33:50 -0500

    allow editing of type None; fix guest member input on type Other

--- client/modules/datafiles/src/projects/ProjectTitleHeader/ProjectTitleHeader.tsx
+++ client/modules/datafiles/src/projects/ProjectTitleHeader/ProjectTitleHeader.tsx
@@ -1,6 +1,6 @@
 import { useProjectDetail } from '@client/hooks';
 import React from 'react';
-import { BaseProjectUpdateModal, ChangeProjectTypeModal } from '../modals';
+import { BaseProjectUpdateModal } from '../modals';
 import { Button } from 'antd';
 
 export const ProjectTitleHeader: React.FC<{ projectId: string }> = ({
@@ -17,28 +17,14 @@ export const ProjectTitleHeader: React.FC<{ projectId: string }> = ({
         {baseProject.value.projectId} |{' '}
         <span style={{ fontWeight: 'normal' }}>{baseProject.value.title}</span>
       </h3>
-      {data.baseProject.value.projectType === 'None' ? (
-        <ChangeProjectTypeModal projectId={projectId}>
-          {({ onClick }) => (
-            <Button
-              onClick={(evt) => {
-                onClick(evt);
-              }}
-              type="link"
-            >
-              <strong>Select Project Type</strong>
-            </Button>
-          )}
-        </ChangeProjectTypeModal>
-      ) : (
-        <BaseProjectUpdateModal projectId={projectId}>
-          {({ onClick }) => (
-            <Button onClick={onClick} type="link">
-              <strong>Edit Project</strong>
-            </Button>
-          )}
-        </BaseProjectUpdateModal>
-      )}
+
+      <BaseProjectUpdateModal projectId={projectId}>
+        {({ onClick }) => (
+          <Button onClick={onClick} type="link">
+            <strong>Edit Project</strong>
+          </Button>
+        )}
+      </BaseProjectUpdateModal>
     </div>
   );
 };
