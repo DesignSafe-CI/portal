35f38fb5d6e8efb53eb071bf0f0e70726d847a22
Van Go <35277477+van-go@users.noreply.github.com>
2024-07-02 16:14:48 -0500

    feat: Add cache page decorator to PublicationDetailView and PublicationDataCiteView

--- designsafe/apps/api/publications/views.py
+++ designsafe/apps/api/publications/views.py
@@ -6,6 +6,7 @@ from requests.exceptions import HTTPError
 from designsafe.apps.api.publications import operations
 from designsafe.apps.projects.managers import datacite as DataciteManager
 from django.views.decorators.cache import cache_page
+from django.utils.decorators import method_decorator
 import json
 import logging
 import requests
@@ -30,7 +31,7 @@ class PublicationListingView(BaseApiView):
 View for getting details (description, full definition) from a specific publication.
 """
 class PublicationDetailView(BaseApiView):
-    @cache_page(60 * 15)
+    @method_decorator(cache_page(60 * 15))
     def get(self, request, operation, project_id, revision=None):
         client = get_service_account_client()
         try:
@@ -44,7 +45,7 @@ class PublicationDetailView(BaseApiView):
 View for getting DataCite DOI details from publications.
 """
 class PublicationDataCiteView(BaseApiView):
-    @cache_page(60 * 15)
+    @method_decorator(cache_page(60 * 15))
     def get(self, request, doi):
         url = f'https://api.datacite.org/dois/{doi}' 
 
