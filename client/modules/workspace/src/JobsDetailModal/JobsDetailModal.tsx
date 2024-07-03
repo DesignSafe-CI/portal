22e05c493a3dd927044c4f0cdfefc23780e9bbc9
Chandra Y <cyemparala@tacc.utexas.edu>
2024-07-03 16:03:03 -0500

    Job Details Modal

--- client/modules/workspace/src/JobsDetailModal/JobsDetailModal.tsx
+++ client/modules/workspace/src/JobsDetailModal/JobsDetailModal.tsx
@@ -1,16 +1,123 @@
 import React, { useState, useEffect } from 'react';
-import { Modal, Layout, Button } from 'antd';
+import { Collapse, Modal, Layout, Button } from 'antd';
 import { useNavigate } from 'react-router-dom';
-import { useGetJobs, TTapisJob } from '@client/hooks';
+import { useGetApps, useGetJobs, TAppResponse, TTapisJob } from '@client/hooks';
 import styles from './JobsDetailModal.module.css';
-import { getStatusText, isOutputState, isTerminalState } from '../utils/jobs';
+import {
+  getOutputPath,
+  getStatusText,
+  isOutputState,
+  isTerminalState,
+  getJobDisplayInformation,
+  TJobDisplayInputOrParameter,
+  TJobDisplayInfo,
+} from '../utils/jobs';
 import { formatDateTimeFromValue } from '../utils/timeFormat';
 import { JobActionButton } from '../JobsListing/JobsListing';
+import { DescriptionList } from '../DescriptionList/DescriptionList';
 import { Spinner } from '@client/common-components';
+
+type InputParamsObj = {
+  [key: string]: string;
+};
+const reduceInputParameters = (data: TJobDisplayInputOrParameter[]) =>
+  data.reduce((acc: InputParamsObj, item: TJobDisplayInputOrParameter) => {
+    acc[item.label] = item.value ?? '';
+    return acc;
+  }, {});
 
 export const JobsDetailModalBody: React.FC<{
   jobData: TTapisJob;
-}> = ({ jobData }) => {
+  appData: TAppResponse | undefined;
+}> = ({ jobData, appData }) => {
+  const jobDisplay = getJobDisplayInformation(
+    jobData,
+    appData
+  ) as TJobDisplayInfo;
+
+  const outputLocation = getOutputPath(jobData);
+  const created = formatDateTimeFromValue(new Date(jobData.created));
+  const lastUpdated = formatDateTimeFromValue(new Date(jobData.lastUpdated));
+  const hasFailedStatus = jobData.status === 'FAILED';
+
+  const appDataObj = {
+    'App ID': jobDisplay.appId,
+    ...('appVersion' in jobDisplay
+      ? { 'App Version': jobDisplay.appVersion }
+      : {}),
+  };
+  const lastMessageTitle = hasFailedStatus
+    ? 'Failure Report'
+    : 'Last Status Message';
+  const statusDataObj = {
+    Submitted: created,
+    [`${getStatusText(jobData.status)}`]: lastUpdated,
+    [lastMessageTitle]: (
+      <Collapse
+        defaultActiveKey={[]}
+        expandIconPosition="end"
+        size="small"
+        bordered={false}
+        style={{ flex: 1 }}
+        items={[
+          {
+            label: (
+              <span>
+                {hasFailedStatus ? 'Last Status Message' : 'System Output'}
+              </span>
+            ),
+            children: <pre>{jobData.lastMessage}</pre>,
+          },
+        ]}
+      ></Collapse>
+    ),
+    ...(jobData.remoteOutcome && { 'Remote Outcome': jobData.remoteOutcome }),
+  };
+
+  if (jobData.remoteOutcome) {
+    statusDataObj['Remote Outcome'] = jobData.remoteOutcome;
+  }
+
+  const configDataObj = {
+    'Execution System': jobDisplay.systemName,
+    'Max Minutes': String(jobData.maxMinutes),
+    queue: 'queue' in jobDisplay ? jobDisplay.queue : undefined,
+    'Cores On Each Node':
+      'coresPerNode' in jobDisplay
+        ? String(jobDisplay.coresPerNode)
+        : undefined,
+    'Node Count':
+      'coresPerNode' in jobDisplay
+        ? String(jobDisplay.coresPerNode)
+        : undefined,
+    Allocation: 'allocation' in jobDisplay ? jobDisplay.allocation : undefined,
+    'Execution Directory':
+      'execSystemExecDir' in jobData ? jobData.execSystemExecDir : undefined,
+  };
+  const outputDataObj = {
+    'Job Name': jobData.name,
+    'Output Location': outputLocation,
+    'Archive System': jobData.archiveSystemId,
+    'Archive Directory': jobData.archiveSystemDir,
+  };
+
+  const inputDataObj = {
+    ...reduceInputParameters(jobDisplay.inputs),
+  };
+
+  const paramsDataObj = {
+    ...reduceInputParameters(jobDisplay.appArgs),
+    ...reduceInputParameters(jobDisplay.envVars),
+  };
+  const data = {
+    Application: <DescriptionList data={appDataObj} />,
+    Status: <DescriptionList data={statusDataObj} />,
+    Inputs: <DescriptionList data={inputDataObj} />,
+    Parameters: <DescriptionList data={paramsDataObj} />,
+    Configuration: <DescriptionList data={configDataObj} />,
+    Output: <DescriptionList data={outputDataObj} />,
+  };
+
   return (
     <div className={styles['modal-body-container']}>
       <div className={`${styles['left-panel']}`}>
@@ -70,30 +177,10 @@ export const JobsDetailModalBody: React.FC<{
           />
         )}
       </div>
-      <dl className={`${styles['right-panel']} ${styles['panel-content']}`}>
-        <dt>Application ID</dt>
-        <dd>{jobData.appId}</dd>
-        {jobData.appVersion && (
-          <>
-            <dt>Application Version</dt>
-            <dd>{jobData.appVersion}</dd>
-          </>
-        )}
-        <dt>Job ID</dt>
-        <dd>{jobData.uuid}</dd>
-        <dt>Status</dt>
-        <dd>{getStatusText(jobData.status)}</dd>
-        <dt>Submitted</dt>
-        <dd>{formatDateTimeFromValue(jobData.created)}</dd>
-        {jobData.ended && (
-          <>
-            <dt>Finished</dt>
-            <dd>{formatDateTimeFromValue(jobData.ended)}</dd>
-          </>
-        )}
-        <dt>Last Status Message</dt>
-        <dd>{jobData.lastMessage}</dd>
-      </dl>
+      <DescriptionList
+        className={`${styles['right-panel']} ${styles['panel-content']}`}
+        data={data}
+      />
     </div>
   );
 };
@@ -105,9 +192,19 @@ export const JobsDetailModal: React.FC<{ uuid: string }> = ({ uuid }) => {
   const handleCancel = () => {
     navigate('..', { relative: 'path' });
   };
-
   const { data: jobData, isLoading } = useGetJobs('select', { uuid }) as {
     data: TTapisJob;
+    isLoading: boolean;
+  };
+
+  const appId = jobData?.appId;
+  const appVersion = jobData?.appVersion;
+
+  const { data: appData, isLoading: isAppLoading } = useGetApps({
+    appId,
+    appVersion,
+  }) as {
+    data: TAppResponse;
     isLoading: boolean;
   };
 
@@ -140,12 +237,12 @@ export const JobsDetailModal: React.FC<{ uuid: string }> = ({ uuid }) => {
       onCancel={handleCancel}
       footer={null}
     >
-      {isLoading ? (
+      {isLoading && isAppLoading ? (
         <Layout style={{ height: 300 }}>
           <Spinner />
         </Layout>
       ) : (
-        jobData && <JobsDetailModalBody jobData={jobData} />
+        jobData && <JobsDetailModalBody jobData={jobData} appData={appData} />
       )}
     </Modal>
   );
--- client/modules/workspace/src/utils/jobs.ts
+++ client/modules/workspace/src/utils/jobs.ts
@@ -1,5 +1,12 @@
 import { STATUS_TEXT_MAP, TERMINAL_STATES } from '../constants';
-import { TJobStatusNotification, TTapisJob } from '@client/hooks';
+import {
+  TJobStatusNotification,
+  TAppResponse,
+  TTapisJob,
+  TAppFileInput,
+  TJobArgSpecs,
+  TJobKeyValuePair,
+} from '@client/hooks';
 
 export function getStatusText(status: string) {
   if (status in STATUS_TEXT_MAP) {
@@ -72,4 +79,137 @@ export function getJobInteractiveSessionInfo(
     interactiveSessionLink: notif?.action_link,
     message: notif?.message,
   };
+}
+
+/* Return allocation
+   queue directive has form: '-A TACC-ACI'
+   */
+export function getAllocatonFromDirective(directive: string | undefined) {
+  if (!directive) return directive;
+  const parts = directive.split(' ');
+  const allocationArgIndex = parts.findIndex((obj) => obj === '-A') + 1;
+  if (allocationArgIndex !== 0 && allocationArgIndex < parts.length) {
+    return parts[allocationArgIndex];
+  }
+  return null;
+}
+
+export type TJobDisplayInputOrParameter = {
+  label: string;
+  id?: string;
+  value?: string;
+};
+
+export type TJobDisplayInfo = {
+  appId: string;
+  appVersion?: string;
+  applicationName: string;
+  systemName: string;
+  inputs: TJobDisplayInputOrParameter[];
+  appArgs: TJobDisplayInputOrParameter[];
+  envVars: TJobDisplayInputOrParameter[];
+  archiveOnAppError: boolean;
+  archiveSystemDir: string;
+  archiveSystemId: string;
+  archiveTransactionId?: string;
+  exec_system_id: string;
+  workPath: string;
+  allocation?: string;
+  queue?: string;
+  coresPerNode?: number;
+  nodeCount?: number;
+};
+
+/**
+ * Get display values from job, app, and execution system info.
+ */
+export function getJobDisplayInformation(
+  job: TTapisJob,
+  app: TAppResponse | undefined
+): TJobDisplayInfo {
+  const filterAppArgs = (objects: TJobArgSpecs) =>
+    objects.filter((obj) => !obj.notes || !obj.notes.isHidden);
+
+  const filterInputs = (objects: TAppFileInput[]) =>
+    objects.filter(
+      (obj) =>
+        (!obj.notes || !obj.notes.isHidden) &&
+        !(obj.name || obj.sourceUrl || '').startsWith('_')
+    );
+
+  const filterParameters = (objects: TJobKeyValuePair[]) =>
+    objects.filter(
+      (obj) => (!obj.notes || !obj.notes.isHidden) && !obj.key.startsWith('_')
+    );
+
+  const fileInputs = filterInputs(
+    JSON.parse(job.fileInputs) as TAppFileInput[]
+  );
+  const parameterSet = JSON.parse(job.parameterSet);
+  const parameters = filterAppArgs(parameterSet.appArgs) as TJobArgSpecs;
+
+  const displayEnvVariables = filterParameters(parameterSet.envVariables);
+  const envVariables = parameterSet.envVariables as TJobKeyValuePair[];
+  const schedulerOptions = parameterSet.schedulerOptions as TJobArgSpecs;
+
+  const display: TJobDisplayInfo = {
+    appId: job.appId,
+    applicationName: job.appId,
+    appVersion: job.appVersion,
+    systemName: job.execSystemId,
+    inputs: fileInputs.map((input) => ({
+      label: input.name || 'Unnamed Input',
+      id: input.sourceUrl,
+      value: input.sourceUrl,
+    })),
+    appArgs: parameters.map((parameter) => ({
+      label: parameter.name,
+      id: parameter.name,
+      value: parameter.arg,
+    })),
+    envVars: displayEnvVariables.map((d) => ({
+      label: d.notes?.label ?? d.key,
+      id: d.key,
+      value: d.value,
+    })),
+    archiveOnAppError: false,
+    archiveSystemDir: '',
+    archiveSystemId: '',
+    exec_system_id: job.execSystemId,
+    workPath: '',
+  };
+
+  if (app) {
+    try {
+      display.applicationName =
+        app.definition.notes.label || display.applicationName;
+
+      const workPath = envVariables.find(
+        (env) => env.key === '_tapisJobWorkingDir'
+      );
+      display.workPath = workPath ? workPath.value : '';
+
+      if (app.definition.jobType === 'BATCH') {
+        const allocationParam = schedulerOptions.find(
+          (opt) => opt.name === 'TACC Allocation'
+        );
+        if (allocationParam) {
+          const allocation = getAllocatonFromDirective(allocationParam.arg);
+          if (allocation) {
+            display.allocation = allocation;
+          }
+        }
+        display.queue = job.execSystemLogicalQueue;
+      }
+
+      if (!app.definition.notes.hideNodeCountAndCoresPerNode) {
+        display.coresPerNode = job.coresPerNode;
+        display.nodeCount = job.nodeCount;
+      }
+    } catch (ignore) {
+      // Ignore if there is a problem using the app definition to improve display
+    }
+  }
+
+  return display;
 }
