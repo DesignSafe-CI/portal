357f1de5d02ed36d7eaa526b9a5b2cd1028d959d
Van Go <35277477+van-go@users.noreply.github.com>
2024-07-08 10:48:16 -0500

    Bug/DES-2982: text/typo/wording/spelling issues (#1329)
    * Fix typo in curation office hours link
    
    * chore: Fix typo in Publish/ Amend/ Version

--- client/modules/datafiles/src/projects/modals/ProjectInfoStepper/ExperimentalSteps.tsx
+++ client/modules/datafiles/src/projects/modals/ProjectInfoStepper/ExperimentalSteps.tsx
@@ -163,7 +163,7 @@ const ExperimentOverview4 = (
         target="_blank"
         aria-describedby="msg-open-new-window"
       >
-        Curation office hours
+        curation office hours
       </a>{' '}
       at the beginning of the curation process. You can <a>download</a> and
       print this diagram to sketch and organize your work.
