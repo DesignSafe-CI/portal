8d2090524534647057bb6afc8af8432e0d11461b
Jake Rosenberg <jrosenberg@tacc.utexas.edu>
2024-07-16 10:14:11 -0500

    Task/DES-2601: DOI logging for file/publication operations (#1356)
    * log DOIs when published files are downloaded/copied/previewed
    
    * re-add logging when entity dropdown is opened

--- client/modules/_hooks/src/datafiles/useFileDetail.ts
+++ client/modules/_hooks/src/datafiles/useFileDetail.ts
@@ -20,12 +20,13 @@ export function useFileDetail(
   api: string,
   system: string,
   scheme: string,
-  path: string
+  path: string,
+  enabled: boolean = true
 ) {
   return useQuery({
     queryKey: ['datafiles', 'fileListing', 'detail', api, scheme, system, path],
     queryFn: ({ signal }) =>
       getFileDetail(api, system, path, scheme, { signal }),
-    enabled: !!path,
+    enabled: !!path && enabled,
   });
 }
