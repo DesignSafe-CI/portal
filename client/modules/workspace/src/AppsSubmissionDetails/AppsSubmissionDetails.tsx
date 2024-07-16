cb4c47f7e18b579f236ecf50e863b6a193433df7
fnets <fnetscher@tacc.utexas.edu>
2024-07-16 13:46:24 -0500

    task/DES-2939 -  New App Notes Field "hideQueue"  (#1326)
    * Adding hideQueue flag for apps to hide the queue selection from users. Should only be used with Compress/Extract for now.
    
    * Can now hide queue from job subission summary
    
    * As suggested, Queue and Allocation now hide independently of each other, and the fields are hidden from the Summary if the app definition requires it.

--- client/modules/workspace/src/AppsSubmissionDetails/AppsSubmissionDetails.tsx
+++ client/modules/workspace/src/AppsSubmissionDetails/AppsSubmissionDetails.tsx
@@ -91,6 +91,20 @@ export const AppsSubmissionDetails: React.FC<{
         );
       }
       entries.forEach(([k, v], childIndex) => {
+        if (
+          definition.notes.hideQueue &&
+          key === 'configuration' &&
+          k === 'execSystemLogicalQueue'
+        ) {
+          return; // Hide the queue, if the app definition requires it
+        }
+        if (
+          definition.notes.hideAllocation &&
+          key === 'configuration' &&
+          k === 'allocation'
+        ) {
+          return; // Hide the allocation, if that field is true
+        }
         if (v instanceof Object) {
           Object.entries(v as object).forEach(([kk, vv], zchildIndex) => {
             const nestedFieldSchema = parent?.shape?.[k]?.shape?.[kk];
