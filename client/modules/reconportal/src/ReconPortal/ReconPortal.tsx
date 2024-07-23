41c673f16d284d94ad7069bb5119cbc255c71fc7
Nathan Franklin <nathan.franklin@gmail.com>
2024-07-23 16:46:34 -0500

    Add module and placeholder components

---
+++ client/modules/reconportal/src/ReconPortal/ReconPortal.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import styles from './ReconPortal.module.css';
+
+export const ReconPortal: React.FC = () => {
+
+  return (
+    <div className={styles.root}>
+      Leaflet + Left-Sided Nav Placeholder
+    </div>
+  );
+};
---
+++ client/modules/reconportal/src/ReconPortalHeader/ReconPortalHeader.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import styles from './ReconPortalHeader.module.css';
+
+export const ReconPortalHeader: React.FC = () => {
+
+  return (
+    <div className={styles.root}>
+      Header Placeholder
+    </div>
+  );
+};
