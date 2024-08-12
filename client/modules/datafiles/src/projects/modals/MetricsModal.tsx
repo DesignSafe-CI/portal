290370d16cb22085e4b40fbc0ea5616fde579bf5
Van Go <35277477+van-go@users.noreply.github.com>
2024-08-12 15:43:21 -0500

    Revert "Update quarters in MetricsModal to match correct time periods"
    This reverts commit 5db4818e88dd014bb31a477dccad35cec5dfbd26.

--- client/modules/datafiles/src/projects/modals/MetricsModal.tsx
+++ client/modules/datafiles/src/projects/modals/MetricsModal.tsx
@@ -314,7 +314,7 @@ export const MetricsModalBody: React.FC<MetricsModalProps> = ({
     },
     {
       key: '2',
-      quarters: 'Apr-Jun',
+      quarters: 'Apr-Jul',
       uniqueInvestigations:
         (quarterSums.views as { [key: string]: number }).Q2 || '--',
       uniqueRequests:
@@ -324,7 +324,7 @@ export const MetricsModalBody: React.FC<MetricsModalProps> = ({
     },
     {
       key: '3',
-      quarters: 'Jul-Sep',
+      quarters: 'Aug-Oct',
       uniqueInvestigations:
         (quarterSums.views as { [key: string]: number }).Q3 || '--',
       uniqueRequests:
@@ -334,7 +334,7 @@ export const MetricsModalBody: React.FC<MetricsModalProps> = ({
     },
     {
       key: '4',
-      quarters: 'Oct-Dec',
+      quarters: 'Nov-Dec',
       uniqueInvestigations:
         (quarterSums.views as { [key: string]: number }).Q4 || '--',
       uniqueRequests:
