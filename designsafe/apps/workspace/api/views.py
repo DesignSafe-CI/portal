a9e35626454304b1b50b325aaaafa65af5446151
Chandra Y <cyemparala@tacc.utexas.edu>
2024-08-14 14:42:19 -0500

    WP-655: Support for UserProjects in env variables

--- designsafe/apps/workspace/api/views.py
+++ designsafe/apps/workspace/api/views.py
@@ -766,6 +766,23 @@ class JobsView(AuthenticatedApiView):
             f"portalName: {settings.PORTAL_NAMESPACE}"
         ]
 
+        parameter_set = job_post.get("parameterSet", {})
+        env_variables = parameter_set.get("envVariables", [])
+        # Add user projects based on env var in the parameters.
+        projects_updated = False
+        for entry in env_variables:
+            if entry.get("key") == "_UserProjects":
+                projects = request.user.projects.order_by("-last_updated")
+                entry["value"] = " ".join(
+                    f"{project.uuid},{project.value['projectId'] if project.value['projectId'] != 'None' else project.uuid}"
+                    for project in projects[:500]
+                )
+                projects_updated = True
+                break
+
+        if projects_updated:
+            job_post["parameterSet"]["envVariables"] = env_variables 
+
         # Add additional data for interactive apps
         if body.get("isInteractive"):
             # Add webhook URL environment variable for interactive apps
