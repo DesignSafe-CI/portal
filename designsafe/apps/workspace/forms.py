1ee11f352a72d69f10d47eedfef5126196d3126b
Wesley Bomar <wbomar@tacc.utexas.edu>
2024-08-16 16:35:09 -0500

    style(lint): WA-326

--- designsafe/apps/workspace/forms.py
+++ designsafe/apps/workspace/forms.py
@@ -2,16 +2,21 @@
 
 from django.forms import ModelForm
 
-from designsafe.apps.workspace.models.app_entries import (
-    AppVariant
-)
+from designsafe.apps.workspace.models.app_entries import AppVariant
+
 
 class AppVariantForm(ModelForm):
     """Customizes app variant admin form"""
 
     class Meta:
         """To add or change help text"""
+
         model = AppVariant
         fields = []
 
-        help_texts = {'href': "The auto-generated path to app in the Apps Tray. Changes are reflected only after save.",}
+        help_texts = {
+            "href": (
+                "The auto-generated path to app in the Apps Tray. Changes are reflected"
+                " only after save."
+            ),
+        }
