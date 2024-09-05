4aec942b596f1dce242ea980510094e8a4efdb94
Jake Rosenberg <jrosenberg@tacc.utexas.edu>
2024-09-05 14:10:54 -0500

    Unset file selections when changing the selected publication version

--- client/modules/datafiles/src/projects/BaseProjectDetails.tsx
+++ client/modules/datafiles/src/projects/BaseProjectDetails.tsx
@@ -1,5 +1,9 @@
 import React, { useEffect, useState, useCallback } from 'react';
-import { TBaseProjectValue, TProjectUser } from '@client/hooks';
+import {
+  TBaseProjectValue,
+  TProjectUser,
+  useSelectedFiles,
+} from '@client/hooks';
 
 import styles from './BaseProjectDetails.module.css';
 import { Button, Col, Popover, Row, Select, Tooltip } from 'antd';
@@ -155,8 +159,14 @@ export const BaseProjectDetails: React.FC<{
   ].join(' | ');
 
   const [searchParams, setSearchParams] = useSearchParams();
+  const { unsetSelections } = useSelectedFiles(
+    'tapis',
+    'designsafe.storage.published',
+    ''
+  );
 
   const setSelectedVersion = (newVersion: number) => {
+    unsetSelections();
     setSearchParams((prevParams) => {
       prevParams.set('version', newVersion.toString());
       return prevParams;
