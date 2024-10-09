4a90578708924d5813169405cd9e2cb78aff06fb
Wesley B <62723358+wesleyboar@users.noreply.github.com>
2024-10-09 17:30:59 -0500

    chore: delete external_resource_secrets.sample.py
    Not needed. Do not reference file in README (next commit).

--- conf/env_files/external_resource_secrets.sample.py
+++
@@ -1,9 +0,0 @@
-"""External resource secrets"""
-
-###
-# Secrets for Google Auth flow
-#
-GOOGLE_OAUTH2_CLIENT_SECRET = "CHANGE_ME"
-GOOGLE_OAUTH2_CLIENT_ID = "CHANGE_ME"
-
-WEBHOOK_POST_URL = "CHANGE_ME"
