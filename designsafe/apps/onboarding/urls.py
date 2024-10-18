28fb786621523eb203b694653a49fa2c1ebfaa43
Sal Tijerina <r.sal.tijerina@gmail.com>
2024-10-18 11:19:12 -0500

    show onboarding admin in account dropdown

--- designsafe/apps/onboarding/urls.py
+++ designsafe/apps/onboarding/urls.py
@@ -1,9 +1,10 @@
 """URLs for the Onboarding app."""
 
-from django.urls import re_path
+from django.urls import re_path, path
 from designsafe.apps.onboarding.views import OnboardingView
 
 app_name = "workbench"
 urlpatterns = [
     re_path("^", OnboardingView.as_view(), name="onboarding"),
+    path("admin", OnboardingView.as_view(), name="admin"),
 ]
