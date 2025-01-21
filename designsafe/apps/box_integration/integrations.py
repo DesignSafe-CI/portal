ee54ab22d53db2b7e70686774df69c49e517d2ad
Juan Martinez <jmartinez@tacc.utexas.edu>
2025-01-21 16:21:14 -0600

    Remove Box and Google drive integrations. This is only UI change, no functional.

--- designsafe/apps/box_integration/integrations.py
+++ designsafe/apps/box_integration/integrations.py
@@ -3,9 +3,9 @@ from django.urls import reverse
 
 def provide_integrations():
     return [
-        {
-            'label': 'Box.com',
-            'href': reverse('box_integration:index'),
-            'description': 'Access files from your Box.com account in DesignSafe.',
-        },
+        #{
+        #    'label': 'Box.com',
+        #    'href': reverse('box_integration:index'),
+        #    'description': 'Access files from your Box.com account in DesignSafe.',
+        #},
     ]
--- designsafe/apps/googledrive_integration/integrations.py
+++ designsafe/apps/googledrive_integration/integrations.py
@@ -3,9 +3,9 @@ from django.urls import reverse
 
 def provide_integrations():
     return [
-        {
-            'label': 'GoogleDrive',
-            'href': reverse('googledrive_integration:index'),
-            'description': 'Access files from your Google Drive account in DesignSafe.',
-        },
+        #{
+        #    'label': 'GoogleDrive',
+        #    'href': reverse('googledrive_integration:index'),
+        #    'description': 'Access files from your Google Drive account in DesignSafe.',
+        #},
     ]
