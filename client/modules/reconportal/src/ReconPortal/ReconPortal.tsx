ca67a96b32b1fdbdf5ebb8519967b8105db81eb3
sophia-massie <smassie@tacc.utexas.edu>
2025-04-15 16:42:47 -0500

    - First pass of layout for ReconPortal

--- client/modules/reconportal/src/ReconPortal/ReconPortal.tsx
+++ client/modules/reconportal/src/ReconPortal/ReconPortal.tsx
@@ -1,8 +1,12 @@
 import React from 'react';
 import styles from './ReconPortal.module.css';
+import { Layout, Flex } from 'antd';
 
+const { Content } = Layout;
 export const ReconPortal: React.FC = () => {
   return (
-    <div className={styles.root}>Leaflet + Left-Sided Nav Placeholder</div>
+    <Layout className={styles.root}>
+      <Content>Leaflet + Left-Sided Nav Placeholder</Content>
+    </Layout>
   );
 };
