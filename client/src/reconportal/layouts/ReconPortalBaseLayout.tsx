8673d6bf4e66c3c805041049dd2cc1690a03c7e0
Nathan Franklin <nathan.franklin@gmail.com>
2025-05-24 18:13:38 -0500

    Add hook/context to select DS event

--- client/src/reconportal/layouts/ReconPortalBaseLayout.tsx
+++ client/src/reconportal/layouts/ReconPortalBaseLayout.tsx
@@ -1,34 +1,37 @@
 import React from 'react';
 import { Layout, Flex } from 'antd';
 import { ReconPortal, ReconSidePanel } from '@client/reconportal';
+import { SelectedReconPortalEventProvider } from '@client/hooks';
 
 const { Sider, Content } = Layout;
 
 const ReconPortalRoot: React.FC = () => {
   return (
-    <Flex
-      vertical
-      style={{
-        margin: '-20px 0 0 0px',
-        height: '100vh',
-      }}
-    >
-      <Layout
-        hasSider
+    <SelectedReconPortalEventProvider>
+      <Flex
+        vertical
         style={{
-          gap: '0px',
+          margin: '-20px 0 0 0px',
+          height: '100vh',
         }}
       >
-        <Sider width="20%" breakpoint="md" collapsedWidth={0}>
-          <Flex vertical justify="left" style={{ height: '100%' }}>
-            <ReconSidePanel />
-          </Flex>
-        </Sider>
-        <Content>
-          <ReconPortal />
-        </Content>
-      </Layout>
-    </Flex>
+        <Layout
+          hasSider
+          style={{
+            gap: '0px',
+          }}
+        >
+          <Sider width="20%" breakpoint="md" collapsedWidth={0}>
+            <Flex vertical justify="left" style={{ height: '100%' }}>
+              <ReconSidePanel />
+            </Flex>
+          </Sider>
+          <Content>
+            <ReconPortal />
+          </Content>
+        </Layout>
+      </Flex>
+    </SelectedReconPortalEventProvider>
   );
 };
 
