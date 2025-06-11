84f9047214b9d2941273626a5c1c562d6ea6ee32
Nathan Franklin <nfranklin@tacc.utexas.edu>
2025-06-11 22:32:23 +0200

    task/WG-495: show open topo datasets on leaflet map (#1567)
    * Add opentopo data to map
    
    * Add icon for opentopo marker
    
    * Refactor into util files
    
    * Refactor getting first point
    
    * Add popup
    
    * Show datasets only when zoomed in
    
    * Fix format issues
    
    * Fix imports
    
    * Allow for default styling
    
    * Add bit of space before external link icon
    
    * Fix bolding to match Design
    
    * Tweak icon
    
    * Fix formating
    
    * Avoid using any
    
    * Fix format

--- client/modules/reconportal/src/LeafletMap/LeafletMap.tsx
+++ client/modules/reconportal/src/LeafletMap/LeafletMap.tsx
@@ -1,10 +1,23 @@
-import React from 'react';
+import React, { useMemo } from 'react';
+import ReactDOM from 'react-dom/client';
 import {
   MapContainer,
   ZoomControl,
   TileLayer,
   LayersControl,
+  GeoJSON,
+  Marker,
+  Popup,
 } from 'react-leaflet';
+import { faMap } from '@fortawesome/free-solid-svg-icons';
+import { OpenTopoPopup } from './OpenTopoPopup';
+import {
+  createSvgMarkerIcon,
+  getOpenTopoColor,
+  ZoomConditionalLayerGroup,
+} from './leafletUtil';
+import { getFirstLatLng } from './utils';
+import { useGetOpenTopo } from '@client/hooks';
 
 /* no need to import leaflet css as already in base index
 import 'leaflet/dist/leaflet.css';
@@ -26,6 +39,84 @@ export const mapConfig = {
  * Leaflet Map
  */
 export const LeafletMap: React.FC = () => {
+  const { data: openTopoData } = useGetOpenTopo();
+
+  const openTopoMapFeatures = useMemo(() => {
+    const datasets = openTopoData?.Datasets ?? [];
+
+    // Features for open topo (just polygon and multipolygons)
+    const openTopoGeojsonFeatures: React.ReactNode[] = [];
+
+    // Markers for open topo things that aren't points
+    const openTopoMarkers: React.ReactNode[] = [];
+
+    // Fill openTopoGeojsonFeatures with everything
+    datasets.map(({ Dataset: dataset }, index) => {
+      const { geojson } = dataset.spatialCoverage.geo;
+
+      // Main GeoJSON rendering
+      openTopoGeojsonFeatures.push(
+        <GeoJSON
+          /* Duplicate datasets exist so need an extra index to make unqiue.  Background: OpenTopo seems to have \
+          a duplicate entry for some with multiple types of data e.g. "Point Clouds, Raster" */
+          key={`geojson-${dataset.identifier.value}-${index}`}
+          data={geojson}
+          style={() => ({
+            color: getOpenTopoColor(dataset),
+            weight: 2,
+            fillOpacity: 0.3,
+          })}
+          onEachFeature={(feature, layer) => {
+            const container = document.createElement('div');
+            layer.bindPopup(container);
+
+            layer.on('popupopen', () => {
+              if (container.hasChildNodes()) return;
+
+              ReactDOM.createRoot(container).render(
+                <OpenTopoPopup dataset={dataset} />
+              );
+
+              // Force Leaflet to recalculate popup size
+              // otherwise issue on first popup
+              setTimeout(() => {
+                layer.getPopup()?.update();
+              }, 0);
+            });
+          }}
+        />
+      );
+    });
+
+    // Fill openTopoMarkers with a point for each dataset
+    datasets.map(({ Dataset: dataset }, index) => {
+      const { geojson } = dataset.spatialCoverage.geo;
+
+      const latlngPosition = getFirstLatLng(geojson);
+
+      const icon = createSvgMarkerIcon({
+        color: getOpenTopoColor(dataset),
+        icon: faMap,
+      });
+
+      openTopoMarkers.push(
+        <Marker
+          /* Duplicate datasets exist so need an extra index to make unqiue.  Background: OpenTopo seems to have \
+          a duplicate entry for some with multiple types of data e.g. "Point Clouds, Raster" */
+          key={`marker-${dataset.identifier.value}-${index}`}
+          position={latlngPosition}
+          icon={icon}
+        >
+          <Popup>
+            <OpenTopoPopup dataset={dataset} />
+          </Popup>
+        </Marker>
+      );
+    });
+
+    return [...openTopoGeojsonFeatures, ...openTopoMarkers];
+  }, [openTopoData]);
+
   return (
     <>
       <MapContainer
@@ -57,6 +148,11 @@ export const LeafletMap: React.FC = () => {
             />
           </LayersControl.BaseLayer>
         </LayersControl>
+
+        {/* Open Topo features, only visible when zoomed in and DS event selected*/}
+        <ZoomConditionalLayerGroup minZoom={10}>
+          {openTopoMapFeatures}
+        </ZoomConditionalLayerGroup>
 
         {/* Zoom control */}
         <ZoomControl position="topright" />
---
+++ client/modules/reconportal/src/LeafletMap/OpenTopoPopup.tsx
@@ -0,0 +1,64 @@
+import React from 'react';
+import dayjs from 'dayjs';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faExternalLinkAlt } from '@fortawesome/free-solid-svg-icons';
+import { OpenTopoDataset } from '@client/hooks';
+import { Typography, Space } from 'antd';
+
+const { Title, Text, Link } = Typography;
+
+interface OpenTopoPopupProps {
+  dataset: OpenTopoDataset;
+}
+
+export const OpenTopoPopup: React.FC<OpenTopoPopupProps> = ({ dataset }) => {
+  const title = dataset.name;
+  const startDate = dayjs(dataset.temporalCoverage.split('/')[0]).format(
+    'MM/DD/YYYY'
+  );
+  const endDate = dayjs(dataset.temporalCoverage.split('/')[1]).format(
+    'MM/DD/YYYY'
+  );
+  const doiUrl = dataset.url;
+  const product = dataset.fileFormat;
+  return (
+    <Typography>
+      <Title level={5} style={{ marginBottom: 6 }}>
+        {title}
+      </Title>
+
+      <Space direction="vertical" size={6}>
+        <div>
+          <Text>Date of Survey</Text>
+          <br />
+          <Text strong>
+            {startDate} – {endDate}
+          </Text>
+        </div>
+
+        <div>
+          <Text>Data Source</Text>
+          <br />
+          <Text strong>OpenTopography</Text>
+        </div>
+
+        <div>
+          <Text>Products Available</Text>
+          <br />
+          <Text strong>{product}</Text>
+        </div>
+
+        <div>
+          <Text>DOI</Text>
+          <br />
+          <Link href={doiUrl} target="_blank" rel="noopener noreferrer">
+            {doiUrl}
+            <span style={{ marginLeft: 4 }}>
+              <FontAwesomeIcon icon={faExternalLinkAlt} size="sm" />
+            </span>
+          </Link>
+        </div>
+      </Space>
+    </Typography>
+  );
+};
