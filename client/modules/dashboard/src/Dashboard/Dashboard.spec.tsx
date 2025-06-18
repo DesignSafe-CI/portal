b1da8aeb2a6e89a64aeb650ba93c7c4d59b90180
Sameera Sineen <ss238343@TACCs-MacBook-Pro.local>
2025-06-18 11:25:58 -0500

    Changed Dashboard.specs.ts

--- client/modules/dashboard/src/Dashboard/Dashboard.spec.tsx
+++ client/modules/dashboard/src/Dashboard/Dashboard.spec.tsx
@@ -1,17 +1,8 @@
 import { render } from '@testing-library/react';
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import Dashboard from './Dashboard';
-
 describe('Dashboard', () => {
   it('should render successfully', () => {
-    const queryClient = new QueryClient();
-
-    const { baseElement } = render(
-      <QueryClientProvider client={queryClient}>
-        <Dashboard />
-      </QueryClientProvider>
-    );
-
+    const { baseElement } = render(<Dashboard />);
     expect(baseElement).toBeTruthy();
   });
 });
