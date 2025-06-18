3a51712fbc68ad0c45ad218845ace617763ab4d1
Sameera Sineen <ss238343@TACCs-MacBook-Pro.local>
2025-06-18 11:12:43 -0500

    Changed Dashboard.spec.tsx for client query error

--- client/modules/dashboard/src/Dashboard/Dashboard.spec.tsx
+++ client/modules/dashboard/src/Dashboard/Dashboard.spec.tsx
@@ -1,10 +1,17 @@
 import { render } from '@testing-library/react';
-
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import Dashboard from './Dashboard';
 
 describe('Dashboard', () => {
   it('should render successfully', () => {
-    const { baseElement } = render(<Dashboard />);
+    const queryClient = new QueryClient();
+
+    const { baseElement } = render(
+      <QueryClientProvider client={queryClient}>
+        <Dashboard />
+      </QueryClientProvider>
+    );
+
     expect(baseElement).toBeTruthy();
   });
 });
