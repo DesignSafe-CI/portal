a99e07dde1d4170beff05c2c7cfd4d413177afda
sophia-massie <96220951+sophia-massie@users.noreply.github.com>
2025-06-26 18:07:10 -0500

    Update client/modules/reconportal/src/LeafletMap/LeafletMap.tsx
    Co-authored-by: Nathan Franklin <nfranklin@tacc.utexas.edu>

--- client/modules/reconportal/src/LeafletMap/LeafletMap.tsx
+++ client/modules/reconportal/src/LeafletMap/LeafletMap.tsx
@@ -40,7 +40,6 @@ export const mapConfig = {
   startingCenter: [40, -80] as L.LatLngTuple,
   minZoom: 2, // 2 typically prevents zooming out too far to see multiple earths
   maxZoom: 24, // Maximum possible detail
-  maxFitBoundsInitialZoom: 18,
   maxFitBoundsSelectedFeatureZoom: 18,
   maxPointSelectedFeatureZoom: 15,
   maxBounds: [
