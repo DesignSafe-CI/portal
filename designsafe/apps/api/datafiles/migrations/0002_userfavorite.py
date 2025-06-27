9698ff43c0de265c5f9a90f410a3915c2f3f3401
vani-walvekar1494 <vaniw555@gmail.com>
2025-06-27 15:04:18 -0500

    Fix lint error and update files after merging feat/react-dashboard

--- designsafe/apps/api/datafiles/migrations/0002_userfavorite.py
+++ designsafe/apps/api/datafiles/migrations/0002_userfavorite.py
@@ -9,16 +9,30 @@ class Migration(migrations.Migration):
 
     dependencies = [
         migrations.swappable_dependency(settings.AUTH_USER_MODEL),
-        ('datafiles', '0001_initial'),
+        ("datafiles", "0001_initial"),
     ]
 
     operations = [
         migrations.CreateModel(
-            name='UserFavorite',
+            name="UserFavorite",
             fields=[
-                ('id', models.AutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
-                ('file_path', models.TextField()),
-                ('user', models.ForeignKey(on_delete=django.db.models.deletion.CASCADE, to=settings.AUTH_USER_MODEL)),
+                (
+                    "id",
+                    models.AutoField(
+                        auto_created=True,
+                        primary_key=True,
+                        serialize=False,
+                        verbose_name="ID",
+                    ),
+                ),
+                ("file_path", models.TextField()),
+                (
+                    "user",
+                    models.ForeignKey(
+                        on_delete=django.db.models.deletion.CASCADE,
+                        to=settings.AUTH_USER_MODEL,
+                    ),
+                ),
             ],
         ),
     ]
