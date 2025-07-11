0ef7089d5085c541f682fd08a7235871a49eebae
sophia-massie <smassie@tacc.utexas.edu>
2025-07-11 17:33:08 -0500

    Fix test

--- client/modules/reconportal/src/LeafletMap/LeafletMap.spec.tsx
+++ client/modules/reconportal/src/LeafletMap/LeafletMap.spec.tsx
@@ -1,10 +1,14 @@
 import { render } from '@client/test-fixtures';
-
 import { LeafletMap } from './LeafletMap';
+import { ReconEventProvider } from '@client/hooks';
 
 describe('Recon Portal leaflet map', () => {
   it('should render successfully', () => {
-    const { baseElement } = render(<LeafletMap />);
+    const { baseElement } = render(
+      <ReconEventProvider>
+        <LeafletMap />
+      </ReconEventProvider>
+    );
     expect(baseElement).toBeTruthy();
   });
 });
