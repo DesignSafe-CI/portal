9aede58cb44a16af35430fe87177fe0d3b79064f
Sameera Sineen <ss238343@TACCs-MacBook-Pro.local>
2025-07-17 11:04:15 -0500

    Updated Quicklinks to add icons

--- client/modules/dashboard/src/Dashboard/Quicklinks.tsx
+++ client/modules/dashboard/src/Dashboard/Quicklinks.tsx
@@ -6,10 +6,12 @@ const Quicklinks = () => {
     <div className={styles.sidebar}>
       <div className={styles.sidebarTitle}>Quick Links</div>
       <a href="/account" className={styles.sidebarLink}>
+        <i className="fa fa-user" style={{ marginRight: '8px' }}></i>
         Manage Account
       </a>
 
       <a href="/learning-center/overview" className={styles.sidebarLink}>
+        <i className="fa fa-book" style={{ marginRight: '8px' }}></i>
         Training
       </a>
     </div>
