200c88bd15e4cf5e07c65b71c072a03deac2274f
Van Go <35277477+van-go@users.noreply.github.com>
2025-07-23 12:33:30 -0500

    update mapping

--- client/modules/_hooks/src/index.ts
+++ client/modules/_hooks/src/index.ts
@@ -2,7 +2,7 @@ export { useAuthenticatedUser, type TUser } from './useAuthenticatedUser';
 export { useDebounceValue } from './useDebounceValue';
 export { useSystemOverview } from './useSystemOverview';
 export { useSystemQueue } from './useSystemQueue';
-export { useKeywordSuggestions } from '../../datafiles/src/hooks/useKeywordSuggestions';
+export { useKeywordSuggestions } from '@client/datafiles/hooks/useKeywordSuggestions';
 
 export { default as apiClient } from './apiClient';
 export * from './workspace';
