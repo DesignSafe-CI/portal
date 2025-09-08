6fc98bd469d65d3fc451fb2c75dd1bcc9ba18620
sophia-massie <smassie@tacc.utexas.edu>
2025-09-08 17:34:03 -0500

    Linting

--- client/modules/reconportal/src/LeafletMap/LeafletMap.tsx
+++ client/modules/reconportal/src/LeafletMap/LeafletMap.tsx
@@ -161,7 +161,7 @@ export const LeafletMap: React.FC = () => {
         <LayersControl position="topright" collapsed={false}>
           <LayersControl.BaseLayer checked name="View Terrain">
             <TileLayer
-              attribution='Tiles &copy; Esri &mdash; Source: Esri, i-cubed, USDA, USGS, AEX, GeoEye, Getmapping, Aerogrid, IGN, IGP, UPR-EGP, and the GIS User Community'
+              attribution="Tiles &copy; Esri &mdash; Source: Esri, i-cubed, USDA, USGS, AEX, GeoEye, Getmapping, Aerogrid, IGN, IGP, UPR-EGP, and the GIS User Community"
               url="https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}"
               maxNativeZoom={
                 14
