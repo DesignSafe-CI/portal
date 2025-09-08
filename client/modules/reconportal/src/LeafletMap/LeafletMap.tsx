acead9fcb5b78015b0bc72c5e3b051ea2643c939
sophia-massie <smassie@tacc.utexas.edu>
2025-09-08 17:42:34 -0500

    Change label to Satellite rather than Terrain

--- client/modules/reconportal/src/LeafletMap/LeafletMap.tsx
+++ client/modules/reconportal/src/LeafletMap/LeafletMap.tsx
@@ -159,7 +159,7 @@ export const LeafletMap: React.FC = () => {
       >
         {/* Base layers */}
         <LayersControl position="topright" collapsed={false}>
-          <LayersControl.BaseLayer checked name="View Terrain">
+          <LayersControl.BaseLayer checked name="View Satellite">
             <TileLayer
               attribution="Tiles &copy; Esri &mdash; Source: Esri, i-cubed, USDA, USGS, AEX, GeoEye, Getmapping, Aerogrid, IGN, IGP, UPR-EGP, and the GIS User Community"
               url="https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}"
