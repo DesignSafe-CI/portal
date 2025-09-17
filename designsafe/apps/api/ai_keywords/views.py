1051d97bcea189df1bb8e8192c0f0c327901571c
Sal Tijerina <r.sal.tijerina@gmail.com>
2025-09-17 17:03:46 -0500

    add score threshold, increase returned documents to 10

--- designsafe/apps/api/ai_keywords/views.py
+++ designsafe/apps/api/ai_keywords/views.py
@@ -1,6 +1,5 @@
 """Views for RAG-based keyword suggestions."""
 
-import json
 import logging
 from chromadb import HttpClient, Settings
 from typing_extensions import List, TypedDict
@@ -14,7 +13,10 @@ from django.http import HttpRequest, JsonResponse
 from django.conf import settings
 from designsafe.apps.api.views import BaseApiView, ApiException
 
-logger = logging.Logger(__name__)
+logger = logging.getLogger(__name__)
+
+
+SCORE_THRESHOLD = 0.25
 
 
 class KeywordsView(BaseApiView):
@@ -41,9 +43,10 @@ class KeywordsView(BaseApiView):
             {"question": f"project title: {title}, description: {description}"}
         )
         try:
-            answer = json.loads(resp["answer"])
-        except json.JSONDecodeError:
+            answer = resp["answer"].split(', ')
+        except AttributeError:
             logger.exception("Error decoding answer")
+            logger.debug(f"Raw answer: {resp['answer']}")
             answer = []
 
         resp_list = answer if isinstance(answer, list) else []
@@ -67,15 +70,17 @@ class RAG:
 
     template = """
     You are an assistant for finding keywords for a supplied project title and description. Use the following pieces of retrieved context to answer the question. If you don't know the answer, respond with nothing.
-    Reference the 'keyword' field in the metadata object of the retrieved responses for existing keyword examples.
-    Respond only with the suggested keywords as a Python array of comma-separated strings, ordered by rank.
+    Reference the "keywords" field in the metadata object of the retrieved responses for existing keyword examples.
+    Respond only with the suggested keywords as a comma-separated list of strings, ordered by rank. Respond only with your final answer, and do not include any other text or commentary.
+    Make sure to follow these guidelines for keyword suggestions:
 
     Best Practices for Keywords
-    • Keywords may be single words or multi-word phrases (e.g. “storm surge”, “tropical storm”, “machine learning”). Match the format of suggested keywords to that of the existing keyword examples.
-    • Think like a user: choose terms someone would actually type (hazard type, method, region…).
-    • Include technology or problem addressed, and the purpose of the data.
-    • Repeat important words from your title/description to boost discoverability.
-    • Rank by frequency among other existing projects, by similarity or synonym to the words in the project title and project description; proper nouns carry less weight.
+    - Keywords may be single words or multi-word phrases (e.g. "storm surge", "tropical storm", "machine learning"). Match the format of suggested keywords to that of the existing keyword examples.
+    - Think like a user: choose terms someone would actually type (hazard type, method, region).
+    - Include technology or problem addressed, and the purpose of the data.
+    - Repeat important words from your title/description to boost discoverability.
+    - Rank by frequency among other existing projects, by similarity or synonym to the words in the project title and project description; proper nouns carry less weight.
+    - Do not include proper nouns (e.g. specific names of people, places, or events) unless directly referenced in the provided title or description.
 
     Question: {question}
     Context: {context}
@@ -121,12 +126,12 @@ class RAG:
 
     def retrieve(self, state: State):
         """Retrieve relevant documents from vector store based on the question."""
-        retrieved_docs = self.vector_store.similarity_search(state["question"])
-        return {"context": retrieved_docs}
+        retrieved_docs = self.vector_store.similarity_search_with_relevance_scores(state["question"], k=10, score_threshold=SCORE_THRESHOLD)
+        return {"context": [doc[0].page_content for doc in retrieved_docs]}
 
     def generate(self, state: State):
         """Generate an answer based on the question and retrieved documents."""
-        docs_content = "\n\n".join(doc.page_content for doc in state["context"])
+        docs_content = "\n\n".join(doc for doc in state["context"])
         messages = self.QA_CHAIN_PROMPT.invoke(
             {"question": state["question"], "context": docs_content}
         )
