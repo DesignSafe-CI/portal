44e07c8199908a52d362d0246dca3ac30205e23b
Sarah Gray <sgray@tacc.utexas.edu>
2026-09-11 10:08:05 -0500

    fixing linting

--- client/modules/datafiles/src/projects/BaseProjectDetails.tsx
+++ client/modules/datafiles/src/projects/BaseProjectDetails.tsx
@@ -73,13 +73,18 @@ export const LicenseDisplay: React.FC<{ licenseType: string }> = ({
     '3-Clause BSD License': 'curation-3bsd',
   };
   const LICENSE_URL_MAP: Record<string, string> = {
-    'GNU General Public License': 'https://choosealicense.com/licenses/gpl-3.0/',
-    'Open Data Commons Attribution': 'https://opendatacommons.org/licenses/by/summary/',
-    'Open Data Commons Public Domain Dedication': 'https://opendatacommons.org/licenses/pddl/summary/',
-    'Creative Commons Attribution': 'https://creativecommons.org/licenses/by/4.0/deed.en',
-    'Creative Commons Public Domain Dedication': 'https://creativecommons.org/publicdomain/zero/1.0/',
+    'GNU General Public License':
+      'https://choosealicense.com/licenses/gpl-3.0/',
+    'Open Data Commons Attribution':
+      'https://opendatacommons.org/licenses/by/summary/',
+    'Open Data Commons Public Domain Dedication':
+      'https://opendatacommons.org/licenses/pddl/summary/',
+    'Creative Commons Attribution':
+      'https://creativecommons.org/licenses/by/4.0/deed.en',
+    'Creative Commons Public Domain Dedication':
+      'https://creativecommons.org/publicdomain/zero/1.0/',
     '3-Clause BSD License': 'https://opensource.org/license/bsd-3-clause',
-  }
+  };
 
   return (
     <div style={{ display: 'flex', alignItems: 'center' }}>
